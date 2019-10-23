b73af5d32c85c8e0ba0bcf8e1af034f3c919c400
Lorenz Bauer <lmb@cloudflare.com>
2019-10-23 13:20:51 +0100

    editor: remove RewriteConstant
    RewriteConstant is a useful hack, that is superseded by proper static data
    support in BPF. Remove it so that we don't have to support it going forward.

--- editor.go
+++ editor.go
@@ -60,47 +60,6 @@ func (ed *Editor) rewriteMap(symbol string, m *Map, overwrite bool) error {
 	return nil
 }
 
-// RewriteConstant rewrites all loads of a symbol to a constant value.
-//
-// This is a way to parameterize clang-compiled eBPF byte code at load
-// time.
-//
-// The following macro should be used to access the constant:
-//
-//    #define LOAD_CONSTANT(param, var) asm("%0 = " param " ll" : "=r"(var))
-//
-//    int xdp() {
-//        bool my_constant;
-//        LOAD_CONSTANT("SYMBOL_NAME", my_constant);
-//
-//        if (my_constant) ...
-//
-// Caveats:
-//   - The symbol name you pick must be unique
-//
-//   - Failing to rewrite a symbol will not result in an error,
-//     0 will be loaded instead (subject to change)
-//
-// Use IsUnreferencedSymbol if you want to rewrite potentially
-// unused symbols.
-func (ed *Editor) RewriteConstant(symbol string, value uint64) error {
-	indices := ed.ReferenceOffsets[symbol]
-	if len(indices) == 0 {
-		return &unreferencedSymbolError{symbol}
-	}
-
-	ldDWImm := asm.LoadImmOp(asm.DWord)
-	for _, index := range indices {
-		load := &(*ed.instructions)[index]
-		if load.OpCode != ldDWImm {
-			return errors.Errorf("symbol %v: load: found %v instead of %v", symbol, load.OpCode, ldDWImm)
-		}
-
-		load.Constant = int64(value)
-	}
-	return nil
-}
-
 // Link resolves bpf-to-bpf calls.
 //
 // Each section may contain multiple functions / labels, and is only linked

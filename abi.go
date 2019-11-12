371e66207440c9dd58daef131f9b5658dd757981
Lorenz Bauer <lmb@cloudflare.com>
2019-11-12 12:24:57 +0000

    abi: remove CollectionABI
    Code loading eBPF from an ELF file invariably ends up checking
    that the correct maps and programs are present, and sometimes
    checking the type or size of them. The various *ABI types attempt
    to make this simple to achieve, by providing the Check and CheckSpec
    functions.
    
    However, they suffer from multiple problems: first, CheckSpec only
    checks for the presence of a program or map, even if type, etc.
    are specified in the CollectionABI. This makes sense because
    CollectionABI.Check does verify this later on, but it's very
    unintuitive for the user. Second, using the *ABI means that checking
    whether a map / program exist is divorced from actually using or
    modifying them. The user has to repeat map and program names all over
    the place. Finally, the Check functions assume that the zero value of
    an ABI member means "I don't care about this value". This breaks for
    Map flags, since the zero value is valid and indeed very common.
    
    Remove the CollectionABI and the Check functions for now, until we
    find a better way to reduce the repetition required when dealing
    with collections. This enables us to remove the zero value behaviour
    from *ABI.

--- abi.go
+++ abi.go
@@ -1,61 +1,8 @@
 package ebpf
 
-import (
-	"github.com/pkg/errors"
-)
-
-// CollectionABI describes the interface of an eBPF collection.
-type CollectionABI struct {
-	Maps     map[string]*MapABI
-	Programs map[string]*ProgramABI
-}
-
-// CheckSpec verifies that all maps and programs mentioned
-// in the ABI are present in the spec.
-func (abi *CollectionABI) CheckSpec(cs *CollectionSpec) error {
-	for name := range abi.Maps {
-		if cs.Maps[name] == nil {
-			return errors.Errorf("missing map %s", name)
-		}
-	}
-
-	for name := range abi.Programs {
-		if cs.Programs[name] == nil {
-			return errors.Errorf("missing program %s", name)
-		}
-	}
-
-	return nil
-}
-
-// Check verifies that all items in a collection conform to this ABI.
-func (abi *CollectionABI) Check(coll *Collection) error {
-	for name, mapABI := range abi.Maps {
-		m := coll.Maps[name]
-		if m == nil {
-			return errors.Errorf("missing map %s", name)
-		}
-		if err := mapABI.Check(m); err != nil {
-			return errors.Wrapf(err, "map %s", name)
-		}
-	}
-
-	for name, progABI := range abi.Programs {
-		p := coll.Programs[name]
-		if p == nil {
-			return errors.Errorf("missing program %s", name)
-		}
-		if err := progABI.Check(p); err != nil {
-			return errors.Wrapf(err, "program %s", name)
-		}
-	}
-
-	return nil
-}
+import "github.com/pkg/errors"
 
 // MapABI describes a Map.
-//
-// Use it to assert that a Map matches what your code expects.
 type MapABI struct {
 	Type       MapType
 	KeySize    uint32
@@ -99,45 +46,7 @@ func newMapABIFromFd(fd *bpfFD) (*MapABI, error) {
 	}, nil
 }
 
-// Check verifies that a Map conforms to the ABI.
-//
-// Members of ABI which have the zero value of their type are not checked.
-func (abi *MapABI) Check(m *Map) error {
-	return abi.check(&m.abi)
-}
-
-func (abi *MapABI) check(other *MapABI) error {
-	if abi.Type != UnspecifiedMap && other.Type != abi.Type {
-		return errors.Errorf("expected map type %s, have %s", abi.Type, other.Type)
-	}
-	if err := checkUint32("key size", abi.KeySize, other.KeySize); err != nil {
-		return err
-	}
-	if err := checkUint32("value size", abi.ValueSize, other.ValueSize); err != nil {
-		return err
-	}
-	if err := checkUint32("max entries", abi.MaxEntries, other.MaxEntries); err != nil {
-		return err
-	}
-
-	if abi.InnerMap == nil {
-		if abi.Type == ArrayOfMaps || abi.Type == HashOfMaps {
-			return errors.New("missing inner map ABI")
-		}
-
-		return nil
-	}
-
-	if other.InnerMap == nil {
-		return errors.New("missing inner map")
-	}
-
-	return errors.Wrap(abi.InnerMap.check(other.InnerMap), "inner map")
-}
-
 // ProgramABI describes a Program.
-//
-// Use it to assert that a Program matches what your code expects.
 type ProgramABI struct {
 	Type ProgramType
 }
@@ -161,23 +70,4 @@ func newProgramABIFromInfo(info *bpfProgInfo) *ProgramABI {
 	return &ProgramABI{
 		Type: ProgramType(info.progType),
 	}
-}
-
-// Check verifies that a Program conforms to the ABI.
-//
-// Members which have the zero value of their type
-// are not checked.
-func (abi *ProgramABI) Check(prog *Program) error {
-	if abi.Type != UnspecifiedProgram && prog.abi.Type != abi.Type {
-		return errors.Errorf("expected program type %s, have %s", abi.Type, prog.abi.Type)
-	}
-
-	return nil
-}
-
-func checkUint32(name string, want, have uint32) error {
-	if want != 0 && have != want {
-		return errors.Errorf("expected %s to be %d, have %d", name, want, have)
-	}
-	return nil
 }

d9fb101ca1fbfe02b4caf393f2b706f72839b30b
Lorenz Bauer <lmb@cloudflare.com>
2019-11-13 09:52:28 +0000

    map, program: fall back to /proc for getting an ABI
    Fall back to reading /proc/self/*/fdinfo if BPF_OBJ_GET_INFO_BY_FD is
    not available on the current kernel. This should give us compatibility
    to at least 4.9.
    
    Fixes #5

--- abi_test.go
+++ abi_test.go
@@ -22,6 +22,66 @@ func TestMapABIEqual(t *testing.T) {
 	}
 }
 
+func TestMapABIFromProc(t *testing.T) {
+	hash, err := NewMap(&MapSpec{
+		Type:       Hash,
+		KeySize:    4,
+		ValueSize:  5,
+		MaxEntries: 2,
+		Flags:      0x1, // BPF_F_NO_PREALLOC
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer hash.Close()
+
+	abi, err := newMapABIFromProc(hash.fd)
+	if err != nil {
+		t.Fatal("Can't get map ABI:", err)
+	}
+
+	if abi.Type != Hash {
+		t.Error("Expected Hash, got", abi.Type)
+	}
+
+	if abi.KeySize != 4 {
+		t.Error("Expected KeySize of 4, got", abi.KeySize)
+	}
+
+	if abi.ValueSize != 5 {
+		t.Error("Expected ValueSize of 5, got", abi.ValueSize)
+	}
+
+	if abi.MaxEntries != 2 {
+		t.Error("Expected MaxEntries of 2, got", abi.MaxEntries)
+	}
+
+	if abi.Flags != 1 {
+		t.Error("Expected Flags to be 1, got", abi.Flags)
+	}
+
+	nested, err := NewMap(&MapSpec{
+		Type:       ArrayOfMaps,
+		KeySize:    4,
+		MaxEntries: 2,
+		InnerMap: &MapSpec{
+			Type:       Array,
+			KeySize:    4,
+			ValueSize:  4,
+			MaxEntries: 2,
+		},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer nested.Close()
+
+	_, err = newMapABIFromProc(nested.fd)
+	if err != nil {
+		t.Fatal("Can't get nested map ABI from /proc:", err)
+	}
+}
+
 func TestProgramABI(t *testing.T) {
 	abi := &ProgramABI{Type: SocketFilter}
 

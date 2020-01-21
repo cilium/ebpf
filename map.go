01b862cbf2597b39ec59c1a886287b3a707050b5
josyuan <josyuan@cisco.com>
2020-01-21 11:41:01 -0600

    removed a change i did not mean to make concerning adding a pinning field to mapspec

--- map.go
+++ map.go
@@ -20,7 +20,6 @@ type MapSpec struct {
 	ValueSize  uint32
 	MaxEntries uint32
 	Flags      uint32
-	Pinning    uint32
 
 	// InnerMap is used as a template for ArrayOfMaps and HashOfMaps
 	InnerMap *MapSpec

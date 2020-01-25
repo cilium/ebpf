dd24de62627af358ee35270040abe7a1640135e4
Lehner Florian <dev@der-flo.net>
2020-01-25 15:58:45 +0100

    migrate from github.com/pkg/errors to golang.org/x/xerrors
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- internal/testutils/feature.go
+++ internal/testutils/feature.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/cilium/ebpf/internal"
 	"github.com/cilium/ebpf/internal/unix"
-	"github.com/pkg/errors"
+	"golang.org/x/xerrors"
 )
 
 var (
@@ -48,13 +48,11 @@ func CheckFeatureTest(t *testing.T, fn func() error) {
 }
 
 func SkipIfNotSupported(tb testing.TB, err error) {
-	ufe, ok := errors.Cause(err).(*internal.UnsupportedFeatureError)
-	if !ok {
+	if err == nil || !xerrors.Is(err, (err).(*internal.UnsupportedFeatureError)) {
 		return
 	}
-
-	checkKernelVersion(tb, ufe)
-	tb.Skip(ufe.Error())
+	checkKernelVersion(tb, xerrors.Unwrap(err).(*internal.UnsupportedFeatureError))
+	tb.Skip(err.Error())
 }
 
 func checkKernelVersion(tb testing.TB, ufe *internal.UnsupportedFeatureError) {

eda5b92cbb0ec08535ac384d7947049a73dd1b96
Lorenz Bauer <lmb@cloudflare.com>
2020-01-30 15:08:40 +0000

    asm: introduce PseudoCall
    Introduce a constant analogous to BPF_PSEUDO_CALL.

--- asm/register.go
+++ asm/register.go
@@ -33,10 +33,11 @@ const (
 	RFP          = R10
 )
 
-// Pseudo registers used by 64bit loads
+// Pseudo registers used by 64bit loads and jumps
 const (
 	PseudoMapFD    = R1 // BPF_PSEUDO_MAP_FD
 	PseudoMapValue = R2 // BPF_PSEUDO_MAP_VALUE
+	PseudoCall     = R1 // BPF_PSEUDO_CALL
 )
 
 func (r Register) String() string {

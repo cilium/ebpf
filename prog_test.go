914a016c32b3960a010d1e7cac8ab39d83d85752
Lehner Florian <dev@der-flo.net>
2020-02-10 17:29:26 +0100

    Add *GetNextID
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- prog_test.go
+++ prog_test.go
@@ -319,6 +319,30 @@ func TestHaveProgTestRun(t *testing.T) {
 	testutils.CheckFeatureTest(t, haveProgTestRun)
 }
 
+func TestProgGetNextID(t *testing.T) {
+	var next uint32
+
+	prog, err := NewProgram(&ProgramSpec{
+		Type: SkSKB,
+		Instructions: asm.Instructions{
+			asm.LoadImm(asm.R0, 0, asm.DWord),
+			asm.Return(),
+		},
+		License: "MIT",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer prog.Close()
+
+	if err := ProgGetNextID(0, &next); err != nil {
+		t.Fatal("Can't get next ID:", err)
+	}
+	if next == 0 {
+		t.Fatal("Expected next ID other than 0")
+	}
+}
+
 func createProgramArray(t *testing.T) *Map {
 	t.Helper()
 

27a183dffa79e46137a02504bb9909f01aab3f7b
Lorenz Bauer <lmb@cloudflare.com>
2020-05-21 12:01:38 +0100

    run tests regardless of endianness
    The library should be able to deal with files in any endianness. Rework
    tests to load both little and big-endian ELFs.

--- internal/btf/btf_test.go
+++ internal/btf/btf_test.go
@@ -54,53 +54,51 @@ func TestParseCurrentKernelBTF(t *testing.T) {
 }
 
 func TestLoadSpecFromElf(t *testing.T) {
-	fh, err := os.Open(fmt.Sprintf("../../testdata/loader-clang-9-%s.elf", testutils.GetHostEndianness()))
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer fh.Close()
+	testutils.TestFiles(t, "../../testdata/loader-clang-9-*.elf", func(t *testing.T, file string) {
+		fh, err := os.Open(file)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer fh.Close()
 
-	spec, err := LoadSpecFromReader(fh)
-	if err != nil {
-		t.Fatal("Can't load BTF:", err)
-	}
-
-	if spec == nil {
-		t.Error("No BTF found in ELF")
-	}
-
-	if sec, err := spec.Program("xdp", 1); err != nil {
-		t.Error("Can't get BTF for the xdp section:", err)
-	} else if sec == nil {
-		t.Error("Missing BTF for the xdp section")
-	}
-
-	if sec, err := spec.Program("socket", 1); err != nil {
-		t.Error("Can't get BTF for the socket section:", err)
-	} else if sec == nil {
-		t.Error("Missing BTF for the socket section")
-	}
-
-	var bpfMapDef Struct
-	if err := spec.FindType("bpf_map_def", &bpfMapDef); err != nil {
-		t.Fatal("Can't find bpf_map_def:", err)
-	}
-
-	if name := bpfMapDef.Name; name != "bpf_map_def" {
-		t.Error("struct bpf_map_def has incorrect name:", name)
-	}
-
-	if spec.byteOrder != internal.NativeEndian {
-		return
-	}
-
-	t.Run("Handle", func(t *testing.T) {
-		btf, err := NewHandle(spec)
-		testutils.SkipIfNotSupported(t, err)
+		spec, err := LoadSpecFromReader(fh)
 		if err != nil {
 			t.Fatal("Can't load BTF:", err)
 		}
-		defer btf.Close()
+
+		if spec == nil {
+			t.Error("No BTF found in ELF")
+		}
+
+		if sec, err := spec.Program("xdp", 1); err != nil {
+			t.Error("Can't get BTF for the xdp section:", err)
+		} else if sec == nil {
+			t.Error("Missing BTF for the xdp section")
+		}
+
+		if sec, err := spec.Program("socket", 1); err != nil {
+			t.Error("Can't get BTF for the socket section:", err)
+		} else if sec == nil {
+			t.Error("Missing BTF for the socket section")
+		}
+
+		var bpfMapDef Struct
+		if err := spec.FindType("bpf_map_def", &bpfMapDef); err != nil {
+			t.Fatal("Can't find bpf_map_def:", err)
+		}
+
+		if spec.byteOrder != internal.NativeEndian {
+			return
+		}
+
+		t.Run("Handle", func(t *testing.T) {
+			btf, err := NewHandle(spec)
+			testutils.SkipIfNotSupported(t, err)
+			if err != nil {
+				t.Fatal("Can't load BTF:", err)
+			}
+			defer btf.Close()
+		})
 	})
 }
 

6b529261e0990b411e64fc9b1cb6363a5499c545
Lorenz Bauer <lmb@cloudflare.com>
2020-06-05 16:01:24 +0100

    link: add support for attaching to cgroups
    Use the BPF_PROG_ATTACH and bpf_link infrastructure to implement a
    generic way to attach to cgroups. Older kernels will use BPF_PROG_ATTACH,
    newer ones bpf_link.

--- link/syscalls_test.go
+++ link/syscalls_test.go
@@ -10,6 +10,10 @@ func TestHaveProgAttach(t *testing.T) {
 	testutils.CheckFeatureTest(t, haveProgAttach)
 }
 
+func TestHaveProgAttachReplace(t *testing.T) {
+	testutils.CheckFeatureTest(t, haveProgAttachReplace)
+}
+
 func TestHaveBPFLink(t *testing.T) {
 	testutils.CheckFeatureTest(t, haveBPFLink)
 }

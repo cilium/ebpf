38a623999e1543c48150967beab159ccf9cfcf77
Bryce Kahle <bryce.kahle@datadoghq.com>
2020-07-27 17:07:40 -0400

    Add uint8 version of utsnameStr

--- kernel_version.go
+++ kernel_version.go
@@ -115,14 +115,3 @@ func CurrentKernelVersion() (uint32, error) {
 	}
 	return currentVersionUname()
 }
-
-func utsnameStr(in []int8) string {
-	out := make([]byte, len(in))
-	for i := 0; i < len(in); i++ {
-		if in[i] == 0 {
-			break
-		}
-		out = append(out, byte(in[i]))
-	}
-	return string(out)
-}
---
+++ utsname_int8.go
@@ -0,0 +1,14 @@
+// +build linux,amd64 linux,arm64
+
+package ebpf
+
+func utsnameStr(in []int8) string {
+	out := make([]byte, len(in))
+	for i := 0; i < len(in); i++ {
+		if in[i] == 0 {
+			break
+		}
+		out = append(out, byte(in[i]))
+	}
+	return string(out)
+}

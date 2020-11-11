82d6f802405515c5b63b8cc94462e2a6a6fc2f58
Lorenz Bauer <lmb@cloudflare.com>
2020-11-11 12:16:53 +0000

    link: add AttachSkLookup
    Add a wrapper for SkLookup programs that allows retrieving information
    about the link.

---
+++ link/sk_lookup_test.go
@@ -0,0 +1,84 @@
+package link
+
+import (
+	"os"
+	"testing"
+
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/asm"
+	"github.com/cilium/ebpf/internal/testutils"
+)
+
+func TestSkLookup(t *testing.T) {
+	testutils.SkipOnOldKernel(t, "5.8", "sk_lookup program")
+
+	prog := createSkLookupProgram()
+	defer prog.Close()
+
+	netns, err := os.Open("/proc/self/ns/net")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer netns.Close()
+
+	link, err := AttachSkLookup(int(netns.Fd()), prog)
+	if err != nil {
+		t.Fatal("Can't attach link:", err)
+	}
+
+	info, err := link.Info()
+	if err != nil {
+		t.Fatal("Can't get info:", err)
+	}
+
+	progID, err := prog.ID()
+	if err != nil {
+		t.Fatal("Can't get program ID:", err)
+	}
+	if info.Program != progID {
+		t.Error("Link program ID doesn't match program ID")
+	}
+
+	testLink(t, link, testLinkOptions{
+		prog: prog,
+		loadPinned: func(fileName string) (Link, error) {
+			return LoadPinnedSkLookup(fileName)
+		},
+	})
+}
+
+func createSkLookupProgram() *ebpf.Program {
+	prog, err := ebpf.NewProgram(&ebpf.ProgramSpec{
+		Type:       ebpf.SkLookup,
+		AttachType: ebpf.AttachSkLookup,
+		License:    "MIT",
+		Instructions: asm.Instructions{
+			asm.Mov.Imm(asm.R0, 0),
+			asm.Return(),
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+	return prog
+}
+
+func ExampleAttachSkLookup() {
+	prog := createSkLookupProgram()
+	defer prog.Close()
+
+	// This can be a path to another netns as well.
+	netns, err := os.Open("/proc/self/ns/net")
+	if err != nil {
+		panic(err)
+	}
+	defer netns.Close()
+
+	link, err := AttachSkLookup(int(netns.Fd()), prog)
+	if err != nil {
+		panic(err)
+	}
+
+	// The socket lookup program is now active until Close().
+	link.Close()
+}

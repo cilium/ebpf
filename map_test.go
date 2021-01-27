28735c9ce6174fe6cab0f2c65fa7513a312de252
Aditi Ghag <aditi@cilium.io>
2021-01-26 19:35:22 -0800

    map: Add a method to Unpin maps
    This is an inverse of the Pin method. Also, add a method
    to get a map's pinned state.
    
    Signed-off-by: Aditi Ghag aditi@cilium.io

--- map_test.go
+++ map_test.go
@@ -15,6 +15,8 @@ import (
 	"github.com/cilium/ebpf/internal"
 	"github.com/cilium/ebpf/internal/testutils"
 	"github.com/cilium/ebpf/internal/unix"
+
+	qt "github.com/frankban/quicktest"
 )
 
 func TestMain(m *testing.M) {
@@ -96,6 +98,7 @@ func TestMapCloneNil(t *testing.T) {
 
 func TestMapPin(t *testing.T) {
 	m := createArray(t)
+	c := qt.New(t)
 	defer m.Close()
 
 	if err := m.Put(uint32(0), uint32(42)); err != nil {
@@ -112,6 +115,11 @@ func TestMapPin(t *testing.T) {
 	if err := m.Pin(path); err != nil {
 		t.Fatal(err)
 	}
+
+	pinned, path := m.IsPinned()
+	c.Assert(pinned, qt.Equals, true)
+	c.Assert(path, qt.Equals, path)
+
 	m.Close()
 
 	m, err := LoadPinnedMap(path)
@@ -165,6 +173,39 @@ func TestNestedMapPin(t *testing.T) {
 		t.Fatal(err)
 	}
 	defer m.Close()
+}
+
+func TestMapUnpin(t *testing.T) {
+	tmp := tempBPFFS(t)
+	c := qt.New(t)
+	spec := &MapSpec{
+		Name:       "foo",
+		Type:       Hash,
+		KeySize:    4,
+		ValueSize:  4,
+		MaxEntries: 1,
+		Pinning:    PinByName,
+	}
+
+	m, err := NewMapWithOptions(spec, MapOptions{PinPath: tmp})
+	if err != nil {
+		t.Fatal("Failed to create map:", err)
+	}
+	defer m.Close()
+
+	pinned, path := m.IsPinned()
+	c.Assert(pinned, qt.Equals, true)
+	c.Assert(path, qt.Equals, path)
+	path = filepath.Join(tmp, spec.Name)
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		t.Fatal("Failed to pin map:", err)
+	}
+	if err = m.Unpin(); err != nil {
+		t.Fatal("Failed to unpin map:", err)
+	}
+	if _, err := os.Stat(path); err == nil {
+		t.Fatal("Pinned map path still exists after unpinning:", err)
+	}
 }
 
 func createArray(t *testing.T) *Map {
@@ -810,6 +851,7 @@ func TestNewMapFromID(t *testing.T) {
 
 func TestMapPinning(t *testing.T) {
 	tmp := tempBPFFS(t)
+	c := qt.New(t)
 
 	spec := &MapSpec{
 		Name:       "test",
@@ -825,6 +867,9 @@ func TestMapPinning(t *testing.T) {
 		t.Fatal("Can't create map:", err)
 	}
 	defer m1.Close()
+	pinned, path := m1.IsPinned()
+	c.Assert(pinned, qt.Equals, true)
+	c.Assert(path, qt.Equals, path)
 
 	if err := m1.Put(uint32(0), uint32(42)); err != nil {
 		t.Fatal("Can't write value:", err)

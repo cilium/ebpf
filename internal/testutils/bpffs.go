8bb9d0e198fc2101ea7ab9d976b2cf14a860c387
Lorenz Bauer <lmb@cloudflare.com>
2021-03-09 12:11:45 +0000

    testutil: add TempBPFFS
    Move the helper to create a temporary BPF FS directory to testutils.

---
+++ internal/testutils/bpffs.go
@@ -0,0 +1,22 @@
+package testutils
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+// TempBPFFS creates a temporary directory on a BPF FS.
+//
+// The directory is automatically cleaned up at the end of the test run.
+func TempBPFFS(tb testing.TB) string {
+	tb.Helper()
+
+	tmp, err := ioutil.TempDir("/sys/fs/bpf", "ebpf-test")
+	if err != nil {
+		tb.Fatal("Create temporary directory on BPFFS:", err)
+	}
+	tb.Cleanup(func() { os.RemoveAll(tmp) })
+
+	return tmp
+}

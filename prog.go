069ebaa30d52431152f2c784dcae066437af1934
Aditi Ghag <aditi@cilium.io>
2021-04-01 21:43:21 -0700

    pinning: Disallow replace during pinning
    The current implementation uses `os.Rename` which calls `rename` to overwrite an existing pinned path. If the new path already existed before the overwrite, it'll simply be replaced with this syscall. Hence, use the `renameat2` syscall instead that can disallow the replace by default behavior.
    
    Signed-off-by: Aditi Ghag aditi@cilium.io

--- prog.go
+++ prog.go
@@ -345,6 +345,9 @@ func (p *Program) Clone() (*Program, error) {
 // Pin persists the Program on the BPF virtual file system past the lifetime of
 // the process that created it
 //
+// Calling Pin on a previously pinned program will overwrite the path, except when
+// the new path already exists. Re-pinning across filesystems is not supported.
+//
 // This requires bpffs to be mounted above fileName. See https://docs.cilium.io/en/k8s-doc/admin/#admin-mount-bpffs
 func (p *Program) Pin(fileName string) error {
 	if err := pin(p.pinnedPath, fileName, p.fd); err != nil {

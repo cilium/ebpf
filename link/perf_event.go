422b27d8403c1912528d88b16b124ec449d281a4
Lorenz Bauer <lmb@cloudflare.com>
2021-04-06 10:10:23 +0100

    link: set a finalizer on perfEvent
    Try to clean up perfEvent even if it's leaked by setting a finalizer
    on it.

--- link/perf_event.go
+++ link/perf_event.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
+	"runtime"
 	"strconv"
 	"strings"
 	"unsafe"
@@ -157,6 +158,7 @@ func (pe *perfEvent) attach(prog *ebpf.Program) error {
 		return fmt.Errorf("enable perf event: %s", err)
 	}
 
+	runtime.SetFinalizer(pe, (*perfEvent).Close)
 	return nil
 }
 

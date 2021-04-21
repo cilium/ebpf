eb7aba95352855cef0f504d50d1734274c00091e
Lorenz Bauer <lmb@cloudflare.com>
2021-04-21 16:45:04 +0100

    WIP more debugging

--- run-tests.sh
+++ run-tests.sh
@@ -27,6 +27,10 @@ if [[ "${1:-}" = "--in-vm" ]]; then
   fi
 
   dmesg -C
+  for i in kmalloc kmalloc_node kfree kmem_cache_alloc kmem_cache_alloc_node kmem_cache_free; do
+    echo 1 > /sys/kernel/debug/tracing/events/kmem/$i/enable
+  done
+  cat /sys/kernel/debug/tracing/trace_pipe &
   if ! eval "$@"; then
     dmesg
     exit 1
@@ -77,7 +81,7 @@ fi
 if (( $# > 0 )); then
   printf -v cmd " %q" "$@"
 else
-  printf -v cmd " %q" go test -v -coverpkg=./... -coverprofile="/run/output/coverage.txt" -count 1 ./...
+  printf -v cmd " %q" go test -v -failfast -coverpkg=./... -coverprofile="/run/output/coverage.txt" -count 1 ./...
 fi
 
 echo Testing on "${kernel_version}"

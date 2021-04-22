4de7bd3691ee300e483b3433016d5e0ee2492490
Lorenz Bauer <lmb@cloudflare.com>
2021-04-22 14:28:37 +0100

    CI: don't run go tests concurrently
    go test by default compiles and tests multiple packages at once.
    This leads to OOM-kills in our VM if two memory intensive tests
    run simultaneously. Limit the VM to compile and run a single test.

--- run-tests.sh
+++ run-tests.sh
@@ -77,7 +77,7 @@ fi
 if (( $# > 0 )); then
   printf -v cmd " %q" "$@"
 else
-  printf -v cmd " %q" go test -v -coverpkg=./... -coverprofile="/run/output/coverage.txt" -count 1 ./...
+  printf -v cmd " %q" go test -p 1 -v -coverpkg=./... -coverprofile="/run/output/coverage.txt" -count 1 ./...
 fi
 
 echo Testing on "${kernel_version}"

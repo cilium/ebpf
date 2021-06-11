8220043b5deaa6816f3df9f687044c9c2265bb04
Tobias Klauser <tobias@cilium.io>
2021-06-11 15:26:39 +0200

    examples/kprobe: mention sys_execve in package godoc
    Commit 1e7f01c7124c ("arch: support arm64") changed the example to
    attach to sys_execve instead of __x64_sys_execve, but didn't update the
    package godoc accordingly.
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- examples/kprobe/main.go
+++ examples/kprobe/main.go
@@ -1,7 +1,7 @@
 // +build linux
 
 // This program demonstrates attaching an eBPF program to a kernel symbol.
-// The eBPF program will be attached to the start of the
+// The eBPF program will be attached to the start of the sys_execve
 // kernel function and prints out the number of times it has been called
 // every second.
 package main

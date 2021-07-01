cilium/ebpf

commit de57e915ff34190f6633850097196473c7d4ebff

Add *GetNextID
Signed-off-by: Lehner Florian <dev@der-flo.net>

syscalls.go
@@ -11,6 +11,11 @@ import (
 	"golang.org/x/xerrors"
 )
 
+// Generic errors returned by BPF syscalls.
+var (
+	ErrNotExist = xerrors.New("requested object does not exit")
+)
+
 // bpfObjName is a null-terminated string made up of
 // 'A-Za-z0-9_' characters.
 type bpfObjName [unix.BPF_OBJ_NAME_LEN]byte
@@ -148,6 +153,12 @@ type bpfGetFDByIDAttr struct {
 
 type bpfMapFreezeAttr struct {
 	mapFd uint32
+}
+
+type bpfObjGetNextIDAttr struct {
+	startID   uint32
+	nextID    uint32
+	openFlags uint32
 }
 
 func bpfProgLoad(attr *bpfProgLoadAttr) (*internal.FD, error) {
@@ -299,6 +310,25 @@ func bpfMapGetNextKey(m *internal.FD, key, nextKeyOut internal.Pointer) error {
 	}
 	_, err = internal.BPF(_MapGetNextKey, unsafe.Pointer(&attr), unsafe.Sizeof(attr))
 	return wrapMapError(err)
+}
+
+func objGetNextID(cmd int, start uint32) (uint32, error) {
+	attr := bpfObjGetNextIDAttr{
+		startID: start,
+	}
+	_, err := internal.BPF(cmd, unsafe.Pointer(&attr), unsafe.Sizeof(attr))
+	return attr.nextID, wrapObjError(err)
+}
+
+func wrapObjError(err error) error {
+	if err == nil {
+		return nil
+	}
+	if xerrors.Is(err, unix.ENOENT) {
+		return xerrors.Errorf("%w", ErrNotExist)
+	}
+
+	return xerrors.New(err.Error())
 }
 
 func wrapMapError(err error) error {

map.go
@@ -17,6 +17,9 @@ var (
 	ErrIterationAborted = xerrors.New("iteration aborted")
 )
 
+// MapID represents the unique ID of an eBPF map
+type MapID uint32
+
 // MapSpec defines a Map.
 type MapSpec struct {
 	// Name is passed to the kernel as a debug aid. Must only contain
@@ -764,4 +767,12 @@ func (mi *MapIterator) Next(keyOut, valueOut interface{}) bool {
 // Returns ErrIterationAborted if it wasn't possible to do a full iteration.
 func (mi *MapIterator) Err() error {
 	return mi.err
+}
+
+// MapGetNextID returns the ID of the next eBPF map.
+//
+// Returns ErrNotExist, if there is no next eBPF map.
+func MapGetNextID(startID MapID) (MapID, error) {
+	id, err := objGetNextID(_MapGetNextID, uint32(startID))
+	return MapID(id), err
 }

map_test.go
@@ -690,6 +690,37 @@ func TestMapFreeze(t *testing.T) {
 	}
 }
 
+func TestMapGetNextID(t *testing.T) {
+	testutils.SkipOnOldKernel(t, "4.13", "bpf_map_get_next_id")
+	var next MapID
+	var err error
+
+	hash := createHash()
+	defer hash.Close()
+
+	if next, err = MapGetNextID(MapID(0)); err != nil {
+		t.Fatal("Can't get next ID:", err)
+	}
+	if next == MapID(0) {
+		t.Fatal("Expected next ID other than 0")
+	}
+
+	// As there can be multiple eBPF maps, we loop over all of them and
+	// make sure, the IDs increase and the last call will return ErrNotExist
+	for {
+		last := next
+		if next, err = MapGetNextID(last); err != nil {
+			if !xerrors.Is(err, ErrNotExist) {
+				t.Fatal("Expected ErrNotExist, got:", err)
+			}
+			break
+		}
+		if next <= last {
+			t.Fatalf("Expected next ID (%d) to be higher than the last ID (%d)", next, last)
+		}
+	}
+}
+
 type benchValue struct {
 	ID      uint32
 	Val16   uint16

prog.go
@@ -19,6 +19,9 @@ import (
 // ErrNotSupported is returned whenever the kernel doesn't support a feature.
 var ErrNotSupported = internal.ErrNotSupported
 
+// ProgramID represents the unique ID of an eBPF program
+type ProgramID uint32
+
 const (
 	// Number of bytes to pad the output buffer for BPF_PROG_TEST_RUN.
 	// This is currently the maximum of spare space allocated for SKB
@@ -516,4 +519,12 @@ func SanitizeName(name string, replacement rune) string {
 		}
 		return char
 	}, name)
+}
+
+// ProgramGetNextID returns the ID of the next eBPF program.
+//
+// // Returns ErrNotExist, if there is no next eBPF program.
+func ProgramGetNextID(startID ProgramID) (ProgramID, error) {
+	id, err := objGetNextID(_ProgGetNextID, uint32(startID))
+	return ProgramID(id), err
 }

prog_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/cilium/ebpf/asm"
 	"github.com/cilium/ebpf/internal"
 	"github.com/cilium/ebpf/internal/testutils"
+	"golang.org/x/xerrors"
 )
 
 func TestProgramRun(t *testing.T) {
@@ -317,6 +318,46 @@ func TestProgramAlter(t *testing.T) {
 
 func TestHaveProgTestRun(t *testing.T) {
 	testutils.CheckFeatureTest(t, haveProgTestRun)
+}
+
+func TestProgramGetNextID(t *testing.T) {
+	testutils.SkipOnOldKernel(t, "4.13", "bpf_prog_get_next_id")
+	var next ProgramID
+
+	prog, err := NewProgram(&ProgramSpec{
+		Type: SkSKB,
+		Instructions: asm.Instructions{
+			asm.LoadImm(asm.R0, 0, asm.DWord),
+			asm.Return(),
+		},
+		License: "MIT",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer prog.Close()
+
+	if next, err = ProgramGetNextID(ProgramID(0)); err != nil {
+		t.Fatal("Can't get next ID:", err)
+	}
+	if next == ProgramID(0) {
+		t.Fatal("Expected next ID other than 0")
+	}
+
+	// As there can be multiple eBPF programs, we loop over all of them and
+	// make sure, the IDs increase and the last call will return ErrNotExist
+	for {
+		last := next
+		if next, err = ProgramGetNextID(last); err != nil {
+			if !xerrors.Is(err, ErrNotExist) {
+				t.Fatal("Expected ErrNotExist, got:", err)
+			}
+			break
+		}
+		if next <= last {
+			t.Fatalf("Expected next ID (%d) to be higher than the last ID (%d)", next, last)
+		}
+	}
 }
 
 func createProgramArray(t *testing.T) *Map {

types.go
@@ -115,6 +115,7 @@ const (
 	_TaskFDQuery
 	_MapLookupAndDeleteElem
 	_MapFreeze
+	_BTFGetNextID
 )
 
 const (

commit 7cf7600e1518ef25104b3914997574917a1980fc

syscalls: do not conceal BPF_PROG_* syscall errors
The error wrapping code for BPF_PROG_* syscall-related errors would mask
the true source of all underlying syscall errors, which meant that you
couldn't detect several fairly important cases (such as -EACESS and
-EPERM). It seems that this behaviour wasn't intentional (prior to
commit de57e915ff34, the behaviour was to bubble up the syscall error)
and the similar wrapping of BPF_MAP_* errors did bubble up the syscall
error to.

This is needed for runc to be able to detect permission errors due to
SELinux labels blocking certain operations (mainly NewProgramFromID),
and unifies the behaviour for BPF_PROG_* and BPF_MAP_* syscalls.

Fixes: de57e915ff34 ("Add *GetNextID")
Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

syscalls.go
@@ -357,10 +357,9 @@ func wrapObjError(err error) error {
 		return nil
 	}
 	if errors.Is(err, unix.ENOENT) {
-		return fmt.Errorf("%w", ErrNotExist)
+		return ErrNotExist
 	}
-
-	return errors.New(err.Error())
+	return err
 }
 
 func wrapMapError(err error) error {
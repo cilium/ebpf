1d1f8af3c41c3ae5fe882287fae24b274e9f949b
Timo Beckers <timo@isovalent.com>
2021-07-20 14:15:51 +0200

    link: move cgroup2Path to testutils/, clean up mustCgroupFixtures
    Also execute cgroup2Path() in testutils.CreateCgroup(), simplify
    mustCgroupFixtures().
    
    Signed-off-by: Timo Beckers <timo@isovalent.com>

--- internal/testutils/cgroup.go
+++ internal/testutils/cgroup.go
@@ -1,17 +1,59 @@
 package testutils
 
 import (
+	"errors"
 	"io/ioutil"
 	"os"
+	"strings"
+	"sync"
 	"testing"
 
 	"golang.org/x/sys/unix"
 )
 
+var cgroup2 = struct {
+	once sync.Once
+	path string
+	err  error
+}{}
+
+func cgroup2Path() (string, error) {
+	cgroup2.once.Do(func() {
+		mounts, err := ioutil.ReadFile("/proc/mounts")
+		if err != nil {
+			cgroup2.err = err
+			return
+		}
+
+		for _, line := range strings.Split(string(mounts), "\n") {
+			mount := strings.SplitN(line, " ", 3)
+			if mount[0] == "cgroup2" {
+				cgroup2.path = mount[1]
+				return
+			}
+
+			continue
+		}
+
+		cgroup2.err = errors.New("cgroup2 not mounted")
+	})
+
+	if cgroup2.err != nil {
+		return "", cgroup2.err
+	}
+
+	return cgroup2.path, nil
+}
+
 func CreateCgroup(tb testing.TB) *os.File {
 	tb.Helper()
 
-	cgdir, err := ioutil.TempDir("/sys/fs/cgroup/unified", "ebpf-link")
+	cg2, err := cgroup2Path()
+	if err != nil {
+		tb.Fatal("Can't locate cgroup2 mount:", err)
+	}
+
+	cgdir, err := ioutil.TempDir(cg2, "ebpf-link")
 	if err != nil {
 		tb.Fatal("Can't create cgroupv2:", err)
 	}

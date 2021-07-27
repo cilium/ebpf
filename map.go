f615158b37cc11627e0c0a68cefcdf073da71089
Timo Beckers <timo@isovalent.com>
2021-07-27 20:08:10 +0200

    collection: support lazy-populating maps
    Return a new populateMaps from lazyLoadCollection to allow Maps to be populated
    in a separate step after they've been created. This allows Programs and Maps
    to be created first, so their references can be taken for insertion into other
    Maps.
    
    Signed-off-by: Timo Beckers <timo@isovalent.com>

--- map.go
+++ map.go
@@ -195,7 +195,17 @@ func NewMapWithOptions(spec *MapSpec, opts MapOptions) (*Map, error) {
 	handles := newHandleCache()
 	defer handles.close()
 
-	return newMapWithOptions(spec, opts, handles)
+	m, err := newMapWithOptions(spec, opts, handles)
+	if err != nil {
+		return nil, fmt.Errorf("creating map: %w", err)
+	}
+
+	err = m.populate(spec)
+	if err != nil {
+		return nil, fmt.Errorf("populating map: %w", err)
+	}
+
+	return m, nil
 }
 
 func newMapWithOptions(spec *MapSpec, opts MapOptions, handles *handleCache) (_ *Map, err error) {
@@ -261,11 +271,6 @@ func newMapWithOptions(spec *MapSpec, opts MapOptions, handles *handleCache) (_ 
 		return nil, err
 	}
 	defer closeOnError(m)
-
-	err = m.populate(spec)
-	if err != nil {
-		return nil, err
-	}
 
 	if spec.Pinning == PinByName {
 		path := filepath.Join(opts.PinPath, spec.Name)
@@ -899,7 +904,7 @@ func (m *Map) Freeze() error {
 func (m *Map) populate(spec *MapSpec) error {
 	for _, kv := range spec.Contents {
 		if err := m.Put(kv.Key, kv.Value); err != nil {
-			return fmt.Errorf("populating map: key %v: %w", kv.Key, err)
+			return fmt.Errorf("putting value: key %v: %w", kv.Key, err)
 		}
 	}
 

33f3b5d633076bb90f361878b33d7f8c99f01de4
Vlad Ungureanu <vladu@palantir.com>
2021-08-10 10:18:24 -0400

    Use BTF style map pinning
    Signed-off-by: Vlad Ungureanu <vladu@palantir.com>

--- examples/kprobepin/main.go
+++ examples/kprobepin/main.go
@@ -10,7 +10,6 @@ import (
 	"log"
 	"os"
 	"os/signal"
-	"path"
 	"syscall"
 	"time"
 
@@ -22,9 +21,8 @@ import (
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang-11 KProbePinExample ./bpf/kprobe_pin_example.c -- -I../headers
 
 const (
-	mapKey        uint32 = 0
-	kProbeMapName        = "kprobe_map"
-	bpfFSPath            = "/sys/fs/bpf"
+	mapKey    uint32 = 0
+	bpfFSPath        = "/sys/fs/bpf"
 )
 
 func main() {
@@ -45,57 +43,30 @@ func main() {
 		log.Fatalf("failed to set temporary rlimit: %v", err)
 	}
 
-	kProbeMapPath := path.Join(bpfFSPath, kProbeMapName)
-	var kProbeMap *ebpf.Map
-	var kProbeProg *ebpf.Program
-	if _, err := os.Stat(kProbeMapPath); os.IsNotExist(err) {
-		var kProbeObj KProbePinExampleObjects
-		if err := LoadKProbePinExampleObjects(&kProbeObj, nil); err != nil {
-			log.Fatalf("loading objects: %v", err)
-		}
-		defer kProbeObj.Close()
-		if err := kProbeObj.KprobeMap.Pin(kProbeMapPath); err != nil {
-			log.Fatalf("failed to pin map at path %s: %v", kProbeMapPath, err)
-		}
-		kProbeMap = kProbeObj.KprobeMap
-		kProbeProg = kProbeObj.KprobeExecve
-	} else if err == nil {
-		pinnedMap, err := ebpf.LoadPinnedMap(kProbeMapPath, nil)
-		if err != nil {
-			log.Fatalf("failed to load pinned map from path %s: %v", kProbeMapPath, err)
-		}
-		kProbeMap = pinnedMap
-		specs, err := LoadKProbePinExample()
-		if err != nil {
-			log.Fatalf("failed to load specs: %v", err)
-		}
-		if err := specs.RewriteMaps(map[string]*ebpf.Map{
-			kProbeMapName: kProbeMap,
-		}); err != nil {
-			log.Fatalf("failed to rewrite maps: %v", err)
-		}
-		var localProg KProbePinExamplePrograms
-		if err := specs.LoadAndAssign(&localProg, nil); err != nil {
-			log.Fatalf("loading objects: %v", err)
-		}
-		defer localProg.Close()
-		kProbeProg = localProg.KprobeExecve
-	} else {
-		log.Fatalf("failed to check if pinned map exists at path %s: %v", kProbeMapPath, err)
+	var kProbeObj KProbePinExampleObjects
+	if err := LoadKProbePinExampleObjects(&kProbeObj, &ebpf.CollectionOptions{
+		Maps: ebpf.MapOptions{
+			PinPath: bpfFSPath,
+		},
+		Programs: ebpf.ProgramOptions{
+			LogLevel: 1,
+		},
+	}); err != nil {
+		log.Fatalf("loading objects: %v", err)
 	}
 
 	// Open a Kprobe at the entry point of the kernel function and attach the
 	// pre-compiled program. Each time the kernel function enters, the program
 	// will increment the execution counter by 1. The read loop below polls this
 	// map value once per second.
-	kp, err := link.Kprobe(fn, kProbeProg)
+	kp, err := link.Kprobe(fn, kProbeObj.KprobeExecve)
 	if err != nil {
 		log.Fatalf("opening kprobe: %s", err)
 	}
 	defer kp.Close()
 
-	// Read loop reporting the total amount of times the kernel
-	// function was entered, once per second.
+	// // Read loop reporting the total amount of times the kernel
+	// // function was entered, once per second.
 	ticker := time.NewTicker(1 * time.Second)
 
 	log.Println("Waiting for events..")
@@ -104,7 +75,7 @@ func main() {
 		select {
 		case <-ticker.C:
 			var value uint64
-			if err := kProbeMap.Lookup(mapKey, &value); err != nil {
+			if err := kProbeObj.KprobeMap.Lookup(mapKey, &value); err != nil {
 				log.Fatalf("reading map: %v", err)
 			}
 			log.Printf("%s called %d times\n", fn, value)

f3383200891e4a05a6700b693ca1e9c8006ec3cc
folbrich <frank.olbricht@gmail.com>
2021-08-31 18:44:47 -0600

    Return a function to reset the limit

--- process.go
+++ process.go
@@ -7,13 +7,25 @@ import (
 )
 
 // RemoveMemlockRlimit removes the limit on the amount of memory
-// the process can lock into RAM.
-func RemoveMemlockRlimit() error {
+// the process can lock into RAM. Returns a function that restores
+// the limit to its previous value.
+func RemoveMemlockRlimit() (func() error, error) {
+	oldLimit := new(unix.Rlimit)
+	if err := unix.Getrlimit(unix.RLIMIT_MEMLOCK, oldLimit); err != nil {
+		return nil, fmt.Errorf("failed to get memlock rlimit: %w", err)
+	}
+
 	if err := unix.Setrlimit(unix.RLIMIT_MEMLOCK, &unix.Rlimit{
 		Cur: unix.RLIM_INFINITY,
 		Max: unix.RLIM_INFINITY,
 	}); err != nil {
-		return fmt.Errorf("failed to remove memlock rlimit: %w", err)
+		return nil, fmt.Errorf("failed to set memlock rlimit: %w", err)
 	}
-	return nil
+
+	return func() error {
+		if err := unix.Setrlimit(unix.RLIMIT_MEMLOCK, oldLimit); err != nil {
+			return fmt.Errorf("failed to reset memlock rlimit: %w", err)
+		}
+		return nil
+	}, nil
 }

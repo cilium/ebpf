cilium/ebpf

commit af50a38dd5c85581f4082d41d2fd4d418a8c0ba4

ringbuf: add new package to read from BPF ring buffers
Linux 5.8 added support for BPF ring buffer that addresses some
limitations with perf events when delivering variable size records
from BPF programs to user space [1].

Similar to and based on the 'perf' package, 'ringbuf' package implements
an io.Reader to read such records submitted by bpf_ringbuf_output()/
bpf_ringbuf_submit().

[1] https://www.kernel.org/doc/html/latest/bpf/ringbuf.html

Signed-off-by: Mikko Ylinen <mikko.ylinen@intel.com>

ringbuf/reader.go
@@ -1,0 +1,257 @@
+package ringbuf
+
+import (
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+	"runtime"
+	"sync"
+
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/internal"
+	"github.com/cilium/ebpf/internal/unix"
+)
+
+var (
+	ErrClosed  = errors.New("ringbuf reader was closed")
+	errDiscard = errors.New("sample discarded")
+	errBusy    = errors.New("sample not committed yet")
+)
+
+func addToEpoll(epollfd, fd int) error {
+
+	event := unix.EpollEvent{
+		Events: unix.EPOLLIN,
+		Fd:     int32(fd),
+	}
+
+	if err := unix.EpollCtl(epollfd, unix.EPOLL_CTL_ADD, fd, &event); err != nil {
+		return fmt.Errorf("can't add fd to epoll: %w", err)
+	}
+	return nil
+}
+
+// ringbufHeader from 'struct bpf_ringbuf_hdr' in kernel/bpf/ringbuf.c
+type ringbufHeader struct {
+	Len   uint32
+	PgOff uint32
+}
+
+func (rh *ringbufHeader) isBusy() bool {
+	return rh.Len&unix.BPF_RINGBUF_BUSY_BIT != 0
+}
+
+func (rh *ringbufHeader) isDiscard() bool {
+	return rh.Len&unix.BPF_RINGBUF_DISCARD_BIT != 0
+}
+
+func (rh *ringbufHeader) dataLen() int {
+	return int(rh.Len & ^uint32(unix.BPF_RINGBUF_BUSY_BIT|unix.BPF_RINGBUF_DISCARD_BIT))
+}
+
+type Record struct {
+	RawSample []byte
+}
+
+func readRecord(rd *ringbufEventRing) (r Record, err error) {
+	rd.loadConsumer()
+	var header ringbufHeader
+	err = binary.Read(rd, internal.NativeEndian, &header)
+	if err == io.EOF {
+		return Record{}, err
+	}
+
+	if err != nil {
+		return Record{}, fmt.Errorf("can't read event header: %w", err)
+	}
+
+	if header.isBusy() {
+		// the next sample in the ring is not committed yet so we
+		// exit without storing the reader/consumer position
+		// and start again from the same position.
+		return Record{}, fmt.Errorf("%w", errBusy)
+	}
+
+	/* read up to 8 byte alignment */
+	dataLenAligned := uint64(internal.Align(header.dataLen(), 8))
+
+	if header.isDiscard() {
+		// when the record header indicates that the data should be
+		// discarded, we skip it by just updating the consumer position
+		// to the next record instead of normal Read() to avoid allocating data
+		// and reading/copying from the ring (which normally keeps track of the
+		// consumer position).
+		rd.skipRead(dataLenAligned)
+		rd.storeConsumer()
+
+		return Record{}, fmt.Errorf("%w", errDiscard)
+	}
+
+	data := make([]byte, dataLenAligned)
+
+	if _, err := io.ReadFull(rd, data); err != nil {
+		return Record{}, fmt.Errorf("can't read sample: %w", err)
+	}
+
+	rd.storeConsumer()
+
+	return Record{RawSample: data[:header.dataLen()]}, nil
+}
+
+// Reader allows reading bpf_ringbuf_output
+// from user space.
+type Reader struct {
+	// mu protects read/write access to the Reader structure
+	mu sync.Mutex
+
+	ring *ringbufEventRing
+
+	epollFd     int
+	epollEvents []unix.EpollEvent
+
+	closeFd int
+	// Ensure we only close once
+	closeOnce sync.Once
+}
+
+// NewReader creates a new BPF ringbuf reader.
+func NewReader(ringbufMap *ebpf.Map) (r *Reader, err error) {
+	if ringbufMap.Type() != ebpf.RingBuf {
+		return nil, fmt.Errorf("invalid Map type: %s", ringbufMap.Type())
+	}
+
+	epollFd, err := unix.EpollCreate1(unix.EPOLL_CLOEXEC)
+	if err != nil {
+		return nil, fmt.Errorf("can't create epoll fd: %w", err)
+	}
+
+	var (
+		fds        = []int{epollFd}
+		maxEntries = int(ringbufMap.MaxEntries())
+		ring       *ringbufEventRing
+	)
+
+	if maxEntries == 0 || (maxEntries&(maxEntries-1)) != 0 {
+		return nil, fmt.Errorf("Ringbuffer map size %d is zero or not a power of two", maxEntries)
+	}
+	defer func() {
+		if err != nil {
+			// close epollFd and closeFd
+			for _, fd := range fds {
+				unix.Close(fd)
+			}
+			if ring != nil {
+				ring.Close()
+			}
+		}
+	}()
+
+	ring, err = newRingBufEventRing(ringbufMap.FD(), maxEntries)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create ringbuf ring: %w", err)
+	}
+
+	if err := addToEpoll(epollFd, ringbufMap.FD()); err != nil {
+		return nil, err
+	}
+
+	closeFd, err := unix.Eventfd(0, unix.O_CLOEXEC|unix.O_NONBLOCK)
+	if err != nil {
+		return nil, err
+	}
+	fds = append(fds, closeFd)
+
+	if err := addToEpoll(epollFd, closeFd); err != nil {
+		return nil, err
+	}
+
+	r = &Reader{
+		ring:    ring,
+		epollFd: epollFd,
+		// Allocate extra event for closeFd
+		epollEvents: make([]unix.EpollEvent, 2),
+		closeFd:     closeFd,
+	}
+	runtime.SetFinalizer(r, (*Reader).Close)
+	return r, nil
+}
+
+// Close frees resources used by the reader.
+//
+// It interrupts calls to Read.
+func (r *Reader) Close() error {
+	var err error
+	r.closeOnce.Do(func() {
+		runtime.SetFinalizer(r, nil)
+
+		// Interrupt Read() via the closeFd event fd.
+		var value [8]byte
+		internal.NativeEndian.PutUint64(value[:], 1)
+
+		if _, err = unix.Write(r.closeFd, value[:]); err != nil {
+			err = fmt.Errorf("can't write event fd: %w", err)
+			return
+		}
+
+		// Acquire the lock. This ensures that Read isn't running.
+		r.mu.Lock()
+		defer r.mu.Unlock()
+
+		unix.Close(r.epollFd)
+		unix.Close(r.closeFd)
+		r.epollFd, r.closeFd = -1, -1
+
+		if r.ring != nil {
+			r.ring.Close()
+		}
+		r.ring = nil
+
+	})
+	if err != nil {
+		return fmt.Errorf("close Reader: %w", err)
+	}
+	return nil
+}
+
+// Read the next record from the BPF ringbuf.
+//
+// Calling Close interrupts the function.
+func (r *Reader) Read() (Record, error) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	if r.epollFd == -1 {
+		return Record{}, fmt.Errorf("%w", ErrClosed)
+	}
+
+	for {
+		nEvents, err := unix.EpollWait(r.epollFd, r.epollEvents, -1)
+		if temp, ok := err.(temporaryError); ok && temp.Temporary() {
+			// Retry the syscall if we we're interrupted, see https://github.com/golang/go/issues/20400
+			continue
+		}
+
+		if err != nil {
+			return Record{}, err
+		}
+
+		for _, event := range r.epollEvents[:nEvents] {
+			if int(event.Fd) == r.closeFd {
+				return Record{}, fmt.Errorf("%w", ErrClosed)
+			}
+		}
+
+		record, err := readRecord(r.ring)
+
+		if errors.Is(err, errBusy) || errors.Is(err, errDiscard) {
+			continue
+		}
+
+		return record, err
+	}
+}
+
+type temporaryError interface {
+	Temporary() bool
+}

internal/unix/types_linux.go
@@ -38,6 +38,9 @@ const (
 	BPF_F_INNER_MAP          = linux.BPF_F_INNER_MAP
 	BPF_OBJ_NAME_LEN         = linux.BPF_OBJ_NAME_LEN
 	BPF_TAG_SIZE             = linux.BPF_TAG_SIZE
+	BPF_RINGBUF_BUSY_BIT     = linux.BPF_RINGBUF_BUSY_BIT
+	BPF_RINGBUF_DISCARD_BIT  = linux.BPF_RINGBUF_DISCARD_BIT
+	BPF_RINGBUF_HDR_SZ       = linux.BPF_RINGBUF_HDR_SZ
 	SYS_BPF                  = linux.SYS_BPF
 	F_DUPFD_CLOEXEC          = linux.F_DUPFD_CLOEXEC
 	EPOLL_CTL_ADD            = linux.EPOLL_CTL_ADD

internal/unix/types_other.go
@@ -37,6 +37,9 @@ const (
 	BPF_F_INNER_MAP          = 0
 	BPF_OBJ_NAME_LEN         = 0x10
 	BPF_TAG_SIZE             = 0x8
+	BPF_RINGBUF_BUSY_BIT     = 0
+	BPF_RINGBUF_DISCARD_BIT  = 0
+	BPF_RINGBUF_HDR_SZ       = 0
 	SYS_BPF                  = 321
 	F_DUPFD_CLOEXEC          = 0x406
 	EPOLLIN                  = 0x1

perf/ring_test.go
@@ -46,7 +46,7 @@ func TestRingBufferReader(t *testing.T) {
 }
 
 func makeRing(size, offset int) *ringReader {
-	if size%2 != 0 {
+	if size != 0 && (size&(size-1)) != 0 {
 		panic("size must be power of two")
 	}
 

ringbuf/doc.go
@@ -1,0 +1,6 @@
+// Package ringbuf allows interacting with Linux BPF ring buffer.
+//
+// BPF allows submitting custom events to a BPF ring buffer map set up
+// by userspace. This is very useful to push things like packet samples
+// from BPF to a daemon running in user space.
+package ringbuf

ringbuf/reader_test.go
@@ -1,0 +1,254 @@
+package ringbuf
+
+import (
+	"errors"
+	"syscall"
+	"testing"
+	"time"
+
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/asm"
+	"github.com/cilium/ebpf/internal/testutils"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestRingbufReader(t *testing.T) {
+	testutils.SkipOnOldKernel(t, "5.8", "BPF ring buffer")
+
+	readerTests := []struct {
+		name     string
+		messages []int
+		want     map[int][]byte
+	}{
+		{
+			name:     "send one short sample",
+			messages: []int{5},
+			want: map[int][]byte{
+				5: {1, 2, 3, 4, 4},
+			},
+		},
+		{
+			name:     "send three short samples, the second is discarded",
+			messages: []int{5, 10, 15},
+			want: map[int][]byte{
+				5:  {1, 2, 3, 4, 4},
+				15: {1, 2, 3, 4, 4, 3, 2, 1, 1, 2, 3, 4, 4, 3, 2},
+			},
+		},
+	}
+	for _, tt := range readerTests {
+		t.Run(tt.name, func(t *testing.T) {
+			prog, events := mustOutputSamplesProg(t, 0, tt.messages...)
+
+			rd, err := NewReader(events)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer rd.Close()
+
+			ret, _, err := prog.Test(make([]byte, 14))
+			testutils.SkipIfNotSupported(t, err)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if errno := syscall.Errno(-int32(ret)); errno != 0 {
+				t.Fatal("Expected 0 as return value, got", errno)
+			}
+
+			raw := make(map[int][]byte)
+
+			for len(raw) < len(tt.want) {
+				record, err := rd.Read()
+				if err != nil {
+					t.Fatal("Can't read samples:", err)
+				}
+				raw[len(record.RawSample)] = record.RawSample
+			}
+
+			if diff := cmp.Diff(tt.want, raw); diff != "" {
+				t.Errorf("Read samples mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func outputSamplesProg(flags int32, sampleSizes ...int) (*ebpf.Program, *ebpf.Map, error) {
+	events, err := ebpf.NewMap(&ebpf.MapSpec{
+		Type:       ebpf.RingBuf,
+		MaxEntries: 4096,
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var maxSampleSize int
+	for _, sampleSize := range sampleSizes {
+		if sampleSize > maxSampleSize {
+			maxSampleSize = sampleSize
+		}
+	}
+
+	insns := asm.Instructions{
+		asm.LoadImm(asm.R0, 0x0102030404030201, asm.DWord),
+		asm.Mov.Reg(asm.R9, asm.R1),
+	}
+
+	bufDwords := (maxSampleSize / 8) + 1
+	for i := 0; i < bufDwords; i++ {
+		insns = append(insns,
+			asm.StoreMem(asm.RFP, int16(i+1)*-8, asm.R0, asm.DWord),
+		)
+	}
+
+	for sampleIdx, sampleSize := range sampleSizes {
+		insns = append(insns,
+			asm.LoadMapPtr(asm.R1, events.FD()),
+			asm.Mov.Imm(asm.R2, int32(sampleSize)),
+			asm.Mov.Imm(asm.R3, int32(0)),
+			asm.FnRingbufReserve.Call(),
+			asm.JEq.Imm(asm.R0, 0, "exit"),
+			asm.Mov.Reg(asm.R5, asm.R0),
+		)
+		for i := 0; i < sampleSize; i++ {
+			insns = append(insns,
+				asm.LoadMem(asm.R4, asm.RFP, int16(i+1)*-1, asm.Byte),
+				asm.StoreMem(asm.R5, int16(i), asm.R4, asm.Byte),
+			)
+		}
+
+		// discard every even sample
+		if sampleIdx&1 != 0 {
+			insns = append(insns,
+				asm.Mov.Reg(asm.R1, asm.R5),
+				asm.Mov.Imm(asm.R2, flags),
+				asm.FnRingbufDiscard.Call(),
+			)
+		} else {
+			insns = append(insns,
+				asm.Mov.Reg(asm.R1, asm.R5),
+				asm.Mov.Imm(asm.R2, flags),
+				asm.FnRingbufSubmit.Call(),
+			)
+		}
+	}
+
+	insns = append(insns,
+		asm.Mov.Imm(asm.R0, int32(0)).Sym("exit"),
+		asm.Return(),
+	)
+
+	prog, err := ebpf.NewProgram(&ebpf.ProgramSpec{
+		License:      "MIT",
+		Type:         ebpf.XDP,
+		Instructions: insns,
+	})
+	if err != nil {
+		events.Close()
+		return nil, nil, err
+	}
+
+	return prog, events, nil
+}
+
+func mustOutputSamplesProg(tb testing.TB, flags int32, sampleSizes ...int) (*ebpf.Program, *ebpf.Map) {
+	tb.Helper()
+
+	prog, events, err := outputSamplesProg(flags, sampleSizes...)
+	if err != nil {
+		tb.Fatal(err)
+	}
+
+	tb.Cleanup(func() {
+		prog.Close()
+		events.Close()
+	})
+
+	return prog, events
+}
+
+func TestRingbufReaderClose(t *testing.T) {
+	testutils.SkipOnOldKernel(t, "5.8", "BPF ring buffer")
+
+	_, events := mustOutputSamplesProg(t, 0, 5)
+
+	rd, err := NewReader(events)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer rd.Close()
+
+	errs := make(chan error, 1)
+	waiting := make(chan struct{})
+	go func() {
+		close(waiting)
+		_, err := rd.Read()
+		errs <- err
+	}()
+
+	<-waiting
+
+	// Close should interrupt blocking Read
+	if err := rd.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	select {
+	case err := <-errs:
+		if !errors.Is(err, ErrClosed) {
+			t.Fatal("Read from RingbufReader that got closed does return ErrClosed")
+		}
+	case <-time.After(time.Second):
+		t.Fatal("Close doesn't interrupt Read")
+	}
+
+	// And we should be able to call it multiple times
+	if err := rd.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err := rd.Read(); !errors.Is(err, ErrClosed) {
+		t.Fatal("Second Read on a closed RingbufReader doesn't return ErrClosed")
+	}
+}
+
+func BenchmarkReader(b *testing.B) {
+	testutils.SkipOnOldKernel(b, "5.8", "BPF ring buffer")
+
+	readerBenchmarks := []struct {
+		name  string
+		flags int32
+	}{
+		{
+			name: "normal epoll with timeout -1",
+		},
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for _, bm := range readerBenchmarks {
+		b.Run(bm.name, func(b *testing.B) {
+			prog, events := mustOutputSamplesProg(b, bm.flags, 80)
+
+			rd, err := NewReader(events)
+			if err != nil {
+				b.Fatal(err)
+			}
+			defer rd.Close()
+
+			for i := 0; i < b.N; i++ {
+				ret, _, err := prog.Benchmark(make([]byte, 14), 1, nil)
+				if err != nil {
+					b.Fatal(err)
+				} else if errno := syscall.Errno(-int32(ret)); errno != 0 {
+					b.Fatal("Expected 0 as return value, got", errno)
+				}
+				_, err = rd.Read()
+				if err != nil {
+					b.Fatal("Can't read samples:", err)
+				}
+			}
+		})
+	}
+}

ringbuf/ring.go
@@ -1,0 +1,109 @@
+package ringbuf
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"runtime"
+	"sync/atomic"
+	"unsafe"
+
+	"github.com/cilium/ebpf/internal/unix"
+)
+
+type ringbufEventRing struct {
+	prod []byte
+	cons []byte
+	*ringReader
+}
+
+func newRingBufEventRing(mapFD, size int) (*ringbufEventRing, error) {
+	cons, err := unix.Mmap(mapFD, 0, os.Getpagesize(), unix.PROT_READ|unix.PROT_WRITE, unix.MAP_SHARED)
+	if err != nil {
+		return nil, fmt.Errorf("can't mmap consumer page: %w", err)
+	}
+
+	prod, err := unix.Mmap(mapFD, (int64)(os.Getpagesize()), os.Getpagesize()+2*size, unix.PROT_READ, unix.MAP_SHARED)
+	if err != nil {
+		_ = unix.Munmap(cons)
+		return nil, fmt.Errorf("can't mmap data pages: %w", err)
+	}
+
+	cons_pos := (*uint64)(unsafe.Pointer(&cons[0]))
+	prod_pos := (*uint64)(unsafe.Pointer(&prod[0]))
+
+	ring := &ringbufEventRing{
+		prod:       prod,
+		cons:       cons,
+		ringReader: newRingReader(cons_pos, prod_pos, prod[os.Getpagesize():]),
+	}
+	runtime.SetFinalizer(ring, (*ringbufEventRing).Close)
+
+	return ring, nil
+}
+
+func (ring *ringbufEventRing) Close() {
+	runtime.SetFinalizer(ring, nil)
+
+	_ = unix.Munmap(ring.prod)
+	_ = unix.Munmap(ring.cons)
+
+	ring.prod = nil
+	ring.cons = nil
+}
+
+type ringReader struct {
+	// These point into mmap'ed memory and must be accessed atomically.
+	prod_pos, cons_pos *uint64
+	cons               uint64
+	mask               uint64
+	ring               []byte
+}
+
+func newRingReader(cons_ptr, prod_ptr *uint64, ring []byte) *ringReader {
+	return &ringReader{
+		prod_pos: prod_ptr,
+		cons_pos: cons_ptr,
+		cons:     atomic.LoadUint64(cons_ptr),
+		// cap is always a power of two
+		mask: uint64(cap(ring)/2 - 1),
+		ring: ring,
+	}
+}
+
+func (rr *ringReader) loadConsumer() {
+	rr.cons = atomic.LoadUint64(rr.cons_pos)
+}
+
+func (rr *ringReader) storeConsumer() {
+	atomic.StoreUint64(rr.cons_pos, rr.cons)
+}
+
+// clamp delta to 'end' if 'start+delta' is beyond 'end'
+func clamp(start, end, delta uint64) uint64 {
+	if remainder := end - start; delta > remainder {
+		return remainder
+	}
+	return delta
+}
+
+func (rr *ringReader) skipRead(skipBytes uint64) {
+	rr.cons += clamp(rr.cons, atomic.LoadUint64(rr.prod_pos), skipBytes)
+}
+
+func (rr *ringReader) Read(p []byte) (int, error) {
+	prod := atomic.LoadUint64(rr.prod_pos)
+
+	n := clamp(rr.cons, prod, uint64(len(p)))
+
+	start := rr.cons & rr.mask
+
+	copy(p, rr.ring[start:start+n])
+	rr.cons += n
+
+	if prod == rr.cons {
+		return int(n), io.EOF
+	}
+
+	return int(n), nil
+}

ringbuf/ring_test.go
@@ -1,0 +1,67 @@
+package ringbuf
+
+import (
+	"bytes"
+	"io"
+	"testing"
+)
+
+func TestRingBufferReader(t *testing.T) {
+	buf := make([]byte, 2)
+
+	ring := makeRing(2, 0)
+	n, err := ring.Read(buf)
+	if err != io.EOF {
+		t.Error("Expected io.EOF, got", err)
+	}
+	if n != 2 {
+		t.Errorf("Expected to read 2 bytes, got %d", n)
+	}
+	if !bytes.Equal(buf, []byte{0, 1}) {
+		t.Error("Expected [0, 1], got", buf)
+	}
+	n, err = ring.Read(buf)
+	if err != io.EOF {
+		t.Error("Expected io.EOF, got", err)
+	}
+	if n != 0 {
+		t.Error("Expected to read 0 bytes, got", n)
+	}
+
+	buf = make([]byte, 4)
+
+	ring = makeRing(4, 4)
+	n, err = io.ReadFull(ring, buf)
+	if err != nil {
+		t.Error("Expected nil, got", err)
+	}
+	if n != 4 {
+		t.Errorf("Expected to read 4 bytes, got %d", n)
+	}
+	if !bytes.Equal(buf, []byte{0, 1, 2, 3}) {
+		t.Error("Expected [0, 1, 2, 3], got", buf)
+	}
+	n, err = ring.Read(buf)
+	if err != io.EOF {
+		t.Error("Expected io.EOF, got", err)
+	}
+	if n != 0 {
+		t.Error("Expected to read 0 bytes, got", n)
+	}
+}
+
+func makeRing(size, offset int) *ringReader {
+	if size != 0 && (size&(size-1)) != 0 {
+		panic("size must be power of two")
+	}
+
+	ring := make([]byte, 2*size)
+	for i := range ring {
+		ring[i] = byte(i)
+	}
+
+	consumer := uint64(offset)
+	producer := uint64(len(ring)/2 + offset)
+
+	return newRingReader(&consumer, &producer, ring)
+}

commit ee5b32556eef5f2c451e2c61ce8c1156d6cdfc66

ringbuf: avoid leaking epoll FD on invalid map size in NewReader
If the ringbuffer map size is zero or not a power of two,
ringbuf.NewReader would leak the epoll file descriptor it just created.

Fix this by moving the ringbuffer map size check before the call to
unix.EpollCreate1.

Fixes: af50a38dd5c8 ("ringbuf: add new package to read from BPF ring buffers")

Signed-off-by: Tobias Klauser <tobias@cilium.io>

ringbuf/reader.go
@@ -121,20 +121,21 @@ func NewReader(ringbufMap *ebpf.Map) (r *Reader, err error) {
 		return nil, fmt.Errorf("invalid Map type: %s", ringbufMap.Type())
 	}
 
+	maxEntries := int(ringbufMap.MaxEntries())
+	if maxEntries == 0 || (maxEntries&(maxEntries-1)) != 0 {
+		return nil, fmt.Errorf("Ringbuffer map size %d is zero or not a power of two", maxEntries)
+	}
+
 	epollFd, err := unix.EpollCreate1(unix.EPOLL_CLOEXEC)
 	if err != nil {
 		return nil, fmt.Errorf("can't create epoll fd: %w", err)
 	}
 
 	var (
-		fds        = []int{epollFd}
-		maxEntries = int(ringbufMap.MaxEntries())
-		ring       *ringbufEventRing
+		fds  = []int{epollFd}
+		ring *ringbufEventRing
 	)
 
-	if maxEntries == 0 || (maxEntries&(maxEntries-1)) != 0 {
-		return nil, fmt.Errorf("Ringbuffer map size %d is zero or not a power of two", maxEntries)
-	}
 	defer func() {
 		if err != nil {
 			// close epollFd and closeFd
7ffb5fa3fb26eedad6eca2ef140682bafb7c740b
Nicolas PLANEL <nicolas.planel@datadoghq.com>
2021-09-23 19:15:11 +0200

    [ebpf/map] add map.Iterate() support for old kernel (< 4.4.132)
    Safe map.Iterate() to use with old kernel by guessing the first key (randomly)
    
    The idea is the following on old kernel (< 4.4.132) :
    * we know that if 2 random key return the same values then it mean we found the 1st element of the map to iterate
    * if one of the key match an element in the map, we try a 3rd random key to decide if we found the 1st element
    * we fallback an error if all 3 random keys doesn't match
    * the cost is 2/3 more map.lookup() calls on the first iteration

--- map_test.go
+++ map_test.go
@@ -928,11 +928,16 @@ func TestNotExist(t *testing.T) {
 	}
 
 	if err := hash.Delete("hello"); !errors.Is(err, ErrKeyNotExist) {
-		t.Error("Deleting unknown key doesn't return ErrKeyNotExist")
+		t.Error("Deleting unknown key doesn't return ErrKeyNotExist", err)
+	}
+
+	var k = []byte{1, 2, 3, 4, 5}
+	if err := hash.NextKey(&k, &tmp); !errors.Is(err, ErrKeyNotExist) {
+		t.Error("Looking up next key in empty map doesn't return a non-existing error", err)
 	}
 
 	if err := hash.NextKey(nil, &tmp); !errors.Is(err, ErrKeyNotExist) {
-		t.Error("Looking up next key in empty map doesn't return a non-existing error")
+		t.Error("Looking up next key in empty map doesn't return a non-existing error", err)
 	}
 }
 

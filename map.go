65f574be7c3a75275de7ca76862a9a38cce8cc00
Nicolas PLANEL <nicolas.planel@datadoghq.com>
2021-09-23 19:15:11 +0200

    [ebpf/map] add map.Iterate() support for old kernel (< 4.4.132)
    Safe map.Iterate() to use with old kernel by guessing the first key (randomly)
    
    Thanks to a trick (map.lookup(key, invalidPointer)) of iovisor/bcc we can quickly test if a key exist or not in the map.
    If the key is unknown map.GetNextKeyBytes() will return the first to key that we can iterate to.
    
    We the first key we try is empty (all zero bytes)
    if the key exist then we fallback by guessing by using pseudo random key bytes.
    
    If after 3 tentatives (all zero + 2 randoms) via map.lookup(key)
    we hit 3 differents key then we have no luck and return the error ErrKeyNotExist.
    
    iovisor/bcc trick : https://github.com/iovisor/bcc/blob/c73d7d8901fdac99510a94a3b9ff5ebf2d8f5956/src/cc/libbpf.c#L402

--- map.go
+++ map.go
@@ -5,9 +5,11 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"math/rand"
 	"path/filepath"
 	"reflect"
 	"strings"
+	"time"
 	"unsafe"
 
 	"github.com/cilium/ebpf/internal"
@@ -18,6 +20,7 @@ import (
 
 // Errors returned by Map and MapIterator methods.
 var (
+	ErrFirstKeyNotFound = errors.New("first key not found")
 	ErrKeyNotExist      = errors.New("key does not exist")
 	ErrKeyExist         = errors.New("key already exists")
 	ErrIterationAborted = errors.New("iteration aborted")
@@ -486,6 +489,48 @@ func (m *Map) Info() (*MapInfo, error) {
 	return newMapInfoFromFd(m.fd)
 }
 
+func (m *Map) guessNonExistingKey() (startKey sys.Pointer, err error) {
+	// (idea from iovisor/bcc) fast lookup the key by passing an invalid value pointer
+	// to validate the key, if the key doesn't exist, it's a valid first key to map iterate (on < 4.4.132 kernel)
+	valuePtr := sys.NewPointer(unsafe.Pointer(^uintptr(0)))
+	randKey := make([]byte, int(m.keySize))
+
+	// First (unknown) key as an array will be 0xff...
+	if m.typ == Array {
+		for r := range randKey {
+			randKey[r] = 0xff
+		}
+		return sys.NewSlicePointer(randKey), nil
+	}
+
+	for i := 0; i < 4; i++ {
+		// let's try ...
+		switch i {
+		case 0:
+			// empty (all zero) key
+		case 1:
+			// all 0xff key
+			for r := range randKey {
+				randKey[r] = 0xff
+			}
+		case 2:
+			// all 0x55 key like iovisor/bcc
+			for r := range randKey {
+				randKey[r] = 0x55
+			}
+		case 3:
+			// random key at last hope
+			rand.New(rand.NewSource(time.Now().UnixNano())).Read(randKey)
+		}
+
+		err := m.lookup(randKey, valuePtr)
+		if errors.Is(err, ErrKeyNotExist) {
+			return sys.NewSlicePointer(randKey), nil
+		}
+	}
+	return sys.Pointer{}, ErrFirstKeyNotFound
+}
+
 // Lookup retrieves a value from a Map.
 //
 // Calls Close() on valueOut if it is of type **Map or **Program,
@@ -683,7 +728,12 @@ func (m *Map) nextKey(key interface{}, nextKeyOut sys.Pointer) error {
 
 	if err = sys.MapGetNextKey(&attr); err != nil {
 		if key == nil && errors.Is(err, unix.EFAULT) {
-			attr.Key = sys.NewSlicePointer(make([]byte, int(m.keySize)))
+			var guessKey sys.Pointer
+			guessKey, err = m.guessNonExistingKey()
+			if err != nil {
+				return fmt.Errorf("can't guess starting key: %w", err)
+			}
+			attr.Key = guessKey
 			if err = sys.MapGetNextKey(&attr); err == nil {
 				return nil
 			}

71c0935ca7cc57adc3c500306f9d2e5763aece65
Minglei Wang <sudomw@gmail.com>
2021-10-27 13:49:17 -0700

    add comments

--- examples/cgroup_skb/main.go
+++ examples/cgroup_skb/main.go
@@ -47,10 +47,14 @@ func main() {
 	}
 	defer objs.Close()
 
+	// Find the count_egress_packets program and the BPF VFS path where the
+	// program will persist.
 	bpfProg := objs.CountEgressPackets
 	pinPath := filepath.Join(bpfFS, bpfProgName)
+	// Pin the program. Make sure the Close is being called.
 	bpfProg.Pin(pinPath)
 
+	// Get root cgroup file discriptor.
 	cgroup, err := os.Open(cgroupFS)
 	if err != nil {
 		log.Fatal(err)
@@ -58,6 +62,7 @@ func main() {
 	}
 	defer cgroup.Close()
 
+	// Link the count_egress_packets program to the cgroup.
 	_, err = link.AttachCgroup(link.CgroupOptions{
 		Path:    cgroup.Name(),
 		Attach:  ebpf.AttachCGroupInetIngress,
@@ -67,6 +72,7 @@ func main() {
 		log.Fatal(err)
 		return
 	}
+	// Unloads the program.
 	defer bpfProg.Close()
 
 	// Read loop reporting the total amount of times the kernel

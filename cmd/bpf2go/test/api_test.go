e90c058f9f63c52efff547f3448ac172a06b1df9
Lorenz Bauer <lmb@cloudflare.com>
2021-11-23 17:30:26 +0000

    cmd/bpf2go: generate types from BTF
    Use GoFormatter to generate type definitions for types used as map keys
    and values. Allow users to emit additional types via a command line flag.

--- cmd/bpf2go/test/api_test.go
+++ cmd/bpf2go/test/api_test.go
@@ -1,7 +1,9 @@
 package test
 
 import (
+	"reflect"
 	"testing"
+	"unsafe"
 
 	// Raise RLIMIT_MEMLOCK
 	_ "github.com/cilium/ebpf/internal/testutils"
@@ -31,5 +33,33 @@ func TestLoadingObjects(t *testing.T) {
 
 	if objs.Map1 == nil {
 		t.Error("Loading returns an object with nil maps")
+	}
+}
+
+func TestTypes(t *testing.T) {
+	if testEHOOPY != 0 {
+		t.Error("Expected testEHOOPY to be 0, got", testEHOOPY)
+	}
+	if testEFROOD != 1 {
+		t.Error("Expected testEFROOD to be 0, got", testEFROOD)
+	}
+
+	e := testE(0)
+	if size := unsafe.Sizeof(e); size != 4 {
+		t.Error("Expected size of exampleE to be 4, got", size)
+	}
+
+	bf := testBarfoo{}
+	if size := unsafe.Sizeof(bf); size != 16 {
+		t.Error("Expected size of exampleE to be 16, got", size)
+	}
+	if reflect.TypeOf(bf.Bar).Kind() != reflect.Int64 {
+		t.Error("Expected testBarfoo.Bar to be int64")
+	}
+	if reflect.TypeOf(bf.Baz).Kind() != reflect.Bool {
+		t.Error("Expected testBarfoo.Baz to be bool")
+	}
+	if reflect.TypeOf(bf.Boo) != reflect.TypeOf(e) {
+		t.Error("Expected testBarfoo.Boo to be exampleE")
 	}
 }

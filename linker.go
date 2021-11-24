b49f0625d2dcfe662b090cdad44cad1fe3610f45
Timo Beckers <timo@isovalent.com>
2021-11-24 17:08:20 +0100

    elf_reader,btf: support multiple programs per ELF section
    This patch adds support for emitting multiple eBPF functions/programs
    to the same ELF section.
    
    Significant changes were necessary, both to the BPF linker and the BTF
    ext_info parser, as previously most of the code (rightfully) made the
    assumption that one ELF section equaled one BPF function. In order to
    remove this limitation, all 'offset' logic that used to track positions
    within an ELF section had to be modified to track positions within a
    function body instead.
    
    Secondly, in order to reassemble instruction streams and BTF func_infos,
    line_infos and also CO-RE relocations back into a flat format the kernel
    expects, the linking logic had to be broken up into smaller pieces.
    This allows the instruction linker and the BTF extinfo linkers to
    operate on the same source of truth, so they can generate their outputs
    using the same program layout. Additionally, by computing neighbours
    (which requires scanning all progs' instruction streams for references
    to other programs) only once during ELF loading, any marshaling logic
    can simply request the flattened program layout, saving cycles.
    
    To simplify the neighbour discovery process, the internal distinction
    between 'libs' and 'progs' has been eliminated. This allows any function
    to be called from anywhere, regardless of section name. For backwards
    compatibility reasons, programs of type UnspecifiedProgram are not yet
    emitted to the CollectionSpec. How we treat functions from .text and
    other unknown sections is still to be debated.
    
    From a high level, the linker is now split into multiple stages:
    
    - Finding neighbours - a program's instruction stream is checked for
      references to any other program in the ELF. If a direct dependency is
      found (a jump to another function), a pointer to that function is
      stored in the program's neighbour table.
    - Flattening - when the program is about to be marshaled for hand-off to
      the kernel, a unique, flat list of dependent programs is generated by
      stepping through each program's neighbour table in a depth-first
      manner. This list must be used by both the instruction and BTF extinfo
      linkers, so they generate their outputs using the same layout.
    - Marshaling - the flat list of programs is simply iterated over and its
      instructions and BTF extinfo's are marshaled to their respective
      output buffers.
    
    Signed-off-by: Timo Beckers <timo@isovalent.com>

--- linker.go
+++ linker.go
@@ -1,57 +1,149 @@
 package ebpf
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 
 	"github.com/cilium/ebpf/asm"
+	"github.com/cilium/ebpf/internal/btf"
 )
 
-// link resolves bpf-to-bpf calls.
+// The linker is responsible for resolving bpf-to-bpf calls between programs
+// within an ELF. Each BPF program must be a self-contained binary blob,
+// so when an instruction in one ELF program section wants to jump to
+// a function in another, the linker needs to pull in the bytecode
+// (and BTF info) of the target function and concatenate the instruction
+// streams together.
 //
-// Each library may contain multiple functions / labels, and is only linked
-// if prog references one of these functions.
+// Later on in the pipeline, all call sites are fixed up with relative jumps
+// within this newly-created instruction stream to then finally hand off to
+// the kernel with BPF_PROG_LOAD.
 //
-// Libraries also linked.
-func link(prog *ProgramSpec, libs []*ProgramSpec) error {
-	var (
-		linked  = make(map[*ProgramSpec]bool)
-		pending = []asm.Instructions{prog.Instructions}
-		insns   asm.Instructions
-	)
-	for len(pending) > 0 {
-		insns, pending = pending[0], pending[1:]
-		for _, lib := range libs {
-			if linked[lib] {
+// Each function is denoted by an ELF symbol and the compiler takes care of
+// register setup before each jump instruction.
+
+// findNeighbours finds bpf-to-bpf calls in all given progs.
+// It populates each ProgramSpec's neighbour list with pointers
+// to all other programs referenced by its bytecode.
+func findNeighbours(progs map[string]*ProgramSpec) error {
+	// Check all ProgramSpecs in the collection against each other.
+	for _, prog := range progs {
+		for _, dep := range progs {
+			// Don't link a program against itself.
+			if prog == dep {
 				continue
 			}
 
-			needed, err := needSection(insns, lib.Instructions)
+			need, err := needProg(prog.Instructions, dep.Instructions)
 			if err != nil {
-				return fmt.Errorf("linking %s: %w", lib.Name, err)
+				return fmt.Errorf("dependency-checking program '%s' and '%s': %w", prog.Name, dep.Name, err)
 			}
 
-			if !needed {
+			if !need {
 				continue
 			}
 
-			linked[lib] = true
-			prog.Instructions = append(prog.Instructions, lib.Instructions...)
-			pending = append(pending, lib.Instructions)
-
-			if prog.BTF != nil && lib.BTF != nil {
-				if err := prog.BTF.Append(lib.BTF); err != nil {
-					return fmt.Errorf("linking BTF of %s: %w", lib.Name, err)
-				}
-			}
+			// Register a direct dependency.
+			prog.neighbours = append(prog.neighbours, dep)
 		}
 	}
 
 	return nil
 }
 
-func needSection(insns, section asm.Instructions) (bool, error) {
-	// A map of symbols to the libraries which contain them.
-	symbols, err := section.SymbolOffsets()
+// collectInstructions returns the instruction stream of all progs in order.
+func collectInstructions(progs []*ProgramSpec) asm.Instructions {
+	var out asm.Instructions
+
+	for _, prog := range progs {
+		out = append(out, prog.Instructions...)
+	}
+
+	return out
+}
+
+// collectFuncInfos returns the BTF func infos of all progs in order.
+func collectFuncInfos(progs []*ProgramSpec) ([]byte, error) {
+	if len(progs) == 0 {
+		return nil, nil
+	}
+
+	var off uint64
+
+	buf := bytes.NewBuffer(make([]byte, 0, binary.Size(&btf.FuncInfo{})*len(progs)))
+	for _, prog := range progs {
+		if err := prog.BTF.FuncInfo.Marshal(buf, off); err != nil {
+			return nil, fmt.Errorf("marshaling prog %s func info: %w", prog.Name, err)
+		}
+		off += prog.Instructions.Size()
+	}
+
+	return buf.Bytes(), nil
+}
+
+// collectLineInfos returns the BTF line infos of all progs in order.
+func collectLineInfos(progs []*ProgramSpec) ([]byte, error) {
+	if len(progs) == 0 {
+		return nil, nil
+	}
+
+	var off uint64
+
+	buf := bytes.NewBuffer(make([]byte, 0, binary.Size(&btf.LineInfo{})*len(progs)))
+	for _, prog := range progs {
+		if err := prog.BTF.LineInfos.Marshal(buf, off); err != nil {
+			return nil, fmt.Errorf("marshaling prog %s line infos: %w", prog.Name, err)
+		}
+		off += prog.Instructions.Size()
+	}
+
+	return buf.Bytes(), nil
+}
+
+// flatten returns a unique list of programs that need to be
+// included in spec's bytestream when inserting it into the kernel.
+//
+// It recursively collects spec's dependencies by stepping into its neighboring
+// ProgramSpecs. Each visited program is recorded to both avoid infinite
+// recursion and to prevent collecting the same program more than once.
+//
+// Always returns spec itself as the first entry. Returns nil when spec was
+// already visited.
+//
+// TODO: The stability of the order of the output matters,
+// this needs to be clear and tested.
+func (spec *ProgramSpec) flatten(visited map[*ProgramSpec]bool) []*ProgramSpec {
+	if visited == nil {
+		visited = make(map[*ProgramSpec]bool)
+	}
+
+	// This program and its dependencies were already collected.
+	if visited[spec] {
+		return nil
+	}
+
+	visited[spec] = true
+
+	// Always return the current program as the first one in the chain.
+	out := []*ProgramSpec{spec}
+
+	// Recurse into each neighbour and collect its neighbours,
+	// keeping track of visited nodes.
+	for _, dep := range spec.neighbours {
+		if dn := dep.flatten(visited); dn != nil {
+			out = append(out, dn...)
+		}
+	}
+
+	return out
+}
+
+// needProg checks insns for references to symbols in instruction stream dep.
+// Returns true if a reference is found from insns to dep.
+func needProg(insns, dep asm.Instructions) (bool, error) {
+	// A map of symbols to the instructions which contain them.
+	symbols, err := dep.SymbolOffsets()
 	if err != nil {
 		return false, err
 	}
@@ -66,7 +158,8 @@ func needSection(insns, section asm.Instructions) (bool, error) {
 		}
 
 		if ins.Constant != -1 {
-			// This is already a valid call, no need to link again.
+			// This is a jump within the same instruction stream,
+			// not a bpf-to-bpf call. Nothing to link.
 			continue
 		}
 

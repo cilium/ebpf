0581fbe58b057266bc44987e28d55d7e758a1adf
Arthur Fabre <afabre@cloudflare.com>
2021-12-02 16:19:29 +0000

    asm: Add Jump32 support
    The unused 0x6 class now represents jumps with 32bit comparisons:
    
    https://lore.kernel.org/all/1548523574-18316-16-git-send-email-jiong.wang@netronome.com/T/
    http://patchwork.ozlabs.org/project/netdev/list/?series=88386&state=*
    
    This complements 32bit ALU operations by allowing comparisons without
    having to sign extend registers beforehand.
    
    Add a Jump32Class to represent this. As some JumpOps are not supported
    by Jump32 (Call, Exit, Ja), centralize the logic for validating jump
    OpCodes in OpCode.JumpOp().

--- asm/opcode.go
+++ asm/opcode.go
@@ -30,6 +30,9 @@ const (
 	ALUClass Class = 0x04
 	// JumpClass jump operators
 	JumpClass Class = 0x05
+	// Jump32Class jump operators with 32 bit comparaisons
+	// Requires kernel 5.1
+	Jump32Class Class = 0x06
 	// ALU64Class arithmetic in 64 bit mode
 	ALU64Class Class = 0x07
 )
@@ -53,9 +56,9 @@ func (cls Class) IsALU() bool {
 	return cls == ALUClass || cls == ALU64Class
 }
 
-// IsJump checks if this is JumpClass.
+// IsJump checks if this is either JumpClass or Jump32Class.
 func (cls Class) IsJump() bool {
-	return cls == JumpClass
+	return cls == JumpClass || cls == Jump32Class
 }
 
 func (cls Class) isJumpOrALU() bool {
@@ -134,11 +137,20 @@ func (op OpCode) Endianness() Endianness {
 }
 
 // JumpOp returns the JumpOp.
+// Returns InvalidJumpOp if it doesn't encode a jump.
 func (op OpCode) JumpOp() JumpOp {
 	if !op.Class().IsJump() {
 		return InvalidJumpOp
 	}
-	return JumpOp(op & jumpMask)
+
+	jumpOp := JumpOp(op & jumpMask)
+
+	// Some JumpOps are only supported by JumpClass, not Jump32Class.
+	if op.Class() == Jump32Class && (jumpOp == Exit || jumpOp == Call || jumpOp == Ja) {
+		return InvalidJumpOp
+	}
+
+	return jumpOp
 }
 
 // SetMode sets the mode on load and store operations.
@@ -188,7 +200,15 @@ func (op OpCode) SetJumpOp(jump JumpOp) OpCode {
 	if !op.Class().IsJump() || !valid(OpCode(jump), jumpMask) {
 		return InvalidOpCode
 	}
-	return (op & ^jumpMask) | OpCode(jump)
+
+	newOp := (op & ^jumpMask) | OpCode(jump)
+
+	// Check newOp is legal.
+	if newOp.JumpOp() == InvalidJumpOp {
+		return InvalidOpCode
+	}
+
+	return newOp
 }
 
 func (op OpCode) String() string {
@@ -228,6 +248,11 @@ func (op OpCode) String() string {
 
 	case class.IsJump():
 		f.WriteString(op.JumpOp().String())
+
+		if class == Jump32Class {
+			f.WriteString("32")
+		}
+
 		if jop := op.JumpOp(); jop != Exit && jop != Call {
 			f.WriteString(strings.TrimSuffix(op.Source().String(), "Source"))
 		}

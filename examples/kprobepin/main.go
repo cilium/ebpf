94d74cb5d9c61f2bb625dd26dd2161a7c7d58f51
Lorenz Bauer <lmb@cloudflare.com>
2021-12-07 17:57:50 +0000

    examples: get rid of bpf subfolder
    It's not necessary to keep the .c file in a subfolder if you add a
    
        // +build ignore
    
    to the top of the file. Move the .c next to the .go and rename the
    files to be consistent with the example name.

--- examples/kprobepin/main.go
+++ examples/kprobepin/main.go
@@ -21,7 +21,7 @@ import (
 )
 
 // $BPF_CLANG and $BPF_CFLAGS are set by the Makefile.
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc $BPF_CLANG -cflags $BPF_CFLAGS bpf ./bpf/kprobe_pin_example.c -- -I../headers
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc $BPF_CLANG -cflags $BPF_CFLAGS bpf kprobe_pin.c -- -I../headers
 
 const (
 	mapKey    uint32 = 0

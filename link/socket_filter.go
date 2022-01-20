d95471345bd5998aed99c8a2a8265e9d1db86cc2
folbrich <frank.olbricht@gmail.com>
2022-01-19 22:41:18 -0700

    Use 2 free functions rather than the Link interface

--- link/socket_filter.go
+++ link/socket_filter.go
@@ -1,53 +1,40 @@
 package link
 
 import (
-	"fmt"
 	"syscall"
 
 	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/internal/unix"
 )
 
-type SocketFilterOptions struct {
-	// File descriptor of the raw socket.
-	SockFD int
-
-	// Program must be of type SocketFilter.
-	Program *ebpf.Program
-}
-
-// AttachSocketFilter links a BPF program to a raw socket.
-func AttachSocketFilter(opts SocketFilterOptions) (Link, error) {
-	if err := syscall.SetsockoptInt(opts.SockFD, unix.SOL_SOCKET, unix.SO_ATTACH_BPF, opts.Program.FD()); err != nil {
-		return nil, err
+// SocketFilterAttachProgram attaches a SocketFilter BPF program to a raw socket.
+func SocketFilterAttachProgram(conn syscall.Conn, program *ebpf.Program) error {
+	rawConn, err := conn.SyscallConn()
+	if err != nil {
+		return err
 	}
-	return &linkSocketFilter{sockFD: opts.SockFD}, nil
+	var ssoErr error
+	err = rawConn.Control(func(fd uintptr) {
+		ssoErr = syscall.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_ATTACH_BPF, program.FD())
+	})
+	if ssoErr != nil {
+		return ssoErr
+	}
+	return err
 }
 
-type linkSocketFilter struct {
-	sockFD int
+// SocketFilterDetachProgram detaches a SocketFilter BPF program from a raw socket.
+func SocketFilterDetachProgram(conn syscall.Conn) error {
+	rawConn, err := conn.SyscallConn()
+	if err != nil {
+		return err
+	}
+	var ssoErr error
+	err = rawConn.Control(func(fd uintptr) {
+		ssoErr = syscall.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_DETACH_BPF, 0)
+	})
+	if ssoErr != nil {
+		return ssoErr
+	}
+	return err
 }
-
-var _ Link = (*linkSocketFilter)(nil)
-
-func (lsf *linkSocketFilter) Update(prog *ebpf.Program) error {
-	return syscall.SetsockoptInt(lsf.sockFD, syscall.SOL_SOCKET, unix.SO_ATTACH_BPF, prog.FD())
-}
-
-func (lsf *linkSocketFilter) Pin(string) error {
-	return fmt.Errorf("can't pin socket filter: %w", ErrNotSupported)
-}
-
-func (lsf *linkSocketFilter) Unpin() error {
-	return fmt.Errorf("can't unpin socket filter: %w", ErrNotSupported)
-}
-
-func (lsf *linkSocketFilter) Info() (*Info, error) {
-	return nil, fmt.Errorf("can't get socket filter info: %w", ErrNotSupported)
-}
-
-func (lsf *linkSocketFilter) Close() error {
-	return syscall.SetsockoptInt(lsf.sockFD, syscall.SOL_SOCKET, unix.SO_DETACH_BPF, 0)
-}
-
-func (lsf *linkSocketFilter) isLink() {}

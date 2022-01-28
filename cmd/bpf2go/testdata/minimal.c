f4a2cf5193a7b1d3020e8a56cc3442cccb2f3b56
Lorenz Bauer <lmb@cloudflare.com>
2022-01-28 15:35:09 +0000

    cmd/bpf2go: generate types from BTF
    Use GoFormatter to generate type definitions for types used as map keys
    and values. Allow users to emit additional types via a command line flag.

--- cmd/bpf2go/testdata/minimal.c
+++ cmd/bpf2go/testdata/minimal.c
@@ -2,13 +2,27 @@
 
 char __license[] __section("license") = "MIT";
 
-struct bpf_map_def map1 __section("maps") = {
-	.type = BPF_MAP_TYPE_HASH,
-	.key_size = 4,
-	.value_size = 4,
-	.max_entries = 1,
-};
+enum e { HOOPY, FROOD };
+
+typedef long long int longint;
+
+typedef struct {
+	longint bar;
+	_Bool baz;
+	enum e boo;
+} barfoo;
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__type(key, enum e);
+	__type(value, barfoo);
+	__uint(max_entries, 1);
+} map1 __section(".maps");
+
+volatile const enum e my_constant = FROOD;
+
+volatile const barfoo struct_const;
 
 __section("socket") int filter() {
-	return 0;
+	return my_constant + struct_const.bar;
 }

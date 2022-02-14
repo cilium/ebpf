5eb685252bedb646cc6c70ce086f9b96f107fae1
Mattia Meleleo <mattia.meleleo@elastic.co>
2022-02-14 20:02:39 +0100

    link/tracepoint: allow specifying bpf cookie via options
    Signed-off-by: Mattia Meleleo <mattia.meleleo@elastic.co>

--- link/tracepoint.go
+++ link/tracepoint.go
@@ -6,12 +6,22 @@ import (
 	"github.com/cilium/ebpf"
 )
 
+// TracepointOptions defines additional parameters that will be used
+// when loading Tracepoints.
+type TracepointOptions struct {
+	// Arbitrary value that can be fetched from an eBPF program
+	// via `bpf_get_attach_cookie()`.
+	//
+	// Needs kernel 5.15+.
+	BpfCookie uint64
+}
+
 // Tracepoint attaches the given eBPF program to the tracepoint with the given
 // group and name. See /sys/kernel/debug/tracing/events to find available
 // tracepoints. The top-level directory is the group, the event's subdirectory
 // is the name. Example:
 //
-//	tp, err := Tracepoint("syscalls", "sys_enter_fork", prog)
+//	tp, err := Tracepoint("syscalls", "sys_enter_fork", prog, nil)
 //
 // Losing the reference to the resulting Link (tp) will close the Tracepoint
 // and prevent further execution of prog. The Link must be Closed during
@@ -19,7 +29,7 @@ import (
 //
 // Note that attaching eBPF programs to syscalls (sys_enter_*/sys_exit_*) is
 // only possible as of kernel 4.14 (commit cf5f5ce).
-func Tracepoint(group, name string, prog *ebpf.Program) (Link, error) {
+func Tracepoint(group, name string, prog *ebpf.Program, opts *TracepointOptions) (Link, error) {
 	if group == "" || name == "" {
 		return nil, fmt.Errorf("group and name cannot be empty: %w", errInvalidInput)
 	}
@@ -49,6 +59,9 @@ func Tracepoint(group, name string, prog *ebpf.Program) (Link, error) {
 		group:     group,
 		name:      name,
 		typ:       tracepointEvent,
+	}
+	if opts != nil {
+		pe.bpfCookie = opts.BpfCookie
 	}
 
 	if err := pe.attach(prog); err != nil {

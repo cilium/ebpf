b605a83b2e732ebb74b5ffe3c65e1c06f63ca732
Timo Beckers <timo@isovalent.com>
2022-03-04 17:54:23 +0100

    btf: add CO-RE read test that fails unless relocations were correctly applied
    The linker rework in 9d739bd caused a piece of code that appended CORERelos
    to the entrypoint instruction stream to be forgotten. The existing CO-RE tests
    were all written in a way where the BPF programs would return 0 (success) if
    no CO-RE relocations were applied at all.
    
    This caused CO-RE relocations against functions (subprogs) that were only
    included by a bpf2bpf call to be skipped.
    
    This patch adds a broken test program that returns non-zero if left unaltered,
    and requires CO-RE relocation against another BTF blob to return 0.
    Additionally, the checks are performed from a non-inlined subprogram to ensure
    CO-RE relocations against subprogs work.
    
    Signed-off-by: Timo Beckers <timo@isovalent.com>

--- internal/btf/core_reloc_test.go
+++ internal/btf/core_reloc_test.go
@@ -1,17 +1,19 @@
 package btf_test
 
 import (
+	"fmt"
 	"io"
 	"os"
 	"strings"
 	"testing"
 
 	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/internal"
 	"github.com/cilium/ebpf/internal/testutils"
 )
 
 func TestCoreRelocationLoad(t *testing.T) {
-	testutils.Files(t, testutils.Glob(t, "testdata/*-el.elf"), func(t *testing.T, file string) {
+	testutils.Files(t, testutils.Glob(t, "testdata/relocs-*.elf"), func(t *testing.T, file string) {
 		fh, err := os.Open(file)
 		if err != nil {
 			t.Fatal(err)
@@ -21,6 +23,10 @@ func TestCoreRelocationLoad(t *testing.T) {
 		spec, err := ebpf.LoadCollectionSpecFromReader(fh)
 		if err != nil {
 			t.Fatal(err)
+		}
+
+		if spec.ByteOrder != internal.NativeEndian {
+			return
 		}
 
 		for _, progSpec := range spec.Programs {
@@ -42,6 +48,59 @@ func TestCoreRelocationLoad(t *testing.T) {
 					return
 				}
 
+				if err != nil {
+					t.Fatal("Load program:", err)
+				}
+				defer prog.Close()
+
+				ret, _, err := prog.Test(make([]byte, 14))
+				testutils.SkipIfNotSupported(t, err)
+				if err != nil {
+					t.Fatal("Error when running:", err)
+				}
+
+				if ret != 0 {
+					t.Error("Assertion failed on line", ret)
+				}
+			})
+		}
+	})
+}
+
+func TestCoreRelocationRead(t *testing.T) {
+	testutils.Files(t, testutils.Glob(t, "testdata/relocs_read-*.elf"), func(t *testing.T, file string) {
+		fh, err := os.Open(file)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer fh.Close()
+
+		spec, err := ebpf.LoadCollectionSpecFromReader(fh)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if spec.ByteOrder != internal.NativeEndian {
+			return
+		}
+
+		tgt, err := os.Open(fmt.Sprintf("testdata/relocs_read_tgt-%s.elf", internal.ClangEndian))
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer tgt.Close()
+
+		for _, progSpec := range spec.Programs {
+			t.Run(progSpec.Name, func(t *testing.T) {
+				if _, err := tgt.Seek(0, io.SeekStart); err != nil {
+					t.Fatal(err)
+				}
+
+				testutils.SkipOnOldKernel(t, "4.16", "bpf2bpf calls")
+
+				prog, err := ebpf.NewProgramWithOptions(progSpec, ebpf.ProgramOptions{
+					TargetBTF: tgt,
+				})
 				if err != nil {
 					t.Fatal("Load program:", err)
 				}

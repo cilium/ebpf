fe5e363eab017ebd19a48c1821d2c423e614779d
Lorenz Bauer <i@lmb.io>
2022-03-21 09:56:08 +0000

    btf: format relocs_read.c and relocs_read_tgt.c
    Run the files through clang-format.

--- internal/btf/testdata/relocs_read_tgt.c
+++ internal/btf/testdata/relocs_read_tgt.c
@@ -19,15 +19,14 @@ typedef unsigned long u64;
 
 struct bits {
 	/*int x;*/
-	u8 b:2, a:4; /* a was before b */
-	my_u32 d:2; /* was 'unsigned int' */
-	u16 c:1; /* was before d */
-	enum { ZERO = 0, ONE = 1 } e:1;
-	u16 f; /* was: u64 f:16 */
-	u32 g:30; /* was: u64 g:30 */
+	u8 b : 2, a : 4; /* a was before b */
+	my_u32 d : 2;    /* was 'unsigned int' */
+	u16 c : 1;       /* was before d */
+	enum { ZERO = 0, ONE = 1 } e : 1;
+	u16 f;      /* was: u64 f:16 */
+	u32 g : 30; /* was: u64 g:30 */
 };
 
 int dummy() {
-	return core_access((struct s){}.a) +
-		core_access((struct bits){}.a);
+	return core_access((struct s){}.a) + core_access((struct bits){}.a);
 }

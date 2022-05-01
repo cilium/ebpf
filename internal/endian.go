afd6ccaf175f7235283ad4406f0c757408cc6c74
Bryce Kahle <bryce.kahle@datadoghq.com>
2022-05-01 12:55:18 -0700

    Hard code endianness

--- internal/endian.go
+++ internal/endian.go
@@ -1,3 +1,6 @@
+//go:build !386 && !amd64 && !amd64p32 && !arm && !arm64 && !mipsle && !mips64le && !mips64p32le && !ppc64le && !riscv64 && !armbe && !arm64be && !mips && !mips64 && !mips64p32 && !ppc64 && !s390 && !s390x && !sparc && !sparc64
+// +build !386,!amd64,!amd64p32,!arm,!arm64,!mipsle,!mips64le,!mips64p32le,!ppc64le,!riscv64,!armbe,!arm64be,!mips,!mips64,!mips64p32,!ppc64,!s390,!s390x,!sparc,!sparc64
+
 package internal
 
 import (
@@ -9,7 +12,7 @@ import (
 // depending on the host's endianness.
 var NativeEndian binary.ByteOrder
 
-// Clang is set to either "el" or "eb" depending on the host's endianness.
+// ClangEndian is set to either "el" or "eb" depending on the host's endianness.
 var ClangEndian string
 
 func init() {

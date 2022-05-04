38d10bec04aa3bc55e8aef1340fb4900026896f8
Lorenz Bauer <i@lmb.io>
2022-05-04 11:49:53 +0000

    btf: allow users to obtain COREFixups
    It's currently not possible to obtain COREFixups since coreRelocate
    is not exported. The relocation process is already opaque to users,
    and this makes it worse. To fix this we export coreRelocate and move
    the bit that collects CORERelocation into ebpf proper.

--- internal/btf/types.go
+++ internal/btf/types.go
@@ -6,6 +6,8 @@ import (
 	"math"
 	"reflect"
 	"strings"
+
+	"github.com/cilium/ebpf/asm"
 )
 
 const maxTypeDepth = 32
@@ -434,6 +436,11 @@ type Func struct {
 	Name    string
 	Type    Type
 	Linkage FuncLinkage
+}
+
+func FuncMetadata(ins *asm.Instruction) *Func {
+	fn, _ := ins.Metadata.Get(funcInfoMeta{}).(*Func)
+	return fn
 }
 
 func (f *Func) Format(fs fmt.State, verb rune) {

3ab9978f03299b53b6b4216092a1822e99ecb427
M. Mert Yildiran <me@mertyildiran.com>
2022-05-28 14:04:15 +0300

    Revert "Change the place where we add the `RelativeOffset`"
    This reverts commit 8a7955ac654c14d0744dc3e48185802c84d94944.

--- link/uprobe.go
+++ link/uprobe.go
@@ -262,11 +262,12 @@ func (ex *Executable) uprobe(symbol string, prog *ebpf.Program, opts *UprobeOpti
 
 	offset := opts.Offset
 	if offset == 0 {
+		offset = opts.RelativeOffset
 		off, err := ex.offset(symbol)
 		if err != nil {
 			return nil, err
 		}
-		offset = off
+		offset += off
 	}
 
 	pid := opts.PID
@@ -283,7 +284,7 @@ func (ex *Executable) uprobe(symbol string, prog *ebpf.Program, opts *UprobeOpti
 	args := probeArgs{
 		symbol:       symbol,
 		path:         ex.path,
-		offset:       offset + opts.RelativeOffset,
+		offset:       offset,
 		pid:          pid,
 		refCtrOffset: opts.RefCtrOffset,
 		ret:          ret,

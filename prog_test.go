501c0dc4641ed10ea92257bfb55f144dc14e6950
Lorenz Bauer <i@lmb.io>
2022-06-01 16:45:52 +0000

    internal: make VerifierError more succinct
    Instead of dumping sometimes megabytes of log lines, summarise verifier
    errors. Very short logs are shown in full, while longer longs only show
    the last few lines by default.
    
        internal/errors_test.go:55: invalid argument: JIT doesn't support bpf-to-bpf calls (1 line(s) omitted)
        internal/errors_test.go:60: invalid argument: [52] STRUCT task_struct size=7744 vlen=218: cpus_mask type_id=109 bitfield_size=0 bits_offset=7744 Invalid member (27 line(s) omitted)
        internal/errors_test.go:64: invalid argument: [11] FUNC helper_func2 type_id=10 vlen != 0 (24 line(s) omitted)
        internal/errors_test.go:69: invalid argument: str_off: 3166088: str_len: 228 (truncated, 6 line(s) omitted)
    
    Using %(+|-)v users can print the first or last n log lines. Individual
    fields of VerifierError are also exported.

--- prog_test.go
+++ prog_test.go
@@ -783,10 +783,29 @@ func mustSocketFilter(tb testing.TB) *Program {
 	return prog
 }
 
+// Retrieve a verifier error when loading a program fails.
+func ExampleProgram_verifierError() {
+	_, err := NewProgram(&ProgramSpec{
+		Type: SocketFilter,
+		Instructions: asm.Instructions{
+			asm.LoadImm(asm.R0, 0, asm.DWord),
+			// Missing Return
+		},
+		License: "MIT",
+	})
+
+	var ve *VerifierError
+	if errors.As(err, &ve) {
+		// Using %+v will print the whole verifier error, not just the last
+		// few lines.
+		fmt.Printf("Verifier error: %+v\n", ve)
+	}
+}
+
 // Use NewProgramWithOptions if you'd like to get the verifier output
 // for a program, or if you want to change the buffer size used when
 // generating error messages.
-func ExampleNewProgramWithOptions() {
+func ExampleProgram_retrieveVerifierOutput() {
 	spec := &ProgramSpec{
 		Type: SocketFilter,
 		Instructions: asm.Instructions{

9e00e33e16bcd984ce75073b97426c6782987331
Mattia Meleleo <mattia.meleleo@elastic.co>
2022-07-20 17:06:33 +0200

    link/kprobe: move kprobe offset test to kprobe_test.go

--- link/kprobe_test.go
+++ link/kprobe_test.go
@@ -428,3 +428,23 @@ func TestKprobeToken(t *testing.T) {
 		})
 	}
 }
+
+func TestKprobeOffset(t *testing.T) {
+	prog := mustLoadProgram(t, ebpf.Kprobe, 0, "")
+
+	for i := uint64(2); i < 10; i++ {
+		k, err := Kprobe("inet6_release", prog, &KprobeOptions{Offset: i})
+		if err != nil {
+			continue
+		}
+		k.Close()
+
+		_, err = Kprobe("inet6_release", prog, &KprobeOptions{Offset: i - 1})
+		if !errors.Is(err, os.ErrNotExist) {
+			t.Fatalf("expected 'os.ErrNotExist', got: '%v'", err)
+		}
+		return
+	}
+
+	t.Fatal("no valid offsets found")
+}

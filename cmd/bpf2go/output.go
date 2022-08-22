5cdf91516e9b9b802f919584043cea974ec108ec
Timo Beckers <timo@isovalent.com>
2022-08-22 16:01:58 +0200

    *: gofmt with 1.19
    Format output docstrings according to 1.19 convention in bpf2go.
    
    Signed-off-by: Timo Beckers <timo@isovalent.com>

--- cmd/bpf2go/output.go
+++ cmd/bpf2go/output.go
@@ -56,9 +56,9 @@ func {{ .Name.Load }}() (*ebpf.CollectionSpec, error) {
 //
 // The following types are suitable as obj argument:
 //
-//     *{{ .Name.Objects }}
-//     *{{ .Name.Programs }}
-//     *{{ .Name.Maps }}
+//	*{{ .Name.Objects }}
+//	*{{ .Name.Programs }}
+//	*{{ .Name.Maps }}
 //
 // See ebpf.CollectionSpec.LoadAndAssign documentation for details.
 func {{ .Name.LoadObjects }}(obj interface{}, opts *ebpf.CollectionOptions) (error) {

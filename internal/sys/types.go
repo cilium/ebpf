d396ddc7bcf3045070dfd58734b5b0603aba33fa
Lorenz Bauer <i@lmb.io>
2022-11-23 15:12:04 +0000

    internal/sys: add BTFTypeID
    Add an exported type for BTF type IDs and use it in generated
    structs.
    
    Signed-off-by: Lorenz Bauer <oss@lmb.io>

--- internal/sys/types.go
+++ internal/sys/types.go
@@ -477,12 +477,12 @@ type MapInfo struct {
 	MapFlags              MapFlags
 	Name                  ObjName
 	Ifindex               uint32
-	BtfVmlinuxValueTypeId BtfTypeId
+	BtfVmlinuxValueTypeId BTFTypeID
 	NetnsDev              uint64
 	NetnsIno              uint64
 	BtfId                 uint32
-	BtfKeyTypeId          BtfTypeId
-	BtfValueTypeId        BtfTypeId
+	BtfKeyTypeId          BTFTypeID
+	BtfValueTypeId        BTFTypeID
 	_                     [4]byte
 	MapExtra              uint64
 }
@@ -616,7 +616,7 @@ type LinkCreateAttr struct {
 	TargetFd    uint32
 	AttachType  AttachType
 	Flags       uint32
-	TargetBtfId BTFID
+	TargetBtfId BTFTypeID
 	_           [28]byte
 }
 
@@ -706,9 +706,9 @@ type MapCreateAttr struct {
 	MapName               ObjName
 	MapIfindex            uint32
 	BtfFd                 uint32
-	BtfKeyTypeId          BtfTypeId
-	BtfValueTypeId        BtfTypeId
-	BtfVmlinuxValueTypeId BtfTypeId
+	BtfKeyTypeId          BTFTypeID
+	BtfValueTypeId        BTFTypeID
+	BtfVmlinuxValueTypeId BTFTypeID
 	MapExtra              uint64
 }
 
@@ -986,7 +986,7 @@ type ProgLoadAttr struct {
 	LineInfoRecSize    uint32
 	LineInfo           Pointer
 	LineInfoCnt        uint32
-	AttachBtfId        BTFID
+	AttachBtfId        BTFTypeID
 	AttachBtfObjFd     uint32
 	CoreReloCnt        uint32
 	FdArray            Pointer
@@ -1081,7 +1081,7 @@ type RawTracepointLinkInfo struct {
 type TracingLinkInfo struct {
 	AttachType  AttachType
 	TargetObjId uint32
-	TargetBtfId BtfTypeId
+	TargetBtfId BTFTypeID
 }
 
 type XDPLinkInfo struct{ Ifindex uint32 }

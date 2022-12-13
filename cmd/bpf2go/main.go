a8362ae31461ddfeb5332ecbd59bd60bd9e2e2a9
Timo Beckers <timo@isovalent.com>
2022-12-13 12:17:19 +0100

    bpf2go,examples: remove deprecated +build tags from generated Go sources
    To follow suit with the new //go:build convention of build tags, this commit
    converts bpf2go to no longer emit the deprecated +build lines.
    
    The bpf2go command line interface remains unchanged to avoid breaking users.
    In the future we can introduce a new -constraints flag and deprecate -tags.
    
    Signed-off-by: Timo Beckers <timo@isovalent.com>
    Co-developed-by: Lorenz Bauer <oss@lmb.io>

--- cmd/bpf2go/main.go
+++ cmd/bpf2go/main.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"go/build/constraint"
 	"go/token"
 	"io"
 	"os"
@@ -76,7 +77,7 @@ func run(stdout io.Writer, pkg, outputDir string, args []string) (err error) {
 	fs.StringVar(&b2g.strip, "strip", "", "`binary` used to strip DWARF from compiled BPF (default \"llvm-strip\")")
 	fs.BoolVar(&b2g.disableStripping, "no-strip", false, "disable stripping of DWARF")
 	flagCFlags := fs.String("cflags", "", "flags passed to the compiler, may contain quoted arguments")
-	fs.StringVar(&b2g.tags, "tags", "", "list of Go build tags to include in generated files")
+	fs.Var(&b2g.tags, "tags", "Comma-separated list of Go build tags to include in generated files")
 	flagTarget := fs.String("target", "bpfel,bpfeb", "clang target(s) to compile for (comma separated)")
 	fs.StringVar(&b2g.makeBase, "makebase", "", "write make compatible depinfo files relative to `directory`")
 	fs.Var(&b2g.cTypes, "type", "`Name` of a type to generate a Go declaration for, may be repeated")
@@ -155,10 +156,6 @@ func run(stdout io.Writer, pkg, outputDir string, args []string) (err error) {
 
 	if b2g.outputStem != "" && strings.ContainsRune(b2g.outputStem, filepath.Separator) {
 		return fmt.Errorf("-output-stem %q must not contain path separation characters", b2g.outputStem)
-	}
-
-	if strings.ContainsRune(b2g.tags, '\n') {
-		return fmt.Errorf("-tags mustn't contain new line characters")
 	}
 
 	targetArches := strings.Split(*flagTarget, ",")
@@ -261,8 +258,8 @@ type bpf2go struct {
 	skipGlobalTypes bool
 	// C types to include in the generatd output.
 	cTypes cTypes
-	// Go tags included in the .go
-	tags string
+	// Build tags to be included in the output.
+	tags buildTags
 	// Base directory of the Makefile. Enables outputting make-style dependencies
 	// in .d files.
 	makeBase string
@@ -292,13 +289,13 @@ func (b2g *bpf2go) convert(tgt target, arches []string) (err error) {
 		return err
 	}
 
-	var tags []string
-	if len(arches) > 0 {
-		tags = append(tags, strings.Join(arches, " "))
+	var archConstraint constraint.Expr
+	for _, arch := range arches {
+		tag := &constraint.TagExpr{Tag: arch}
+		archConstraint = orConstraints(archConstraint, tag)
 	}
-	if b2g.tags != "" {
-		tags = append(tags, b2g.tags)
-	}
+
+	constraints := andConstraints(archConstraint, b2g.tags.Expr)
 
 	cFlags := make([]string, len(b2g.cFlags))
 	copy(cFlags, b2g.cFlags)
@@ -342,7 +339,7 @@ func (b2g *bpf2go) convert(tgt target, arches []string) (err error) {
 		ident:           b2g.ident,
 		cTypes:          b2g.cTypes,
 		skipGlobalTypes: b2g.skipGlobalTypes,
-		tags:            tags,
+		constraints:     constraints,
 		obj:             objFileName,
 		out:             goFile,
 	})

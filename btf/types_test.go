fa3ddc9508ebf61a7c35ae33780d617201fbb82a
Timo Beckers <timo@isovalent.com>
2023-02-17 11:19:37 +0100

    btf: add function for populating Datasec.Vars[].Offset and ds.Size
    fixupDatasecLayout steps through Datasec.Vars to calculate the proper offsets
    based on the Vars' sizes and populates Datasec.Size when done.
    
    alignof() now returns an error if the size of the type it wants to calculate
    is not a power of two.
    
    Signed-off-by: Timo Beckers <timo@isovalent.com>

--- btf/types_test.go
+++ btf/types_test.go
@@ -35,6 +35,29 @@ func TestSizeof(t *testing.T) {
 	}
 }
 
+func TestPow(t *testing.T) {
+	tests := []struct {
+		n int
+		r bool
+	}{
+		{0, false},
+		{1, true},
+		{2, true},
+		{3, false},
+		{4, true},
+		{5, false},
+		{8, true},
+	}
+
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%d", tt.n), func(t *testing.T) {
+			if want, got := tt.r, pow(tt.n); want != got {
+				t.Errorf("unexpected result for n %d; want: %v, got: %v", tt.n, want, got)
+			}
+		})
+	}
+}
+
 func TestCopy(t *testing.T) {
 	_ = Copy((*Void)(nil), nil)
 

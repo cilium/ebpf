c3368618c14f0aa0eb058e92199a8a95a0e0e233
Robin Gögge <r.goegge@isovalent.com>
2023-02-28 12:38:29 +0100

    elf_reader, linker: add initial kfunc support
    This commit adds support for handling the following extern
    kfunc declaration in bpf programs:
    
    extern void bpf_kfunc_foo(void *mem) __ksym;
    
    If during relocation a call to a kfunc is found we set the corresponding
    btf.Func as Metadata on the instruction and change Instruction.Src to
    asm.PseudoKfuncCall.
    
    fixupKfunc() has been added to the linker, to find the type id of the
    btf.Func in the running kernel and set Instruction.Constant to the found
    btf id.
    
    Signed-off-by: Robin Gögge <r.goegge@isovalent.com>

--- linker.go
+++ linker.go
@@ -185,8 +185,50 @@ func fixupAndValidate(insns asm.Instructions) error {
 			return fmt.Errorf("instruction %d: map %s: %w", iter.Index, ins.Reference(), asm.ErrUnsatisfiedMapReference)
 		}
 
+		if err := fixupKfunc(ins); err != nil {
+			return fmt.Errorf("fixing up kfunc: %w", err)
+		}
+
 		fixupProbeReadKernel(ins)
 	}
+
+	return nil
+}
+
+// fixupKfunc sets Instruction.Constant to the running kernels btf id
+// of the btf.Func in the instructions Metadata.
+func fixupKfunc(ins *asm.Instruction) error {
+	if !ins.IsKfuncCall() {
+		return nil
+	}
+
+	// check meta, if no meta return err
+	kfm, _ := ins.Metadata.Get(kfuncMeta{}).(*btf.Func)
+	if kfm == nil {
+		return fmt.Errorf("kfunc call has no kfuncMeta")
+	}
+
+	s, err := btf.LoadKernelSpec()
+	if err != nil {
+		return err
+	}
+
+	var fn *btf.Func
+	if err := s.TypeByName(kfm.Name, &fn); err != nil {
+		return fmt.Errorf("couldn't resolve %s in kernel spec: %v: %w", kfm.Name, err, ErrNotSupported)
+	}
+
+	if err := btf.CheckTypeCompatibility(kfm.Type, fn.Type); err != nil {
+		return err
+	}
+
+	id, err := s.TypeID(fn)
+	if err != nil {
+		return err
+	}
+
+	ins.Constant = int64(id)
+	ins.Offset = int16(0) // currently always 0, no support for kmods
 
 	return nil
 }

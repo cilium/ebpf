1d550575f72d426b05665025dc2488b5fa8777c5
nijich <nijich@163.com>
2023-03-13 10:04:41 +0800

    format c code

--- examples/socket_filter/socket_filter.c
+++ examples/socket_filter/socket_filter.c
@@ -1,6 +1,6 @@
 //go:build ignore
-#include "common.h"
 #include "bpf_endian.h"
+#include "common.h"
 
 #define DROP 0
 #define PASS -1
@@ -10,44 +10,44 @@
 char __license[] SEC("license") = "Dual MIT/GPL";
 
 SEC("socket")
-int socket_fitler(struct __sk_buff* skb){
-    struct ethhdr eth;
-    struct iphdr ip;
-    struct tcphdr tcp;
+int socket_fitler(struct __sk_buff *skb) {
+	struct ethhdr eth;
+	struct iphdr ip;
+	struct tcphdr tcp;
 
-    u32 offset = 0;
+	u32 offset = 0;
 
-    // If bpf_skb_load_bytes() encounters an error, drop the packet directly.
-    if (bpf_skb_load_bytes(skb, offset, &eth, sizeof(eth))) {
-        return DROP;  
-    }
+	// If bpf_skb_load_bytes() encounters an error, drop the packet directly.
+	if (bpf_skb_load_bytes(skb, offset, &eth, sizeof(eth))) {
+		return DROP;
+	}
 
-    if (eth.h_proto != bpf_htons(ETH_P_IP)) {
-        return DROP;
-    }
+	if (eth.h_proto != bpf_htons(ETH_P_IP)) {
+		return DROP;
+	}
 
-    offset += sizeof(eth);
-    if (bpf_skb_load_bytes(skb, offset, &ip, sizeof(ip))){
-        return DROP;
-    }
+	offset += sizeof(eth);
+	if (bpf_skb_load_bytes(skb, offset, &ip, sizeof(ip))) {
+		return DROP;
+	}
 
-    if (ip.protocol != PROTOCOL_TCP) {
-        return DROP;
-    }
+	if (ip.protocol != PROTOCOL_TCP) {
+		return DROP;
+	}
 
-    // Caculate the length of iphdr, and get the start offset of tcp layer.
-    u16 ip_header_len = ip.ihl << 2;
-    offset += ip_header_len;
+	// Caculate the length of iphdr, and get the start offset of tcp layer.
+	u16 ip_header_len = ip.ihl << 2;
+	offset += ip_header_len;
 
-    if (bpf_skb_load_bytes(skb, offset, &tcp, sizeof(tcp))){
-        return DROP;
-    }
+	if (bpf_skb_load_bytes(skb, offset, &tcp, sizeof(tcp))) {
+		return DROP;
+	}
 
-    // Drop the packets whose src and dst ports are not '80'.
-    if (tcp.source != bpf_htons(80) && tcp.dest != bpf_htons(80)) {
-        return DROP;
-    }
+	// Drop the packets whose src and dst ports are not '80'.
+	if (tcp.source != bpf_htons(80) && tcp.dest != bpf_htons(80)) {
+		return DROP;
+	}
 
-    // Pass the packets to userspace.
-    return PASS;
+	// Pass the packets to userspace.
+	return PASS;
 }

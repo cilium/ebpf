3945fa593427ad3fd1e56e03b0bf8dad84176d83
Bryce Kahle <bryce.kahle@datadoghq.com>
2023-03-14 11:37:16 +0200

    link: allow debugfs fstype for /sys/kernel/debug/tracing
    RHEL/CentOS support eBPF on 3.10 before tracefs was introduced,
    and thus have /sys/kernel/debug/tracing mounted with debug fstype.
    
    Signed-off-by: Bryce Kahle <bryce.kahle@datadoghq.com>

--- link/perf_event.go
+++ link/perf_event.go
@@ -450,6 +450,8 @@ var getTracefsPath = internal.Memoize(func() (string, error) {
 	}{
 		{"/sys/kernel/tracing", unix.TRACEFS_MAGIC},
 		{"/sys/kernel/debug/tracing", unix.TRACEFS_MAGIC},
+		// RHEL/CentOS
+		{"/sys/kernel/debug/tracing", unix.DEBUGFS_MAGIC},
 	} {
 		if fsType, err := internal.FSType(p.path); err == nil && fsType == p.fsType {
 			return p.path, nil

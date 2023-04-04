4638773105b637097955de17d1d8f3930ff22149
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-04-04 13:29:27 +0200

    move to internal tracefs
    Signed-off-by: Paul Cacheux <paul.cacheux@datadoghq.com>

---
+++ internal/tracefs/kprobe_test.go
@@ -0,0 +1,50 @@
+package tracefs
+
+import (
+	"fmt"
+	"testing"
+
+	qt "github.com/frankban/quicktest"
+)
+
+func TestKprobeTraceFSGroup(t *testing.T) {
+	c := qt.New(t)
+
+	// Expect <prefix>_<16 random hex chars>.
+	g, err := RandomGroup("ebpftest")
+	c.Assert(err, qt.IsNil)
+	c.Assert(g, qt.Matches, `ebpftest_[a-f0-9]{16}`)
+
+	// Expect error when the generator's output exceeds 63 characters.
+	p := make([]byte, 47) // 63 - 17 (length of the random suffix and underscore) + 1
+	for i := range p {
+		p[i] = byte('a')
+	}
+	_, err = RandomGroup(string(p))
+	c.Assert(err, qt.Not(qt.IsNil))
+
+	// Reject non-alphanumeric characters.
+	_, err = RandomGroup("/")
+	c.Assert(err, qt.Not(qt.IsNil))
+}
+
+func TestKprobeToken(t *testing.T) {
+	tests := []struct {
+		args     ProbeArgs
+		expected string
+	}{
+		{ProbeArgs{Symbol: "symbol"}, "symbol"},
+		{ProbeArgs{Symbol: "symbol", Offset: 1}, "symbol+0x1"},
+		{ProbeArgs{Symbol: "symbol", Offset: 65535}, "symbol+0xffff"},
+		{ProbeArgs{Symbol: "symbol", Offset: 65536}, "symbol+0x10000"},
+	}
+
+	for i, tt := range tests {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			po := KprobeToken(tt.args)
+			if tt.expected != po {
+				t.Errorf("Expected symbol+offset to be '%s', got '%s'", tt.expected, po)
+			}
+		})
+	}
+}

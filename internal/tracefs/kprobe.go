19f8e401a3a370493bbcd08e2c3676f39314cca3
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-04-11 16:55:33 +0200

    remove `types.go`
    Signed-off-by: Paul Cacheux <paul.cacheux@datadoghq.com>

--- internal/tracefs/kprobe.go
+++ internal/tracefs/kprobe.go
@@ -20,6 +20,36 @@ var (
 	ErrInvalidMaxActive = errors.New("can only set maxactive on kretprobes")
 )
 
+type ProbeType uint8
+
+const (
+	KprobeType ProbeType = iota
+	UprobeType
+)
+
+func (pt ProbeType) String() string {
+	if pt == KprobeType {
+		return "kprobe"
+	}
+	return "uprobe"
+}
+
+func (pt ProbeType) EventsFile() (*os.File, error) {
+	path, err := sanitizeTracefsPath(fmt.Sprintf("%s_events", pt.String()))
+	if err != nil {
+		return nil, err
+	}
+
+	return os.OpenFile(path, os.O_APPEND|os.O_WRONLY, 0666)
+}
+
+type ProbeArgs struct {
+	Symbol, Group, Path          string
+	Offset, RefCtrOffset, Cookie uint64
+	Pid, RetprobeMaxActive       int
+	Ret                          bool
+}
+
 func KprobeCheckLite(symbol string, pid int) error {
 	fd, err := tryAttach(symbol, pid)
 	if err != nil {

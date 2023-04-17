a23d2dcb89fa2c267b34bcd167418b5d593e811d
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-04-17 15:24:30 +0200

    bubble up error if we are unable to derive syscall wrapper support value
    Signed-off-by: Paul Cacheux <paul.cacheux@datadoghq.com>

--- collection.go
+++ collection.go
@@ -604,9 +604,13 @@ func resolveKconfig(m *MapSpec) error {
 			if integer, ok := v.Type.(*btf.Int); !ok || integer.Size != 4 {
 				return fmt.Errorf("variable %s must be a 32 bits integer, got %s", n, v.Type)
 			}
-			var value uint32
-			if haveSyscallWrapper() == nil {
-				value = 1
+			var value uint32 = 1
+			err := haveSyscallWrapper()
+			if err != nil {
+				if err != internal.ErrNotSupported {
+					return err
+				}
+				value = 0
 			}
 
 			internal.NativeEndian.PutUint32(data[vsi.Offset:], value)

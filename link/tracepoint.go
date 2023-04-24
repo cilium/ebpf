0322577ff8c500c021e86cf1b5d9d8b424568420
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-04-24 13:58:14 +0200

    move tracefs related functions to `internal/tracefs`
    Signed-off-by: Paul Cacheux <paul.cacheux@datadoghq.com>

--- link/tracepoint.go
+++ link/tracepoint.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/internal/tracefs"
 )
 
 // TracepointOptions defines additional parameters that will be used
@@ -36,14 +37,14 @@ func Tracepoint(group, name string, prog *ebpf.Program, opts *TracepointOptions)
 	if prog == nil {
 		return nil, fmt.Errorf("prog cannot be nil: %w", errInvalidInput)
 	}
-	if !isValidTraceID(group) || !isValidTraceID(name) {
+	if !tracefs.IsValidTraceID(group) || !tracefs.IsValidTraceID(name) {
 		return nil, fmt.Errorf("group and name '%s/%s' must be alphanumeric or underscore: %w", group, name, errInvalidInput)
 	}
 	if prog.Type() != ebpf.TracePoint {
 		return nil, fmt.Errorf("eBPF program type %s is not a Tracepoint: %w", prog.Type(), errInvalidInput)
 	}
 
-	tid, err := getTraceEventID(group, name)
+	tid, err := tracefs.GetTraceEventID(group, name)
 	if err != nil {
 		return nil, err
 	}

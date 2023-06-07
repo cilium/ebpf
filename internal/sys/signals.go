72196040beb219b6f1e90d1a74c92f56f8671427
Lorenz Bauer <lmb@isovalent.com>
2023-06-07 16:30:28 +0200

    internal/sys: don't panic in init()
    Remove the panic when initializing the profSet. It's causing friction
    when k8s tried to update the vendored dependency.
    
    Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

--- internal/sys/signals.go
+++ internal/sys/signals.go
@@ -8,19 +8,20 @@ import (
 	"github.com/cilium/ebpf/internal/unix"
 )
 
+// A sigset containing only SIGPROF.
 var profSet unix.Sigset_t
 
 func init() {
-	if err := sigsetAdd(&profSet, unix.SIGPROF); err != nil {
-		panic(fmt.Errorf("creating signal set: %w", err))
-	}
+	// See sigsetAdd for details on the implementation. Open coded here so
+	// that the compiler will check the constant calculations for us.
+	profSet.Val[sigprofBit/wordBits] |= 1 << (sigprofBit % wordBits)
 }
 
 // maskProfilerSignal locks the calling goroutine to its underlying OS thread
 // and adds SIGPROF to the thread's signal mask. This prevents pprof from
 // interrupting expensive syscalls like e.g. BPF_PROG_LOAD.
 //
-// The caller must defer sys.UnmaskProfilerSignal() to reverse the operation.
+// The caller must defer unmaskProfilerSignal() to reverse the operation.
 func maskProfilerSignal() {
 	runtime.LockOSThread()
 
@@ -43,40 +44,8 @@ func unmaskProfilerSignal() {
 }
 
 const (
-	wordBytes = int(unsafe.Sizeof(unix.Sigset_t{}.Val[0]))
-	wordBits  = wordBytes * 8
-
-	setBytes = int(unsafe.Sizeof(unix.Sigset_t{}))
-	setBits  = setBytes * 8
-)
-
-// sigsetAdd adds signal to set.
-//
-// Note: Sigset_t.Val's value type is uint32 or uint64 depending on the arch.
-// This function must be able to deal with both and so must avoid any direct
-// references to u32 or u64 types.
-func sigsetAdd(set *unix.Sigset_t, signal unix.Signal) error {
-	if signal < 1 {
-		return fmt.Errorf("signal %d must be larger than 0", signal)
-	}
-	if int(signal) > setBits {
-		return fmt.Errorf("signal %d does not fit within unix.Sigset_t", signal)
-	}
-
-	// For amd64, runtime.sigaddset() performs the following operation:
-	// set[(signal-1)/32] |= 1 << ((uint32(signal) - 1) & 31)
-	//
-	// This trick depends on sigset being two u32's, causing a signal in the the
-	// bottom 31 bits to be written to the low word if bit 32 is low, or the high
-	// word if bit 32 is high.
-
 	// Signal is the nth bit in the bitfield.
-	bit := int(signal - 1)
-	// Word within the sigset the bit needs to be written to.
-	word := bit / wordBits
-
-	// Write the signal bit into its corresponding word at the corrected offset.
-	set.Val[word] |= 1 << (bit % wordBits)
-
-	return nil
-}
+	sigprofBit = int(unix.SIGPROF - 1)
+	// The number of bits in one Sigset_t word.
+	wordBits = int(unsafe.Sizeof(unix.Sigset_t{}.Val[0])) * 8
+)

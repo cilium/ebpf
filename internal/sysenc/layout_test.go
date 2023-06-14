77e98600d0bdbf379dd76f8a34a86b264526586e
Lorenz Bauer <lmb@isovalent.com>
2023-06-14 16:08:23 +0100

    map: zero-allocation operations for common types
    Map keys and values are currently marshaled into []byte by souped
    up versions of binary.Write and binary.Read. This allows users to
    be blissfully unaware of compiler inserted padding on the Go side.
    This is wasteful in case the Go in-memory representation matches
    what the kernel expects because we need additional allocations.
    
    Refactor syscall marshaling into a new package sysenc which
    encapsulates the logic we need to determine whether a Go type
    is safe for zero-allocation / zero-copy marshaling. The type
    must be a pointer to or a slice of:
    
    * A primitive type like uint32, ... or
    * An array of valid types or
    * A struct made up of valid types without any compiler
      inserted padding between fields
    
    Per-CPU maps don't support zero-allocation operations for now,
    but the new code already makes things a little bit cheaper.
    
        goos: linux
        goarch: amd64
        pkg: github.com/cilium/ebpf
        cpu: 12th Gen Intel(R) Core(TM) i7-1260P
                                             │ unsafe.txt  │
                                             │   sec/op    │
        Marshaling/ValueUnmarshalReflect-16    356.1n ± 2%
        Marshaling/KeyMarshalReflect-16        368.6n ± 1%
        Marshaling/ValueBinaryUnmarshaler-16   378.6n ± 2%
        Marshaling/KeyBinaryMarshaler-16       356.2n ± 1%
        Marshaling/KeyValueUnsafe-16           328.0n ± 2%
        PerCPUMarshalling/reflection-16        1.232µ ± 1%
    
                                             │  unsafe.txt  │
                                             │     B/op     │
        Marshaling/ValueUnmarshalReflect-16    0.000 ± 0%
        Marshaling/KeyMarshalReflect-16        0.000 ± 0%
        Marshaling/ValueBinaryUnmarshaler-16   24.00 ± 0%
        Marshaling/KeyBinaryMarshaler-16       8.000 ± 0%
        Marshaling/KeyValueUnsafe-16           0.000 ± 0%
        PerCPUMarshalling/reflection-16        280.0 ± 0%
    
                                             │  unsafe.txt  │
                                             │  allocs/op   │
        Marshaling/ValueUnmarshalReflect-16    0.000 ± 0%
        Marshaling/KeyMarshalReflect-16        0.000 ± 0%
        Marshaling/ValueBinaryUnmarshaler-16   1.000 ± 0%
        Marshaling/KeyBinaryMarshaler-16       1.000 ± 0%
        Marshaling/KeyValueUnsafe-16           0.000 ± 0%
        PerCPUMarshalling/reflection-16        3.000 ± 0%
    
    Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

---
+++ internal/sysenc/layout_test.go
@@ -0,0 +1,113 @@
+package sysenc
+
+import (
+	"testing"
+
+	qt "github.com/frankban/quicktest"
+	"github.com/google/go-cmp/cmp"
+)
+
+type struc struct {
+	A uint64
+	B uint32
+}
+
+func TestLayoutOf(t *testing.T) {
+	for _, test := range []struct {
+		name   string
+		value  any
+		result dataLayout
+		cached bool
+	}{
+		{
+			"trailing padding",
+			&struc{},
+			dataLayout{1, 8 + 4, 8 + 8},
+			true,
+		},
+		{
+			"slice size",
+			[]uint32{1, 2},
+			dataLayout{2, 4, 4},
+			false,
+		},
+		{
+			"array size",
+			&[2]uint64{},
+			dataLayout{2, 8, 8},
+			false,
+		},
+		{
+			"padding between slice entries",
+			&[]struc{},
+			dataLayout{0, 8 + 4, 8 + 8},
+			true,
+		},
+		{
+			"padding between array entries",
+			&[2]struc{},
+			dataLayout{2, 8 + 4, 8 + 8},
+			true,
+		},
+		{
+			"pointer to slice",
+			&[]uint32{1},
+			dataLayout{1, 4, 4},
+			false,
+		},
+		{
+			"explicit padding",
+			&struct{ _ uint64 }{},
+			dataLayout{1, 8, 8},
+			true,
+		},
+	} {
+		t.Run("valid: "+test.name, func(t *testing.T) {
+			flushCachedLayouts()
+			qt.Assert(t, layoutOf(test.value), qt.CmpEquals(cmp.AllowUnexported(dataLayout{})), test.result)
+			qt.Assert(t, countCachedLayouts() > 0, qt.Equals, test.cached)
+		})
+	}
+
+	for _, test := range []struct {
+		name   string
+		value  any
+		cached bool
+	}{
+		{
+			"interspersed padding",
+			&struct {
+				B uint32
+				A uint64
+			}{},
+			true,
+		},
+		{
+			"unexported field",
+			&struct{ a uint64 }{},
+			true,
+		},
+	} {
+		t.Run("invalid: "+test.name, func(t *testing.T) {
+			flushCachedLayouts()
+			layout := layoutOf(test.value)
+			qt.Assert(t, layout.valid(), qt.IsFalse)
+			qt.Assert(t, countCachedLayouts() > 0, qt.Equals, test.cached)
+		})
+	}
+}
+
+func flushCachedLayouts() {
+	cachedLayouts.Range(func(key, _ any) bool {
+		cachedLayouts.Delete(key)
+		return true
+	})
+}
+
+func countCachedLayouts() (n int) {
+	cachedLayouts.Range(func(key, value any) bool {
+		n++
+		return true
+	})
+	return
+}

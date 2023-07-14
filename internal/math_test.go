b67f27a12295d06feea26bfcfd8edc3ba5077abf
Jordan Liggitt <liggitt@google.com>
2023-07-14 15:01:45 -0400

    internal: remove dependency on x/exp/constraints
    Temporarily copy the definition of the Integer constraint until the constraints
    package is moved out of x/exp into the stdlib.
    
    Add a test that makes sure Integer contains all of Go's integer types.
    
    Signed-off-by: Timo Beckers <timo@isovalent.com>
    Suggested-by: Jordan Liggitt <liggitt@google.com>

--- internal/math_test.go
+++ internal/math_test.go
@@ -2,6 +2,10 @@ package internal
 
 import (
 	"fmt"
+	"go/importer"
+	"regexp"
+	"slices"
+	"strings"
 	"testing"
 )
 
@@ -25,5 +29,25 @@ func TestPow(t *testing.T) {
 				t.Errorf("unexpected result for n %d; want: %v, got: %v", tt.n, want, got)
 			}
 		})
+	}
+}
+
+func TestIntegerConstraint(t *testing.T) {
+	rgx := regexp.MustCompile(`^(u)?int([0-9]*|ptr)?$`)
+
+	pkg, err := importer.Default().Import("reflect")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, name := range pkg.Scope().Names() {
+		name = strings.ToLower(name)
+		if !rgx.MatchString(name) {
+			continue
+		}
+
+		if !slices.Contains(integers, name) {
+			t.Errorf("Go type %s is not in the list of known integer types", name)
+		}
 	}
 }

becbab154cbf7dfaef8a22652b212de9ae93d448
Lorenz Bauer <lmb@isovalent.com>
2023-08-11 12:40:38 +0100

    btf: move feature tests to separate file
    No other changes intended.
    
    Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

---
+++ btf/feature_test.go
@@ -0,0 +1,23 @@
+package btf
+
+import (
+	"testing"
+
+	"github.com/cilium/ebpf/internal/testutils"
+)
+
+func TestHaveBTF(t *testing.T) {
+	testutils.CheckFeatureTest(t, haveBTF)
+}
+
+func TestHaveMapBTF(t *testing.T) {
+	testutils.CheckFeatureTest(t, haveMapBTF)
+}
+
+func TestHaveProgBTF(t *testing.T) {
+	testutils.CheckFeatureTest(t, haveProgBTF)
+}
+
+func TestHaveFuncLinkage(t *testing.T) {
+	testutils.CheckFeatureTest(t, haveFuncLinkage)
+}

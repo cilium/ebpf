a1153386668532c9543b94ef54ee1b224ba5c27a
Timo Beckers <timo@isovalent.com>
2023-09-27 16:03:39 +0200

    Makefile: set BPF2GO_ environment vars ambiently, remove from go:generate
    This patch removes the -cc and -cflags from all 'go:generate' statements across
    the codebase. Instead, ambiently provide BPF2GO_ env vars to the 'make' call
    in container-all.
    
    This allows running `go generate ./...` on distributions where clang binaries
    don't have their version numbers suffixed.
    
    Signed-off-by: Timo Beckers <timo@isovalent.com>

--- examples/tcprtt_sockops/main.go
+++ examples/tcprtt_sockops/main.go
@@ -40,8 +40,7 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-// $BPF_CLANG and $BPF_CFLAGS are set by the Makefile.
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -tags "linux" -cc $BPF_CLANG -cflags $BPF_CFLAGS -type rtt_event bpf tcprtt_sockops.c -- -I../headers
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -tags "linux" -type rtt_event bpf tcprtt_sockops.c -- -I../headers
 
 func main() {
 	stopper := make(chan os.Signal, 1)
--- examples/xdp/main.go
+++ examples/xdp/main.go
@@ -20,8 +20,7 @@ import (
 	"github.com/cilium/ebpf/link"
 )
 
-// $BPF_CLANG and $BPF_CFLAGS are set by the Makefile.
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc $BPF_CLANG -cflags $BPF_CFLAGS bpf xdp.c -- -I../headers
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go bpf xdp.c -- -I../headers
 
 func main() {
 	if len(os.Args) < 2 {

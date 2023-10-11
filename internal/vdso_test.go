353ca88b89c64d1b726cbeee74defcd3bf128495
Lorenz Bauer <lmb@isovalent.com>
2023-10-11 14:40:00 +0100

    internal: fix reading auxv on 32-bit platforms
    It turns out that the auxiliary vector has a platform specific size.
    Adjust the code to use uintptr to approximate "unsigned long" from C.
    auxv32le.bin is from an i686 Debian bookworm machine.
    
    Fixes https://github.com/cilium/ebpf/issues/1133
    
    Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

--- internal/vdso_test.go
+++ internal/vdso_test.go
@@ -1,41 +1,59 @@
 package internal
 
 import (
+	"encoding/binary"
 	"errors"
 	"os"
 	"testing"
+
+	qt "github.com/frankban/quicktest"
 )
 
 func TestAuxvVDSOMemoryAddress(t *testing.T) {
-	av, err := os.Open("../testdata/auxv.bin")
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Cleanup(func() { av.Close() })
+	for _, testcase := range []struct {
+		source  string
+		is32bit bool
+		address uint64
+	}{
+		{"auxv64le.bin", false, 0x7ffd377e5000},
+		{"auxv32le.bin", true, 0xb7fc3000},
+	} {
+		t.Run(testcase.source, func(t *testing.T) {
+			av, err := os.Open("testdata/" + testcase.source)
+			if err != nil {
+				t.Fatal(err)
+			}
+			t.Cleanup(func() { av.Close() })
 
-	addr, err := vdsoMemoryAddress(av)
-	if err != nil {
-		t.Fatal(err)
-	}
+			addr, err := vdsoMemoryAddress(av, binary.LittleEndian, testcase.is32bit)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	expected := uint64(0x7ffd377e5000)
-	if addr != expected {
-		t.Errorf("Expected vDSO memory address %x, got %x", expected, addr)
+			if uint64(addr) != testcase.address {
+				t.Errorf("Expected vDSO memory address %x, got %x", testcase.address, addr)
+			}
+		})
 	}
 }
 
 func TestAuxvNoVDSO(t *testing.T) {
 	// Copy of auxv.bin with the vDSO pointer removed.
-	av, err := os.Open("../testdata/auxv_no_vdso.bin")
+	av, err := os.Open("testdata/auxv64le_no_vdso.bin")
 	if err != nil {
 		t.Fatal(err)
 	}
 	t.Cleanup(func() { av.Close() })
 
-	_, err = vdsoMemoryAddress(av)
+	_, err = vdsoMemoryAddress(av, binary.LittleEndian, false)
 	if want, got := errAuxvNoVDSO, err; !errors.Is(got, want) {
 		t.Fatalf("expected error '%v', got: %v", want, got)
 	}
+}
+
+func TestVDSOVersion(t *testing.T) {
+	_, err := vdsoVersion()
+	qt.Assert(t, err, qt.IsNil)
 }
 
 func TestLinuxVersionCodeEmbedded(t *testing.T) {
@@ -44,11 +62,11 @@ func TestLinuxVersionCodeEmbedded(t *testing.T) {
 		version uint32
 	}{
 		{
-			"../testdata/vdso.bin",
+			"testdata/vdso.bin",
 			uint32(328828), // 5.4.124
 		},
 		{
-			"../testdata/vdso_multiple_notes.bin",
+			"testdata/vdso_multiple_notes.bin",
 			uint32(328875), // Container Optimized OS v85 with a 5.4.x kernel
 		},
 	}

285069d50ee9d4c1744622f305c40671a3c90cd5
Lorenz Bauer <lmb@isovalent.com>
2023-11-13 16:48:00 +0000

    link: add TCX support
    Add support for the new tcx link type. This supersedes netlink based
    attachment to TC ingress and egress hooks. It is the first user of
    the bpf_mprog API in the kernel, which allows attaching multiple
    programs to the same interface. Semantically programs are put into
    a list and then executed in order. Earlier programs may prevent the
    execution of later programs.
    
    The most interesting part is a way to express where a new program
    should be inserted in the list. By default, a program attached via a
    new link will be appended to the list, which is equivalent to
    passing the BPF_F_AFTER flag. Passing the BPF_F_BEFORE flag prepends
    instead.
    
    BEFORE and AFTER can be combined with a reference to a program or a
    link to specify any position in the list. The BPF_F_LINK and BPF_F_ID
    flags indicate whether the reference is to a link or a program and
    whether it is in the form of an FD or a ID. The default is to
    refer to a program by FD. The other combinations are as follows:
    
    - ..._ID: Refer to a program by ID.
    - ..._LINK: Refer to a link by FD.
    - ..._LINK | ..._ID: Refer to a link by ID.
    
    There is a special case which allows replacing a program with another
    program by specifying BPF_F_REPLACE. It's not possible to replace a
    link.
    
    The flag behaviour is pretty subtle, so the Go API doesn't expose
    it directly. Instead a new concept called an Anchor is introduced,
    which bundles a position with a reference to a program or link.
    A couple of constructors are used to create only valid combinations.
    
    bpf_mprog also introduces the concept of a revision which changes
    with each modification of the list of attached programs. User space
    can pass an expected revision when creating a new link. The kernel
    then rejects the update if the revision has changed.
    
    Breaking change: this commit removes RawAttachProgramOptions.Replace.
    The fix is to set RawAttachProgramOptions.Anchor to ReplaceProgram(...).
    
    Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

--- link/program_test.go
+++ link/program_test.go
@@ -1,10 +1,14 @@
 package link
 
 import (
+	"fmt"
+	"net"
 	"testing"
 
 	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/internal/testutils"
+
+	qt "github.com/frankban/quicktest"
 )
 
 func TestProgramAlter(t *testing.T) {
@@ -41,4 +45,91 @@ func TestProgramAlter(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+}
+
+func TestRawAttachProgramAnchor(t *testing.T) {
+	testutils.SkipOnOldKernel(t, "6.6", "attach anchor")
+
+	iface, err := net.InterfaceByName("lo")
+	qt.Assert(t, err, qt.IsNil)
+
+	a := mustLoadProgram(t, ebpf.SchedCLS, 0, "")
+	info, err := a.Info()
+	qt.Assert(t, err, qt.IsNil)
+	aID, _ := info.ID()
+
+	err = RawAttachProgram(RawAttachProgramOptions{
+		Target:  iface.Index,
+		Program: a,
+		Attach:  ebpf.AttachTCXIngress,
+	})
+	qt.Assert(t, err, qt.IsNil)
+	defer RawDetachProgram(RawDetachProgramOptions{
+		Target:  iface.Index,
+		Program: a,
+		Attach:  ebpf.AttachTCXIngress,
+	})
+
+	link, err := AttachTCX(TCXOptions{
+		Interface: iface.Index,
+		Program:   mustLoadProgram(t, ebpf.SchedCLS, 0, ""),
+		Attach:    ebpf.AttachTCXIngress,
+	})
+	qt.Assert(t, err, qt.IsNil)
+	defer link.Close()
+
+	linkInfo, err := link.Info()
+	qt.Assert(t, err, qt.IsNil)
+
+	b := mustLoadProgram(t, ebpf.SchedCLS, 0, "")
+
+	for _, anchor := range []Anchor{
+		First(),
+		Last(),
+		AfterProgram(a),
+		AfterProgramByID(aID),
+		AfterLink(link),
+		AfterLinkByID(linkInfo.ID),
+	} {
+		t.Run(fmt.Sprintf("%T", anchor), func(t *testing.T) {
+			err := RawAttachProgram(RawAttachProgramOptions{
+				Target:  iface.Index,
+				Program: b,
+				Attach:  ebpf.AttachTCXIngress,
+				Anchor:  anchor,
+			})
+			qt.Assert(t, err, qt.IsNil)
+
+			// Detach doesn't allow first or last anchor.
+			if _, ok := anchor.(firstAnchor); ok {
+				anchor = nil
+			} else if _, ok := anchor.(lastAnchor); ok {
+				anchor = nil
+			}
+
+			err = RawDetachProgram(RawDetachProgramOptions{
+				Target:  iface.Index,
+				Program: b,
+				Attach:  ebpf.AttachTCXIngress,
+				Anchor:  anchor,
+			})
+			qt.Assert(t, err, qt.IsNil)
+		})
+	}
+
+	// Check that legacy replacement with a program works.
+	err = RawAttachProgram(RawAttachProgramOptions{
+		Target:  iface.Index,
+		Program: b,
+		Attach:  ebpf.AttachTCXIngress,
+		Anchor:  ReplaceProgram(a),
+	})
+	qt.Assert(t, err, qt.IsNil)
+
+	err = RawDetachProgram(RawDetachProgramOptions{
+		Target:  iface.Index,
+		Program: b,
+		Attach:  ebpf.AttachTCXIngress,
+	})
+	qt.Assert(t, err, qt.IsNil)
 }
---
+++ link/tcx_test.go
@@ -0,0 +1,90 @@
+package link
+
+import (
+	"fmt"
+	"math"
+	"net"
+	"testing"
+
+	qt "github.com/frankban/quicktest"
+
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/internal/testutils"
+	"github.com/cilium/ebpf/internal/unix"
+)
+
+func TestAttachTCX(t *testing.T) {
+	testutils.SkipOnOldKernel(t, "6.6", "TCX link")
+
+	prog := mustLoadProgram(t, ebpf.SchedCLS, ebpf.AttachNone, "")
+	link, _ := mustAttachTCX(t, prog, ebpf.AttachTCXIngress)
+
+	testLink(t, link, prog)
+}
+
+func TestTCXAnchor(t *testing.T) {
+	testutils.SkipOnOldKernel(t, "6.6", "TCX link")
+
+	a := mustLoadProgram(t, ebpf.SchedCLS, ebpf.AttachNone, "")
+	b := mustLoadProgram(t, ebpf.SchedCLS, ebpf.AttachNone, "")
+
+	linkA, iface := mustAttachTCX(t, a, ebpf.AttachTCXEgress)
+
+	programInfo, err := a.Info()
+	qt.Assert(t, err, qt.IsNil)
+	programID, _ := programInfo.ID()
+
+	linkInfo, err := linkA.Info()
+	qt.Assert(t, err, qt.IsNil)
+	linkID := linkInfo.ID
+
+	for _, anchor := range []Anchor{
+		First(),
+		Last(),
+		BeforeProgram(a),
+		BeforeProgramByID(programID),
+		AfterLink(linkA),
+		AfterLinkByID(linkID),
+	} {
+		t.Run(fmt.Sprintf("%T", anchor), func(t *testing.T) {
+			linkB, err := AttachTCX(TCXOptions{
+				Program:   b,
+				Attach:    ebpf.AttachTCXEgress,
+				Interface: iface,
+				Anchor:    anchor,
+			})
+			qt.Assert(t, err, qt.IsNil)
+			qt.Assert(t, linkB.Close(), qt.IsNil)
+		})
+	}
+}
+
+func TestTCXExpectedRevision(t *testing.T) {
+	testutils.SkipOnOldKernel(t, "6.6", "TCX link")
+
+	iface, err := net.InterfaceByName("lo")
+	qt.Assert(t, err, qt.IsNil)
+
+	_, err = AttachTCX(TCXOptions{
+		Program:          mustLoadProgram(t, ebpf.SchedCLS, ebpf.AttachNone, ""),
+		Attach:           ebpf.AttachTCXEgress,
+		Interface:        iface.Index,
+		ExpectedRevision: math.MaxUint64,
+	})
+	qt.Assert(t, err, qt.ErrorIs, unix.ESTALE)
+}
+
+func mustAttachTCX(tb testing.TB, prog *ebpf.Program, attachType ebpf.AttachType) (Link, int) {
+	iface, err := net.InterfaceByName("lo")
+	qt.Assert(tb, err, qt.IsNil)
+
+	link, err := AttachTCX(TCXOptions{
+		Program:   prog,
+		Attach:    attachType,
+		Interface: iface.Index,
+	})
+	qt.Assert(tb, err, qt.IsNil)
+	tb.Cleanup(func() { qt.Assert(tb, link.Close(), qt.IsNil) })
+
+	return link, iface.Index
+}

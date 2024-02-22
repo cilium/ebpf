5afd9dcb0a9e0ace399adc9c72356d9702865981
Lorenz Bauer <lmb@isovalent.com>
2024-02-22 14:41:23 +0000

    btf: move kernel spec parsing to separate file
    No changes to the source code.
    
    Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

--- btf/btf_test.go
+++ btf/btf_test.go
@@ -320,17 +320,6 @@ func TestVerifierError(t *testing.T) {
 	}
 }
 
-func TestLoadKernelSpec(t *testing.T) {
-	if _, err := os.Stat("/sys/kernel/btf/vmlinux"); os.IsNotExist(err) {
-		t.Skip("/sys/kernel/btf/vmlinux not present")
-	}
-
-	_, err := LoadKernelSpec()
-	if err != nil {
-		t.Fatal("Can't load kernel spec:", err)
-	}
-}
-
 func TestGuessBTFByteOrder(t *testing.T) {
 	bo := guessRawBTFByteOrder(vmlinuxTestdataReader(t))
 	if bo != binary.LittleEndian {
---
+++ btf/kernel_test.go
@@ -0,0 +1,17 @@
+package btf
+
+import (
+	"os"
+	"testing"
+)
+
+func TestLoadKernelSpec(t *testing.T) {
+	if _, err := os.Stat("/sys/kernel/btf/vmlinux"); os.IsNotExist(err) {
+		t.Skip("/sys/kernel/btf/vmlinux not present")
+	}
+
+	_, err := LoadKernelSpec()
+	if err != nil {
+		t.Fatal("Can't load kernel spec:", err)
+	}
+}

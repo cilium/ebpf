d59bf70689803096b03c857c8f1a1f01b6056bb3
Lorenz Bauer <lmb@isovalent.com>
2024-02-22 14:43:12 +0000

    btf: add a test for LoadKernelModuleSpec
    Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

--- btf/kernel_test.go
+++ btf/kernel_test.go
@@ -3,6 +3,8 @@ package btf
 import (
 	"os"
 	"testing"
+
+	"github.com/go-quicktest/qt"
 )
 
 func TestLoadKernelSpec(t *testing.T) {
@@ -14,4 +16,13 @@ func TestLoadKernelSpec(t *testing.T) {
 	if err != nil {
 		t.Fatal("Can't load kernel spec:", err)
 	}
+}
+
+func TestLoadKernelModuleSpec(t *testing.T) {
+	if _, err := os.Stat("/sys/kernel/btf/btf_testmod"); os.IsNotExist(err) {
+		t.Skip("/sys/kernel/btf/btf_testmod not present")
+	}
+
+	_, err := LoadKernelModuleSpec("btf_testmod")
+	qt.Assert(t, qt.IsNil(err))
 }

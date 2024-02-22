55302a5799bef2ea2b83d8c2922003c4d1314cd4
Lorenz Bauer <lmb@isovalent.com>
2024-02-22 15:05:23 +0000

    ebpf: remove FlushKernelModuleCache
    The function is already invoked via btf.FlushKernelSpec().
    
    Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

--- kallsyms.go
+++
@@ -1,8 +0,0 @@
-package ebpf
-
-import "github.com/cilium/ebpf/internal/kallsyms"
-
-// FlushKernelModuleCache removes any cached information about function to kernel module mapping.
-func FlushKernelModuleCache() {
-	kallsyms.FlushKernelModuleCache()
-}

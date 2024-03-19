fb4b0b8789ee8c0d79ac96bf7ffdf60cca1fc3b8
Wenlong Zhang <zhangwenlong@loongson.cn>
2024-03-19 17:27:08 +0800

    internal: add support for loong64
    Signed-off-by: Wenlong Zhang <zhangwenlong@loongson.cn>

--- internal/endian_be.go
+++ internal/endian_be.go
@@ -1,4 +1,4 @@
-//go:build armbe || arm64be || mips || mips64 || mips64p32 || ppc64 || s390 || s390x || sparc || sparc64
+//go:build armbe || arm64be || loong64 || mips || mips64 || mips64p32 || ppc64 || s390 || s390x || sparc || sparc64
 
 package internal
 

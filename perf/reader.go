cilium/ebpf

commit 75bce38196834d5a270d568ef2f771be8f858856

perf: opportunistically check all rings for data
Waking up user space from the kernel is expensive and so the perf reader
allows adjusting the rate at which wakeups happen. This saves CPU at the
cost of latency: some data will remain in the buffer for longer.

The reader is an abstraction over multiple ring buffers. It only reads
from a ring buffer if it has received a wakeup from the kernel. This is
wasteful because wakeups are expensive (due to context switching
and so on) but checking a ring for contents is cheap (just an atomic load).

Change the behaviour so that we read data from any ready ring buffer
regardless of why we were woken up.

Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

perf/reader.go
@@ -31,10 +31,6 @@ type perfEventHeader struct {
 	Size uint16
 }
 
-func cpuForEvent(event *unix.EpollEvent) int {
-	return int(event.Pad)
-}
-
 // Record contains either a sample or a counter of the
 // number of lost samples.
 type Record struct {
@@ -241,7 +237,7 @@ func NewReaderWithOptions(array *ebpf.Map, perCPUBuffer int, opts ReaderOptions)
 		rings = append(rings, ring)
 		eventFds = append(eventFds, event)
 
-		if err := poller.Add(event.Int(), i); err != nil {
+		if err := poller.Add(event.Int(), 0); err != nil {
 			return nil, err
 		}
 	}
@@ -361,7 +357,7 @@ func (pr *Reader) ReadInto(rec *Record) error {
 			// NB: The deferred pauseMu.Unlock will panic if Wait panics, which
 			// might obscure the original panic.
 			pr.pauseMu.Unlock()
-			nEvents, err := pr.poller.Wait(pr.epollEvents, pr.deadline)
+			_, err := pr.poller.Wait(pr.epollEvents, pr.deadline)
 			pr.pauseMu.Lock()
 			if err != nil {
 				return err
@@ -372,14 +368,11 @@ func (pr *Reader) ReadInto(rec *Record) error {
 				return errMustBePaused
 			}
 
-			for _, event := range pr.epollEvents[:nEvents] {
-				ring := pr.rings[cpuForEvent(&event)]
-				pr.epollRings = append(pr.epollRings, ring)
-
-				// Read the current head pointer now, not every time
-				// we read a record. This prevents a single fast producer
-				// from keeping the reader busy.
+			// Waking up userspace is expensive, make the most of it by checking
+			// all rings.
+			for _, ring := range pr.rings {
 				ring.loadHead()
+				pr.epollRings = append(pr.epollRings, ring)
 			}
 		}
 

commit 9bd3c365ccd2c7c312d72114cd564e600255097b

perf: fix panic when CPU is offline
Commit 75bce381 ("perf: opportunistically check all rings for data") introduced a panic on systems where not all CPUs are online. Fix this by skipping nil rings.

Signed-off-by: zhuhuijun <zhuhuijunzhj@gmail.com>

perf/reader.go
@@ -386,6 +386,11 @@ func (pr *Reader) ReadInto(rec *Record) error {
 			// Waking up userspace is expensive, make the most of it by checking
 			// all rings.
 			for _, ring := range pr.rings {
+				// Skip rings that are not currently enabled.
+				if ring == nil {
+					continue
+				}
+
 				ring.loadHead()
 				pr.epollRings = append(pr.epollRings, ring)
 			}
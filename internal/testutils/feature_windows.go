586cb1f66b672bf421608cf4a824201e5322d063
Lorenz Bauer <lmb@isovalent.com>
2024-07-31 16:23:14 +0200

    internal/testutils: make feature test helpers platform agnostic
    Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

---
+++ internal/testutils/feature_windows.go
@@ -0,0 +1,27 @@
+package testutils
+
+import (
+	"runtime"
+	"testing"
+
+	"github.com/cilium/ebpf/internal"
+)
+
+func SkipOnOldKernel(tb testing.TB, minVersion, feature string) {
+	tb.Helper()
+	tb.Logf("Ignoring version constraint %s for %s on %s", minVersion, feature, runtime.GOOS)
+}
+
+func IsKernelLessThan(tb testing.TB, minVersion string) bool {
+	tb.Helper()
+	tb.Logf("Ignoring version constraint %s on %s", minVersion, runtime.GOOS)
+	return false
+}
+
+func runtimeVersion(tb testing.TB) internal.Version {
+	tb.Helper()
+	// TODO(windows): We need a function which exposes the efW runtime version.
+	// Probably need to contribute this upstream.
+	tb.Fatal("runtimeVersion() not implemented yet")
+	return internal.Version{}
+}

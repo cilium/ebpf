790f5ab067e157127ff6aca6a02119efc329b912
Lorenz Bauer <lmb@isovalent.com>
2024-08-08 12:21:29 +0200

    link: minimal windows support WIP
    We retain the Link interface and the RawLink type, but all other link
    types are not available on Windows. There is a possibility that we can
    extend the XDP link to Windows, but that will have to come later.
    
    Attaching a link has to call into the efW runtime directly since
    the interface can't be mapped to Linux's BPF_LINK_CREATE:
    
    - Program and attach types are represented as GUID, which don't fit
      the 32bit allocated by Linux.
    
    Testing the Link attach code is cumbersome since the only way to load
    a program via a signed Windows driver.
    
    Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

---
+++ link/link_linux_test.go
@@ -0,0 +1,172 @@
+package link
+
+import (
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/asm"
+	"github.com/cilium/ebpf/internal/sys"
+	"github.com/cilium/ebpf/internal/testutils"
+	"github.com/go-quicktest/qt"
+	"golang.org/x/sys/unix"
+)
+
+func testLinkArch(t *testing.T, link Link) {
+	t.Run("link/info", func(t *testing.T) {
+		info, err := link.Info()
+		testutils.SkipIfNotSupported(t, err)
+		if err != nil {
+			t.Fatal("Link info returns an error:", err)
+		}
+
+		if info.Type == 0 {
+			t.Fatal("Failed to get link info type")
+		}
+
+		switch info.Type {
+		case sys.BPF_LINK_TYPE_TRACING:
+			if info.Tracing() == nil {
+				t.Fatalf("Failed to get link tracing extra info")
+			}
+		case sys.BPF_LINK_TYPE_CGROUP:
+			cg := info.Cgroup()
+			if cg.CgroupId == 0 {
+				t.Fatalf("Failed to get link Cgroup extra info")
+			}
+		case sys.BPF_LINK_TYPE_NETNS:
+			netns := info.NetNs()
+			if netns.AttachType == 0 {
+				t.Fatalf("Failed to get link NetNs extra info")
+			}
+		case sys.BPF_LINK_TYPE_XDP:
+			xdp := info.XDP()
+			if xdp.Ifindex == 0 {
+				t.Fatalf("Failed to get link XDP extra info")
+			}
+		case sys.BPF_LINK_TYPE_TCX:
+			tcx := info.TCX()
+			if tcx.Ifindex == 0 {
+				t.Fatalf("Failed to get link TCX extra info")
+			}
+		case sys.BPF_LINK_TYPE_NETFILTER:
+			nf := info.Netfilter()
+			if nf.Priority == 0 {
+				t.Fatalf("Failed to get link Netfilter extra info")
+			}
+		case sys.BPF_LINK_TYPE_KPROBE_MULTI:
+			// test default Info data
+			kmulti := info.KprobeMulti()
+			if count, ok := kmulti.AddressCount(); ok {
+				qt.Assert(t, qt.Not(qt.Equals(count, 0)))
+
+				_, ok = kmulti.Missed()
+				qt.Assert(t, qt.IsTrue(ok))
+				// NB: We don't check that missed is actually correct
+				// since it's not easy to trigger from tests.
+			}
+		case sys.BPF_LINK_TYPE_PERF_EVENT:
+			// test default Info data
+			pevent := info.PerfEvent()
+			switch pevent.Type {
+			case sys.BPF_PERF_EVENT_KPROBE, sys.BPF_PERF_EVENT_KRETPROBE:
+				kp := pevent.Kprobe()
+				if addr, ok := kp.Address(); ok {
+					qt.Assert(t, qt.Not(qt.Equals(addr, 0)))
+
+					_, ok := kp.Missed()
+					qt.Assert(t, qt.IsTrue(ok))
+					// NB: We don't check that missed is actually correct
+					// since it's not easy to trigger from tests.
+				}
+			}
+		}
+	})
+
+	type FDer interface {
+		FD() int
+	}
+
+	t.Run("from fd", func(t *testing.T) {
+		fder, ok := link.(FDer)
+		if !ok {
+			t.Skip("Link doesn't allow retrieving FD")
+		}
+
+		// We need to dup the FD since NewLinkFromFD takes
+		// ownership.
+		dupFD, err := unix.FcntlInt(uintptr(fder.FD()), unix.F_DUPFD_CLOEXEC, 1)
+		if err != nil {
+			t.Fatal("Can't dup link FD:", err)
+		}
+		defer unix.Close(dupFD)
+
+		newLink, err := NewFromFD(dupFD)
+		testutils.SkipIfNotSupported(t, err)
+		if err != nil {
+			t.Fatal("Can't create new link from dup link FD:", err)
+		}
+		defer newLink.Close()
+
+		if reflect.TypeOf(newLink) != reflect.TypeOf(link) {
+			t.Fatalf("Expected type %T, got %T", link, newLink)
+		}
+	})
+}
+
+func newRawLink(t *testing.T) (*RawLink, *ebpf.Program) {
+	t.Helper()
+
+	cgroup, prog := mustCgroupFixtures(t)
+	link, err := AttachRawLink(RawLinkOptions{
+		Target:  int(cgroup.Fd()),
+		Program: prog,
+		Attach:  ebpf.AttachCGroupInetEgress,
+	})
+	testutils.SkipIfNotSupported(t, err)
+	if err != nil {
+		t.Fatal("Can't create raw link:", err)
+	}
+	t.Cleanup(func() { link.Close() })
+
+	return link, prog
+}
+
+func mustCgroupFixtures(t *testing.T) (*os.File, *ebpf.Program) {
+	t.Helper()
+
+	testutils.SkipIfNotSupported(t, haveProgAttach())
+
+	return testutils.CreateCgroup(t), mustLoadProgram(t, ebpf.CGroupSKB, 0, "")
+}
+
+func mustLoadProgram(tb testing.TB, typ ebpf.ProgramType, attachType ebpf.AttachType, attachTo string) *ebpf.Program {
+	tb.Helper()
+
+	license := "MIT"
+	switch typ {
+	case ebpf.RawTracepoint, ebpf.LSM:
+		license = "GPL"
+	}
+
+	prog, err := ebpf.NewProgram(&ebpf.ProgramSpec{
+		Type:       typ,
+		AttachType: attachType,
+		AttachTo:   attachTo,
+		License:    license,
+		Instructions: asm.Instructions{
+			asm.Mov.Imm(asm.R0, 0),
+			asm.Return(),
+		},
+	})
+	if err != nil {
+		tb.Fatal(err)
+	}
+
+	tb.Cleanup(func() {
+		prog.Close()
+	})
+
+	return prog
+}

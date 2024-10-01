449a94e283c5bb8f92dd5766a3d1f502e778b3ad
Timo Beckers <timo@isovalent.com>
2024-10-01 13:53:22 +0200

    testdata: split VariableSpec-related tests into variables.c
    This is getting a bit unwieldy with Variable tests being added
    here in a follow-up commit. Split them off into variables.c.
    
    Signed-off-by: Timo Beckers <timo@isovalent.com>

--- variable_test.go
+++ variable_test.go
@@ -9,43 +9,42 @@ import (
 )
 
 func TestVariableSpec(t *testing.T) {
-	file := testutils.NativeFile(t, "testdata/loader-%s.elf")
+	file := testutils.NativeFile(t, "testdata/variables-%s.elf")
 	spec, err := LoadCollectionSpec(file)
-	if err != nil {
-		t.Fatal(err)
-	}
+	qt.Assert(t, qt.IsNil(err))
 
 	qt.Assert(t, qt.IsNil(spec.Variables["hidden"]))
+	qt.Assert(t, qt.IsNil(spec.Variables["unreferenced"]))
 	qt.Assert(t, qt.IsNotNil(spec.Variables["weak"]))
 
 	const want uint32 = 12345
 
 	// Update a variable in each type of data section (.bss,.data,.rodata)
-	qt.Assert(t, qt.IsNil(spec.Variables["key1"].Set(want)))
-	qt.Assert(t, qt.IsNil(spec.Variables["key2"].Set(want)))
-	qt.Assert(t, qt.IsNil(spec.Variables["key3"].Set(want)))
+	qt.Assert(t, qt.IsNil(spec.Variables["var_bss"].Set(want)))
+	qt.Assert(t, qt.IsNil(spec.Variables["var_data"].Set(want)))
+	qt.Assert(t, qt.IsNil(spec.Variables["var_rodata"].Set(want)))
 
 	var v uint32
-	qt.Assert(t, qt.IsNil(spec.Variables["key1"].Get(&v)))
+	qt.Assert(t, qt.IsNil(spec.Variables["var_bss"].Get(&v)))
 	qt.Assert(t, qt.Equals(v, want))
-	qt.Assert(t, qt.IsNil(spec.Variables["key2"].Get(&v)))
+	qt.Assert(t, qt.IsNil(spec.Variables["var_data"].Get(&v)))
 	qt.Assert(t, qt.Equals(v, want))
-	qt.Assert(t, qt.IsNil(spec.Variables["key3"].Get(&v)))
+	qt.Assert(t, qt.IsNil(spec.Variables["var_rodata"].Get(&v)))
 	qt.Assert(t, qt.Equals(v, want))
 
 	// Composite values.
 	type structT struct {
 		A, B uint64
 	}
-	qt.Assert(t, qt.IsNil(spec.Variables["struct_var"].Set(&structT{1, 2})))
+	qt.Assert(t, qt.IsNil(spec.Variables["var_struct"].Set(&structT{1, 2})))
 
 	var s structT
-	qt.Assert(t, qt.IsNil(spec.Variables["struct_var"].Get(&s)))
+	qt.Assert(t, qt.IsNil(spec.Variables["var_struct"].Get(&s)))
 	qt.Assert(t, qt.Equals(s, structT{1, 2}))
 }
 
 func TestVariableSpecCopy(t *testing.T) {
-	file := testutils.NativeFile(t, "testdata/loader-%s.elf")
+	file := testutils.NativeFile(t, "testdata/variables-%s.elf")
 	spec, err := LoadCollectionSpec(file)
 	if err != nil {
 		t.Fatal(err)
@@ -53,13 +52,13 @@ func TestVariableSpecCopy(t *testing.T) {
 
 	cpy := spec.Copy()
 
-	// Update a variable in a section with only a single variable (.rodata.test).
+	// Update a variable in a section with only a single variable (.rodata).
 	const want uint32 = 0xfefefefe
 	wantb := []byte{0xfe, 0xfe, 0xfe, 0xfe} // Same byte sequence regardless of endianness
-	qt.Assert(t, qt.IsNil(cpy.Variables["arg2"].Set(want)))
-	qt.Assert(t, qt.DeepEquals(cpy.Maps[".rodata.test"].Contents[0].Value.([]byte), wantb))
+	qt.Assert(t, qt.IsNil(cpy.Variables["var_rodata"].Set(want)))
+	qt.Assert(t, qt.DeepEquals(cpy.Maps[".rodata"].Contents[0].Value.([]byte), wantb))
 
 	// Verify that the original underlying MapSpec was not modified.
 	zero := make([]byte, 4)
-	qt.Assert(t, qt.DeepEquals(spec.Maps[".rodata.test"].Contents[0].Value.([]byte), zero))
+	qt.Assert(t, qt.DeepEquals(spec.Maps[".rodata"].Contents[0].Value.([]byte), zero))
 }

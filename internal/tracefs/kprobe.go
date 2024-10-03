ec73a047c2fc30758ce8159d2d3c1c2447310ddb
Timo Beckers <timo@isovalent.com>
2024-10-03 14:29:22 +0200

    link: tracefs: expect EINVAL when creating tracefs kprobes against missing symbols
    As highlighted in the code, Linux commit 9d8616034f16 likely introduced a bug that
    causes writes to <tracefs>/kprobe_events to fail with EINVAL if the target symbol
    doesn't exist. Update the tests to reflect that behaviour. At least now it's on par
    with kretprobes..
    
    Also removed a tautological condition in NewEvent.
    
    Signed-off-by: Timo Beckers <timo@isovalent.com>

--- internal/tracefs/kprobe.go
+++ internal/tracefs/kprobe.go
@@ -214,7 +214,10 @@ func NewEvent(args ProbeArgs) (*Event, error) {
 	if err == nil {
 		return nil, fmt.Errorf("trace event %s/%s: %w", args.Group, eventName, os.ErrExist)
 	}
-	if err != nil && !errors.Is(err, os.ErrNotExist) {
+	if errors.Is(err, unix.EINVAL) {
+		return nil, fmt.Errorf("trace event %s/%s: %w (unknown symbol?)", args.Group, eventName, err)
+	}
+	if !errors.Is(err, os.ErrNotExist) {
 		return nil, fmt.Errorf("checking trace event %s/%s: %w", args.Group, eventName, err)
 	}
 

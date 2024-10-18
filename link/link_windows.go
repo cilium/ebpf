238b2189b90abfc630065f36d12de139aeae156d
Lorenz Bauer <lmb@isovalent.com>
2024-10-18 08:57:19 -0700

    link: minimal windows support WIP
    We retain the Link interface and the RawLink type, but all other link
    types are not available on Windows. There is a possibility that we can
    extend the XDP link to Windows, but that will have to come later.
    
    Attaching a link has to call into the efW runtime directly since
    the interface can't be mapped to Linux's BPF_LINK_CREATE:
    
    - Program and attach types are represented as GUID, which don't fit
      the 32bit allocated by Linux.
    
    Testing the Link attach code is cumbersome since the only way to load
    a program via a signed Windows driver.
    
    Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

---
+++ link/link_windows.go
@@ -0,0 +1,71 @@
+package link
+
+import (
+	"fmt"
+	"runtime"
+	"unsafe"
+
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/internal"
+	"github.com/cilium/ebpf/internal/efw"
+	"github.com/cilium/ebpf/internal/sys"
+)
+
+// ebpf_result_t ebpf_program_attach_by_fd(
+// fd_t program_fd,
+// _In_opt_ const ebpf_attach_type_t* attach_type,
+// _In_reads_bytes_opt_(attach_parameters_size) void* attach_parameters,
+// size_t attach_parameters_size,
+// _Outptr_ struct bpf_link** link)
+var ebpfProgramAttachByFd = efw.Module.NewProc("ebpf_program_attach_by_fd")
+
+// fd_t ebpf_link_fd(_Frees_ptr_ struct bpf_link* link)
+var ebpfLinkFree = efw.Module.NewProc("ebpf_link_free")
+
+// const ebpf_attach_type_t* ebpf_get_ebpf_attach_type(bpf_attach_type_t bpf_attach_type)
+var ebpfGetEbpfAttachType = efw.Module.NewProc("ebpf_get_ebpf_attach_type")
+
+func AttachRawLink(opts RawLinkOptions) (*RawLink, error) {
+	if opts.Target != 0 || opts.BTF != 0 || opts.Flags != 0 {
+		return nil, fmt.Errorf("specified option(s) %w", internal.ErrNotSupportedOnOS)
+	}
+
+	p, attachType := opts.Attach.Decode()
+	if p != ebpf.Windows {
+		return nil, fmt.Errorf("attach type %s: %w", opts.Attach, internal.ErrNotSupportedOnOS)
+	}
+
+	attachTypeGUID, err := efw.CallPointer(ebpfGetEbpfAttachType, uintptr(attachType))
+	if err != nil {
+		return nil, fmt.Errorf("get attach type: %w", err)
+	}
+
+	var link uintptr
+	err = efw.CallResult(ebpfProgramAttachByFd,
+		uintptr(opts.Program.FD()),
+		attachTypeGUID,
+		0, // attach_parameters
+		0, // attach_parameters_size
+		uintptr(unsafe.Pointer(&link)),
+	)
+	runtime.KeepAlive(opts.Program)
+	if err != nil {
+		return nil, fmt.Errorf("attach link: %w", err)
+	}
+
+	raw, err := efw.CallFd(ebpfLinkFree, link)
+	if err != nil {
+		return nil, fmt.Errorf("link fd: %w", err)
+	}
+
+	fd, err := sys.NewFD(int(raw))
+	if err != nil {
+		return nil, err
+	}
+
+	return &RawLink{fd: fd}, nil
+}
+
+func wrapRawLink(raw *RawLink) (Link, error) {
+	return raw, nil
+}

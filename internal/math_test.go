f2831062a1f49c984adfe973a866d2ce77c0b066
Timo Beckers <timo@isovalent.com>
2024-12-18 11:40:09 +0100

    internal: add Between function for clamping a value between min and max
    Signed-off-by: Timo Beckers <timo@isovalent.com>

--- internal/math_test.go
+++ internal/math_test.go
@@ -7,6 +7,8 @@ import (
 	"slices"
 	"strings"
 	"testing"
+
+	"github.com/go-quicktest/qt"
 )
 
 func TestPow(t *testing.T) {
@@ -49,5 +51,24 @@ func TestIntegerConstraint(t *testing.T) {
 		if !slices.Contains(integers, name) {
 			t.Errorf("Go type %s is not in the list of known integer types", name)
 		}
+	}
+}
+
+func TestBetween(t *testing.T) {
+	tests := []struct {
+		val, a, b, r int
+	}{
+		{0, 1, 2, 1},
+		{1, 1, 2, 1},
+		{2, 1, 2, 2},
+		{3, 1, 2, 2},
+		{4, 10, 5, 5},
+		{11, 10, 5, 10},
+	}
+
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%d between %d and %d", tt.val, tt.a, tt.b), func(t *testing.T) {
+			qt.Assert(t, qt.Equals(Between(tt.val, tt.a, tt.b), tt.r))
+		})
 	}
 }

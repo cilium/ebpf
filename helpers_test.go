fb916d0ecca069e06f6f6bc6f034d15ec006287e
Timo Beckers <timo@isovalent.com>
2025-01-15 14:03:12 +0100

    prog: memoize haveTestmod, add requireTestmod helper
    This allows calling requireTestmod in a loop for marking subtests as requiring
    bpf_testmod.
    
    Signed-off-by: Timo Beckers <timo@isovalent.com>

--- helpers_test.go
+++ helpers_test.go
@@ -2,25 +2,36 @@ package ebpf
 
 import (
 	"errors"
+	"sync"
 	"testing"
 
 	"github.com/cilium/ebpf/btf"
 	"github.com/cilium/ebpf/internal/testutils"
 )
 
-func haveTestmod(tb testing.TB) bool {
-	haveTestmod := false
-	if !testutils.IsKernelLessThan(tb, "5.11") {
-		// See https://github.com/torvalds/linux/commit/290248a5b7d829871b3ea3c62578613a580a1744
-		testmod, err := btf.FindHandle(func(info *btf.HandleInfo) bool {
-			return info.IsModule() && info.Name == "bpf_testmod"
-		})
-		if err != nil && !errors.Is(err, btf.ErrNotFound) {
-			tb.Fatal(err)
-		}
-		haveTestmod = testmod != nil
-		testmod.Close()
+var haveTestmod = sync.OnceValues(func() (bool, error) {
+	// See https://github.com/torvalds/linux/commit/290248a5b7d829871b3ea3c62578613a580a1744
+	testmod, err := btf.FindHandle(func(info *btf.HandleInfo) bool {
+		return info.IsModule() && info.Name == "bpf_testmod"
+	})
+	if err != nil && !errors.Is(err, btf.ErrNotFound) {
+		return false, err
 	}
+	testmod.Close()
 
-	return haveTestmod
+	return testmod != nil, nil
+})
+
+func requireTestmod(tb testing.TB) {
+	tb.Helper()
+
+	testutils.SkipOnOldKernel(tb, "5.11", "bpf_testmod")
+
+	testmod, err := haveTestmod()
+	if err != nil {
+		tb.Fatal(err)
+	}
+	if !testmod {
+		tb.Skip("bpf_testmod not loaded")
+	}
 }

324d2fb0797af88ee69e836435c7756c436e8fd8
Lorenz Bauer <lmb@isovalent.com>
2025-01-20 15:25:27 +0000

    link: minimal windows support WIP
    We retain the Link interface and the RawLink type, but all other link
    types are not available on Windows. There is a possibility that we can
    extend the XDP link to Windows, but that will have to come later.
    
    There is no support to retrieve link information or to pass additional
    context to the link create call.
    
    - Can probably add a translation layer for BPF_LINK_CREATE
    
    Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

---
+++ link/helpers_windows_test.go
@@ -0,0 +1,33 @@
+package link
+
+import (
+	"testing"
+
+	"golang.org/x/sys/windows"
+
+	"github.com/go-quicktest/qt"
+
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/internal/efw"
+	"github.com/cilium/ebpf/internal/platform"
+)
+
+// windowsProgramTypeFromGUID resolves a GUID to a ProgramType.
+func windowsProgramTypeFromGUID(tb testing.TB, guid windows.GUID) ebpf.ProgramType {
+	rawProgramType, err := efw.EbpfGetBpfProgramType(guid)
+	qt.Assert(tb, qt.IsNil(err))
+
+	typ, err := ebpf.ProgramTypeForPlatform(platform.Windows, rawProgramType)
+	qt.Assert(tb, qt.IsNil(err))
+	return typ
+}
+
+// windowsAttachTypeFromGUID resolves a GUID to an AttachType.
+func windowsAttachTypeFromGUID(tb testing.TB, guid windows.GUID) ebpf.AttachType {
+	rawAttachType, err := efw.EbpfGetBpfAttachType(guid)
+	qt.Assert(tb, qt.IsNil(err))
+
+	typ, err := ebpf.AttachTypeForPlatform(platform.Windows, rawAttachType)
+	qt.Assert(tb, qt.IsNil(err))
+	return typ
+}

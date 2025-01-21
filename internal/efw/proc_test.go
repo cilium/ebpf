5f04d37b4c35e324851abd855ce5debc7aae1573
Lorenz Bauer <lmb@isovalent.com>
2025-01-21 02:59:53 -0800

    efw: add wrappers for eBPF for Windows runtime
    Add basic infrastructure to call into the eBPF for Windows runtime.
    This uses runtime dynamic linking to call into ebpfapi.dll instead
    of relying on CGo. Ultimately this boils down to using
    syscall.SyscallN to call into specific functions in the DLL and looks
    very similar to what we do on Linux.
    
    The most important function is a wrapper for the bpf() function in
    ebpfapi.dll. This is an ABI compatible re-implementation of the Linux
    bpf() syscall. Of course, not all of the syscall is implemented yet.
    
    In addition to that there are a variety of helper functions that do not
    have a bpf() equivalent and therefore need to be called directly.
    
    Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

---
+++ internal/efw/proc_test.go
@@ -0,0 +1,23 @@
+//go:build windows
+
+package efw
+
+import (
+	"testing"
+
+	"github.com/go-quicktest/qt"
+)
+
+func TestNewProc(t *testing.T) {
+	_, err := newProc("a_function_which_doesnt_exist").Find()
+	qt.Assert(t, qt.ErrorMatches(err, ".* a_function_which_doesnt_exist .*"))
+}
+
+func TestCall(t *testing.T) {
+	var err error
+	allocs := testing.AllocsPerRun(10, func() {
+		_, err = EbpfGetEbpfAttachType(1)
+	})
+	qt.Assert(t, qt.IsNil(err))
+	qt.Assert(t, qt.Equals(allocs, 0))
+}

7ed15900bfb14d2cd7fe56e39f5373d85a1c7dbc
Lorenz Bauer <lmb@isovalent.com>
2025-05-21 10:48:20 +0100

    collection: share kernel BTF when loading a collection
    We currently create a new copy of kernel BTF when loading each program in a
    collection, which is quite expensive.
    
    Introduce a new type btf.Cache which allows re-using kernel BTF as-needed.
    As an optimisation, the Cache is opportunistically populated from the global
    cache on creation.
    
        core: 1
        goos: linux
        goarch: amd64
        pkg: github.com/cilium/ebpf
        cpu: 13th Gen Intel(R) Core(TM) i7-1365U
                            │   base.txt   │             cache.txt              │
                            │    sec/op    │   sec/op     vs base               │
        NewCollectionManyProgs   347.84m ± 1%   14.54m ± 2%  -95.82% (p=0.002 n=6)
    
                            │    base.txt    │              cache.txt              │
                            │      B/op      │     B/op      vs base               │
        NewCollectionManyProgs   108.056Mi ± 0%   3.841Mi ± 0%  -96.44% (p=0.002 n=6)
    
                            │   base.txt    │             cache.txt              │
                            │   allocs/op   │  allocs/op   vs base               │
        NewCollectionManyProgs   1026.83k ± 0%   38.04k ± 0%  -96.30% (p=0.002 n=6)
    
    Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

--- btf/kernel_test.go
+++ btf/kernel_test.go
@@ -4,6 +4,8 @@ import (
 	"os"
 	"testing"
 
+	"github.com/cilium/ebpf/internal/testutils"
+
 	"github.com/go-quicktest/qt"
 )
 
@@ -25,4 +27,55 @@ func TestLoadKernelModuleSpec(t *testing.T) {
 
 	_, err := LoadKernelModuleSpec("btf_testmod")
 	qt.Assert(t, qt.IsNil(err))
+}
+
+func TestCache(t *testing.T) {
+	FlushKernelSpec()
+	c := NewCache()
+
+	qt.Assert(t, qt.IsNil(c.KernelTypes))
+	qt.Assert(t, qt.HasLen(c.KernelModules, 0))
+
+	// Test that Kernel() creates only one copy
+	spec1, err := c.Kernel()
+	testutils.SkipIfNotSupported(t, err)
+	qt.Assert(t, qt.IsNil(err))
+	qt.Assert(t, qt.IsNotNil(spec1))
+
+	spec2, err := c.Kernel()
+	qt.Assert(t, qt.IsNil(err))
+	qt.Assert(t, qt.IsNotNil(spec2))
+
+	qt.Assert(t, qt.Equals(spec1, spec2))
+
+	// Test that Module() creates only one copy
+	mod1, err := c.Module("bpf_testmod")
+	if !os.IsNotExist(err) {
+		qt.Assert(t, qt.IsNil(err))
+		qt.Assert(t, qt.IsNotNil(mod1))
+
+		mod2, err := c.Module("bpf_testmod")
+		qt.Assert(t, qt.IsNil(err))
+		qt.Assert(t, qt.IsNotNil(mod2))
+
+		qt.Assert(t, qt.Equals(mod1, mod2))
+	}
+
+	// Pre-populate global cache
+	vmlinux, err := LoadKernelSpec()
+	qt.Assert(t, qt.IsNil(err))
+
+	testmod, err := LoadKernelModuleSpec("bpf_testmod")
+	if !os.IsNotExist(err) {
+		qt.Assert(t, qt.IsNil(err))
+	}
+
+	// Test that NewCache populates from global cache
+	c = NewCache()
+	qt.Assert(t, qt.IsNotNil(c.KernelTypes))
+	qt.Assert(t, qt.Not(qt.Equals(c.KernelTypes, vmlinux)))
+	if testmod != nil {
+		qt.Assert(t, qt.IsNotNil(c.KernelModules["bpf_testmod"]))
+		qt.Assert(t, qt.Not(qt.Equals(c.KernelModules["bpf_testmod"], testmod)))
+	}
 }

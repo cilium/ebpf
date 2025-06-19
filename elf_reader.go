cilium/ebpf

commit 3f1a755f1451d058e8b56b516c7a64ff6f3a33e3

map: add MapSpec.Tags field for reading btf_decl_tag attributes set on maps
This commit exposes btf_decl_tag values set on map defintions in the following manner:

```
struct {
	__uint(type, BPF_MAP_TYPE_ARRAY);
  ...
} map_def __attribute__((btf_decl_tag("my_tag"))) __section(".maps");

```

The MapSpec.Tags field is currently read-only and doesn't affect what's being
loaded into the kernel.

Signed-off-by: Timo Beckers <timo@isovalent.com>

elf_reader.go
@@ -10,6 +10,7 @@ import (
 	"io"
 	"math"
 	"os"
+	"slices"
 	"strings"
 
 	"github.com/cilium/ebpf/asm"
@@ -724,6 +725,20 @@ func (ec *elfCode) loadMaps() error {
 			return fmt.Errorf("section %v: map descriptors are not of equal size", sec.Name)
 		}
 
+		// If the ELF has BTF, pull out the btf.Var for each map definition to
+		// extract decl tags from.
+		varsByName := make(map[string]*btf.Var)
+		var ds *btf.Datasec
+		if err := ec.btf.TypeByName(sec.Name, &ds); err == nil {
+			for _, vsi := range ds.Vars {
+				v, ok := btf.As[*btf.Var](vsi.Type)
+				if !ok {
+					return fmt.Errorf("section %v: btf.VarSecInfo doesn't point to a *btf.Var: %T", sec.Name, vsi.Type)
+				}
+				varsByName[string(v.Name)] = v
+			}
+		}
+
 		var (
 			r    = bufio.NewReader(sec.Open())
 			size = sec.Size / uint64(nSym)
@@ -763,6 +778,10 @@ func (ec *elfCode) loadMaps() error {
 			}
 			if len(extra) > 0 {
 				spec.Extra = bytes.NewReader(extra)
+			}
+
+			if v, ok := varsByName[mapName]; ok {
+				spec.Tags = slices.Clone(v.Tags)
 			}
 
 			ec.maps[mapName] = &spec
@@ -1028,6 +1047,11 @@ func mapSpecFromBTF(es *elfSection, vs *btf.VarSecinfo, def *btf.Struct, spec *b
 		valueSize = 0
 	}
 
+	v, ok := btf.As[*btf.Var](vs.Type)
+	if !ok {
+		return nil, fmt.Errorf("BTF map definition: btf.VarSecInfo doesn't point to a *btf.Var: %T", vs.Type)
+	}
+
 	return &MapSpec{
 		Name:       sanitizeName(name, -1),
 		Type:       MapType(mapType),
@@ -1040,6 +1064,7 @@ func mapSpecFromBTF(es *elfSection, vs *btf.VarSecinfo, def *btf.Struct, spec *b
 		Pinning:    pinType,
 		InnerMap:   innerMapSpec,
 		Contents:   contents,
+		Tags:       slices.Clone(v.Tags),
 	}, nil
 }
 

elf_reader_test.go
@@ -82,6 +82,22 @@ func TestLoadCollectionSpec(t *testing.T) {
 				MaxEntries: 1,
 				Pinning:    PinByName,
 			},
+			"bpf_decl_map": {
+				Name:       "bpf_decl_map",
+				Type:       Array,
+				KeySize:    4,
+				ValueSize:  8,
+				MaxEntries: 1,
+				Tags:       []string{"a", "b"},
+			},
+			"btf_decl_map": {
+				Name:       "btf_decl_map",
+				Type:       Array,
+				KeySize:    4,
+				ValueSize:  8,
+				MaxEntries: 1,
+				Tags:       []string{"a", "b"},
+			},
 			"btf_outer_map": {
 				Name:       "btf_outer_map",
 				Type:       ArrayOfMaps,

map.go
@@ -85,6 +85,11 @@ type MapSpec struct {
 
 	// The key and value type of this map. May be nil.
 	Key, Value btf.Type
+
+	// Tags is a list of btf_decl_tag attributes set on the map definition.
+	//
+	// Decorate a map definition with `__attribute__((btf_decl_tag("foo")))`.
+	Tags []string
 }
 
 func (ms *MapSpec) String() string {
@@ -103,6 +108,7 @@ func (ms *MapSpec) Copy() *MapSpec {
 	cpy.Contents = slices.Clone(cpy.Contents)
 	cpy.Key = btf.Copy(cpy.Key)
 	cpy.Value = btf.Copy(cpy.Value)
+	cpy.Tags = slices.Clone(cpy.Tags)
 
 	if cpy.InnerMap == ms {
 		cpy.InnerMap = &cpy

map_test.go
@@ -96,6 +96,7 @@ func TestMapSpecCopy(t *testing.T) {
 		bytes.NewReader(nil),
 		&btf.Int{},
 		&btf.Int{},
+		nil,
 	}
 	a.InnerMap = a
 

commit 52e4e1a3bb46a8c129d8976818b5c942b82a7a43

elf_reader: do not panic in `loadMaps`if ELF file has no BTF
3f1a755f1451d058e8b56b516c7a64ff6f3a33e3 introduced the ability to read
btf_decl_tag attributes set on maps but `loadMaps` is crashing if the
ELF file has no BTF.

Fix this by checking if the BTF is there before querying it.

Signed-off-by: Paul Cacheux <paul.cacheux@datadoghq.com>

elf_reader.go
@@ -728,14 +728,16 @@ func (ec *elfCode) loadMaps() error {
 		// If the ELF has BTF, pull out the btf.Var for each map definition to
 		// extract decl tags from.
 		varsByName := make(map[string]*btf.Var)
-		var ds *btf.Datasec
-		if err := ec.btf.TypeByName(sec.Name, &ds); err == nil {
-			for _, vsi := range ds.Vars {
-				v, ok := btf.As[*btf.Var](vsi.Type)
-				if !ok {
-					return fmt.Errorf("section %v: btf.VarSecInfo doesn't point to a *btf.Var: %T", sec.Name, vsi.Type)
+		if ec.btf != nil {
+			var ds *btf.Datasec
+			if err := ec.btf.TypeByName(sec.Name, &ds); err == nil {
+				for _, vsi := range ds.Vars {
+					v, ok := btf.As[*btf.Var](vsi.Type)
+					if !ok {
+						return fmt.Errorf("section %v: btf.VarSecInfo doesn't point to a *btf.Var: %T", sec.Name, vsi.Type)
+					}
+					varsByName[string(v.Name)] = v
 				}
-				varsByName[string(v.Name)] = v
 			}
 		}
 
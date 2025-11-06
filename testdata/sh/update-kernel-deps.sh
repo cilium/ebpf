8a100e37bb646c9c7984c4ac6da8c0a227b15147
Lorenz Bauer <lmb@isovalent.com>
2025-11-06 12:10:37 +0000

    btf, internal: fix inconsistent external dependencies
    It seems like we've checked in external dependencies that are
    not reproduced when running make update-kernel-deps.
    Fix this by changing the version number to a patch release and
    committing the newly generated files.
    
    Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

--- testdata/sh/update-kernel-deps.sh
+++ testdata/sh/update-kernel-deps.sh
@@ -31,7 +31,12 @@ cleanup() {
 trap cleanup EXIT
 
 # Download and process libbpf.c
-curl -fL "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/tools/lib/bpf/libbpf.c?h=v$KERNEL_VERSION" -o "$tmp/libbpf.c"
+# Truncate .0 patch versions (e.g., 6.16.0 -> 6.16, but leave 7.0 as 7.0)
+kernel_version_for_url="$KERNEL_VERSION"
+if [[ $KERNEL_VERSION =~ ^([0-9]+\.[0-9]+)\.0$ ]]; then
+	kernel_version_for_url="${BASH_REMATCH[1]}"
+fi
+curl -fL "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/tools/lib/bpf/libbpf.c?h=v$kernel_version_for_url" -o "$tmp/libbpf.c"
 "./internal/cmd/gensections.awk" "$tmp/libbpf.c" | gofmt > "./elf_sections.go"
 
 # Download and process vmlinux and btf_testmod

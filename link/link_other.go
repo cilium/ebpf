eb5072a6c7e5b96bce8c9b8b3d6a9ebf5daefd85
Max Altgelt <max.altgelt@nextron-systems.com>
2025-11-10 08:59:34 +0100

    feat: add dedicated types for protocol family / hooks
    The netfilter options / info required / contained protocol family
    and hook, with little information for the user what
    values were expected.
    Add explicit types with constants to clarify what values are
    supported.
    
    Signed-off-by: Max Altgelt <max.altgelt@nextron-systems.com>

--- link/link_other.go
+++ link/link_other.go
@@ -161,8 +161,8 @@ func resolveIfindex(ifindex uint32) (net.Interface, error) {
 }
 
 type NetfilterInfo struct {
-	Pf       uint32
-	Hooknum  uint32
+	Pf       NetfilterProtocolFamily
+	Hooknum  NetfilterHook
 	Priority int32
 	Flags    uint32
 }
--- link/netfilter_test.go
+++ link/netfilter_test.go
@@ -5,13 +5,10 @@ package link
 import (
 	"testing"
 
+	"github.com/go-quicktest/qt"
+
 	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/internal/testutils"
-)
-
-const (
-	NFPROTO_IPV4      = 0x2
-	NF_INET_LOCAL_OUT = 0x3
 )
 
 func TestAttachNetfilter(t *testing.T) {
@@ -21,8 +18,8 @@ func TestAttachNetfilter(t *testing.T) {
 
 	l, err := AttachNetfilter(NetfilterOptions{
 		Program:        prog,
-		ProtocolFamily: NFPROTO_IPV4,
-		HookNumber:     NF_INET_LOCAL_OUT,
+		ProtocolFamily: NetfilterProtoIPv4,
+		HookNumber:     NetfilterInetLocalOut,
 		Priority:       -128,
 	})
 	if err != nil {
@@ -30,4 +27,29 @@ func TestAttachNetfilter(t *testing.T) {
 	}
 
 	testLink(t, l, prog)
+}
+
+func TestNetfilterInfo(t *testing.T) {
+	testutils.SkipOnOldKernel(t, "6.4", "BPF_LINK_TYPE_NETFILTER")
+
+	prog := mustLoadProgram(t, ebpf.Netfilter, ebpf.AttachNetfilter, "")
+
+	l, err := AttachNetfilter(NetfilterOptions{
+		Program:        prog,
+		ProtocolFamily: NetfilterProtoIPv4,
+		HookNumber:     NetfilterInetLocalOut,
+		Priority:       -128,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer l.Close()
+	info, err := l.Info()
+	if err != nil {
+		t.Fatal(err)
+	}
+	nfInfo := info.Netfilter()
+	qt.Assert(t, qt.Equals(nfInfo.Pf, NetfilterProtoIPv4))
+	qt.Assert(t, qt.Equals(nfInfo.Hooknum, NetfilterInetLocalOut))
+	qt.Assert(t, qt.Equals(nfInfo.Priority, -128))
 }

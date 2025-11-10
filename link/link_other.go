c57ef7340ed62c21ce8bb583ffba54a773bfa7b3
Max Altgelt <max.altgelt@nextron-systems.com>
2025-11-10 08:59:34 +0100

    feat: add dedicated types for protocol family / hooks
    The netfilter options / info required / contained protocol family
    and hook, with little information for the user what
    values were expected.
    Add explicit types with constants to clarify what values are
    supported.
    
    Signed-off-by: Max Altgelt <max.altgelt@nextron-systems.com>

--- link/link_other.go
+++ link/link_other.go
@@ -137,8 +137,8 @@ type XDPInfo struct {
 }
 
 type NetfilterInfo struct {
-	Pf       uint32
-	Hooknum  uint32
+	Pf       NetfilterProtocolFamily
+	Hooknum  NetfilterInetHook
 	Priority int32
 	Flags    uint32
 }

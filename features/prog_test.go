318f05991f0344074d18817e1cd84dfbb5e4968a
Dylan Reimerink <dylan.reimerink@isovalent.com>
2026-09-17 16:40:39 +0200

    features: make `HaveProgramType` and `HaveProgramHelper` BTF based
    Up to now these two functions worked by actually trying to create an
    eBPF program to check for the availability of the specified program type
    or helper. This commit changes the implementation to query the kernels
    BTF to see if it has enum values for the given program type or helper.
    
    This new method is similar to how you would write CO-RE logic in a BPF
    program to check for the availability.
    
    This is however a breaking change, both functions now receive a
    `*btf.Cache` as their first argument, which is used to cache the kernel
    BTF between multiple calls. Secondly, in the case of `HaveProgramHelper`
    we no longer check if the helper is available for a specific program
    type, since that information is not exposed in BTF.
    
    These changes were made because the old method does not always work as
    expected. The verifier on newer kernels is getting increasingly stricter
    and enforces valid arguments for helper functions, which makes it
    unreasonably difficult to write a generic probe program.
    
    Additionally, the old method of probe programs does not work in
    restricted environments such as those where BPF tokens are in use or
    syscalls are restricted with LSM hooks. Reading kernel BTF is a normal
    and expected part of loading eBPF programs and is thus more reliable in
    restricted environments.
    
    Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

--- features/prog_test.go
+++ features/prog_test.go
@@ -1,99 +1,123 @@
 package features
 
 import (
-	"errors"
 	"fmt"
 	"math"
 	"testing"
 
 	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/asm"
-	"github.com/cilium/ebpf/internal"
+	"github.com/cilium/ebpf/btf"
 	"github.com/cilium/ebpf/internal/testutils"
-	"github.com/cilium/ebpf/internal/testutils/testmain"
 )
 
-func TestMain(m *testing.M) {
-	testmain.Run(m)
-}
-
 func TestHaveProgramType(t *testing.T) {
-	testutils.CheckFeatureMatrix(t, haveProgramTypeMatrix)
+	cache := btf.NewCache()
+	testCases := []struct {
+		progType ebpf.ProgramType
+		version  string
+	}{
+		{ebpf.SocketFilter, "3.19"},
+		{ebpf.Kprobe, "4.1"},
+		{ebpf.SchedCLS, "4.1"},
+		{ebpf.SchedACT, "4.1"},
+		{ebpf.TracePoint, "4.7"},
+		{ebpf.XDP, "4.8"},
+		{ebpf.PerfEvent, "4.9"},
+		{ebpf.CGroupSKB, "4.10"},
+		{ebpf.CGroupSock, "4.10"},
+		{ebpf.LWTIn, "4.10"},
+		{ebpf.LWTOut, "4.10"},
+		{ebpf.LWTXmit, "4.10"},
+		{ebpf.SockOps, "4.13"},
+		{ebpf.SkSKB, "4.14"},
+		{ebpf.CGroupDevice, "4.15"},
+		{ebpf.SkMsg, "4.17"},
+		{ebpf.RawTracepoint, "4.17"},
+		{ebpf.CGroupSockAddr, "4.17"},
+		{ebpf.LWTSeg6Local, "4.18"},
+		{ebpf.LircMode2, "4.18"},
+		{ebpf.SkReuseport, "4.19"},
+		{ebpf.FlowDissector, "4.20"},
+		{ebpf.CGroupSysctl, "5.2"},
+		{ebpf.RawTracepointWritable, "5.2"},
+		{ebpf.CGroupSockopt, "5.3"},
+		{ebpf.Tracing, "5.5"},
+		{ebpf.StructOps, "5.6"},
+		{ebpf.Extension, "5.6"},
+		{ebpf.LSM, "5.7"},
+		{ebpf.SkLookup, "5.9"},
+		{ebpf.Syscall, "5.14"},
+		{ebpf.Netfilter, "6.4"},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.progType.String(), func(t *testing.T) {
+			testutils.SkipOnOldKernel(t, testCase.version, testCase.progType.String())
+
+			err := HaveProgramType(cache, testCase.progType)
+			testutils.SkipIfNotSupportedOnOS(t, err)
+			if err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
 }
 
 func TestHaveProgramTypeInvalid(t *testing.T) {
-	if err := HaveProgramType(ebpf.ProgramType(math.MaxUint32)); err == nil {
+	cache := btf.NewCache()
+	if err := HaveProgramType(cache, ebpf.ProgramType(math.MaxUint32)); err == nil {
 		t.Fatal("Expected an error")
-	} else if errors.Is(err, internal.ErrNotSupported) {
-		t.Fatal("Got ErrNotSupported:", err)
 	}
 }
 
 func TestHaveProgramHelper(t *testing.T) {
 	type testCase struct {
-		prog     ebpf.ProgramType
-		helper   asm.BuiltinFunc
-		expected error
-		version  string
+		helper  asm.BuiltinFunc
+		version string
 	}
 
 	// Referencing linux kernel commits to track the kernel version required to pass these test cases.
 	// These cases are derived from libbpf's selftests and helper/prog combinations that are
 	// probed for in cilium/cilium.
 	testCases := []testCase{
-		{ebpf.Kprobe, asm.FnMapLookupElem, nil, "3.19"},                     // d0003ec01c66
-		{ebpf.SocketFilter, asm.FnKtimeGetCoarseNs, nil, "5.11"},            // d05512618056
-		{ebpf.SchedCLS, asm.FnSkbVlanPush, nil, "4.3"},                      // 4e10df9a60d9
-		{ebpf.Kprobe, asm.FnSkbVlanPush, ebpf.ErrNotSupported, "4.3"},       // 4e10df9a60d9
-		{ebpf.Kprobe, asm.FnSysBpf, ebpf.ErrNotSupported, "5.14"},           // 79a7f8bdb159
-		{ebpf.Syscall, asm.FnSysBpf, nil, "5.14"},                           // 79a7f8bdb159
-		{ebpf.XDP, asm.FnJiffies64, nil, "5.5"},                             // 5576b991e9c1
-		{ebpf.XDP, asm.FnKtimeGetBootNs, nil, "5.7"},                        // 71d19214776e
-		{ebpf.SchedCLS, asm.FnSkbChangeHead, nil, "5.8"},                    // 6f3f65d80dac
-		{ebpf.SchedCLS, asm.FnRedirectNeigh, nil, "5.10"},                   // b4ab31414970
-		{ebpf.SchedCLS, asm.FnSkbEcnSetCe, nil, "5.1"},                      // f7c917ba11a6
-		{ebpf.SchedACT, asm.FnSkAssign, nil, "5.6"},                         // cf7fbe660f2d
-		{ebpf.SchedACT, asm.FnFibLookup, nil, "4.18"},                       // 87f5fc7e48dd
-		{ebpf.Kprobe, asm.FnFibLookup, ebpf.ErrNotSupported, "4.18"},        // 87f5fc7e48dd
-		{ebpf.CGroupSockAddr, asm.FnGetsockopt, nil, "5.8"},                 // beecf11bc218
-		{ebpf.CGroupSockAddr, asm.FnSkLookupTcp, nil, "4.20"},               // 6acc9b432e67
-		{ebpf.CGroupSockAddr, asm.FnGetNetnsCookie, nil, "5.7"},             // f318903c0bf4
-		{ebpf.CGroupSock, asm.FnGetNetnsCookie, nil, "5.7"},                 // f318903c0bf4
-		{ebpf.Kprobe, asm.FnKtimeGetCoarseNs, ebpf.ErrNotSupported, "5.16"}, // 5e0bc3082e2e
-		{ebpf.CGroupSockAddr, asm.FnGetCgroupClassid, nil, "5.7"},           // 5a52ae4e32a6
-		{ebpf.Kprobe, asm.FnGetBranchSnapshot, nil, "5.16"},                 // 856c02dbce4f
-		{ebpf.SchedCLS, asm.FnSkbSetTstamp, nil, "5.18"},                    // 9bb984f28d5b
-		{ebpf.CGroupSockopt, asm.FnSkStorageDelete, nil, "5.3"},             // 6ac99e8f23d4
-		{ebpf.SkLookup, asm.FnSkcToUdp6Sock, nil, "5.9"},                    // 0d4fad3e57df
-		{ebpf.Syscall, asm.FnSysClose, nil, "5.14"},                         // 3abea089246f
-		{ebpf.Netfilter, asm.FnCgrpStorageDelete, nil, "6.4"},               // c4bcfb38a95e
+		{asm.FnMapLookupElem, "3.19"},     // d0003ec01c66
+		{asm.FnKtimeGetCoarseNs, "5.11"},  // d05512618056
+		{asm.FnSkbVlanPush, "4.3"},        // 4e10df9a60d9
+		{asm.FnSysBpf, "5.14"},            // 79a7f8bdb159
+		{asm.FnJiffies64, "5.5"},          // 5576b991e9c1
+		{asm.FnKtimeGetBootNs, "5.7"},     // 71d19214776e
+		{asm.FnSkbChangeHead, "5.8"},      // 6f3f65d80dac
+		{asm.FnRedirectNeigh, "5.10"},     // b4ab31414970
+		{asm.FnSkbEcnSetCe, "5.1"},        // f7c917ba11a6
+		{asm.FnSkAssign, "5.6"},           // cf7fbe660f2d
+		{asm.FnFibLookup, "4.18"},         // 87f5fc7e48dd
+		{asm.FnGetsockopt, "5.8"},         // beecf11bc218
+		{asm.FnSkLookupTcp, "4.20"},       // 6acc9b432e67
+		{asm.FnGetNetnsCookie, "5.7"},     // f318903c0bf4
+		{asm.FnGetCgroupClassid, "5.7"},   // 5a52ae4e32a6
+		{asm.FnGetBranchSnapshot, "5.16"}, // 856c02dbce4f
+		{asm.FnSkbSetTstamp, "5.18"},      // 9bb984f28d5b
+		{asm.FnSkStorageDelete, "5.3"},    // 6ac99e8f23d4
+		{asm.FnSkcToUdp6Sock, "5.9"},      // 0d4fad3e57df
+		{asm.FnSysClose, "5.14"},          // 3abea089246f
+		{asm.FnCgrpStorageDelete, "6.4"},  // c4bcfb38a95e
 	}
 
+	cache := btf.NewCache()
 	for _, tc := range testCases {
-		t.Run(fmt.Sprintf("%s/%s", tc.prog.String(), tc.helper.String()), func(t *testing.T) {
-			feature := fmt.Sprintf("helper %s for program type %s", tc.helper.String(), tc.prog.String())
+		t.Run(tc.helper.String(), func(t *testing.T) {
+			feature := fmt.Sprintf("helper %s", tc.helper.String())
 
 			testutils.SkipOnOldKernel(t, tc.version, feature)
 
-			err := HaveProgramHelper(tc.prog, tc.helper)
+			err := HaveProgramHelper(cache, tc.helper)
 			testutils.SkipIfNotSupportedOnOS(t, err)
-			if !errors.Is(err, tc.expected) {
-				t.Fatalf("%s/%s: %v", tc.prog.String(), tc.helper.String(), err)
+			if err != nil {
+				t.Fatalf("%s: %v", tc.helper.String(), err)
 			}
 
 		})
 
-	}
-}
-
-func TestHelperProbeNotImplemented(t *testing.T) {
-	// Currently we don't support probing helpers for Tracing, Extension, LSM and StructOps programs.
-	// For each of those test the availability of the FnMapLookupElem helper and expect it to fail.
-	for _, pt := range []ebpf.ProgramType{ebpf.Tracing, ebpf.Extension, ebpf.LSM, ebpf.StructOps} {
-		t.Run(pt.String(), func(t *testing.T) {
-			if err := HaveProgramHelper(pt, asm.FnMapLookupElem); err == nil {
-				t.Fatal("Expected an error")
-			}
-		})
 	}
 }
